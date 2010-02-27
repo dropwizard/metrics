cc1ff6e9efe93bc086fd6bfcd4cc6abe1895e3a4
Coda Hale <coda.hale@gmail.com>
2010-02-26 17:53:02 -0800

    Simplified the holy hell out of WeightedMeter.

--- src/main/scala/com/yammer/metrics/WeightedMeter.scala
+++ src/main/scala/com/yammer/metrics/WeightedMeter.scala
@@ -1,62 +1,71 @@
 package com.yammer.metrics
 
-import java.util.concurrent.atomic.AtomicLong
-import java.util.concurrent.{Executors, TimeUnit}
-import com.yammer.time.Rate
+import java.util.concurrent.TimeUnit
+import com.yammer.time.{Clock, Duration, Rate}
 
 object WeightedMeter {
-  val interval = 5
-  val intervalUnit = TimeUnit.SECONDS
-  private val oneMinuteFactor = 1 /
-          Math.exp(TimeUnit.SECONDS.convert(interval, intervalUnit) / 60.0)
+  val interval = Duration.seconds(5)
+  private val oneMinuteFactor = 1 / Math.exp(interval.minutes.value)
   private val fiveMinuteFactor = oneMinuteFactor / 5
   private val fifteenMinuteFactor = oneMinuteFactor / 15
-  private val executor = Executors.newSingleThreadScheduledExecutor
 
-  def oneMinute() = schedule(new WeightedMeter(oneMinuteFactor))
-  def fiveMinute() = schedule(new WeightedMeter(fiveMinuteFactor))
-  def fifteenMinute() = schedule(new WeightedMeter(fifteenMinuteFactor))
-
-  private def schedule(meter: WeightedMeter): WeightedMeter = {
-    executor.scheduleAtFixedRate(meter, interval, interval, intervalUnit)
-    meter
-  }
+  def oneMinute() = new WeightedMeter(oneMinuteFactor, interval)
+  def fiveMinute() = new WeightedMeter(fiveMinuteFactor, interval)
+  def fifteenMinute() = new WeightedMeter(fifteenMinuteFactor, interval)
 }
 
 /**
- * An exponentially-weighted moving average meter.
+ * An exponentially-weighted moving average meter. Calculates a moving average
+ * of the rate every five seconds.
  *
  * @author coda
  * @see <a href="http://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average">EMA</a>
  */
-class WeightedMeter private(factor: Double) extends Meter with Runnable {
+class WeightedMeter private(factor: Double, interval: Duration) extends Meter {
   import WeightedMeter._
-  private val uncounted = new AtomicLong(0)
-  private val weightedRate = new AtomicLong(0)
+  private val intervalInTicks = interval.ns.value.toLong
+  private var uncounted = 0L
+  private var initialized = false
+  private var ema = 0.0
+  private var lastRecalculation = Clock.nanoTime
 
   override def mark(count: Long) {
-    uncounted.addAndGet(count)
     super.mark(count)
+    synchronized {
+      recalculateIfNeeded()
+      uncounted += count
+    }
   }
   
-  override def rate = Rate(if (weightedRate.get != 0)
-    fromLong(weightedRate.get) / interval
-  else
-    0.0,
-  intervalUnit)
+  override def rate = {
+    val r = synchronized {
+      recalculateIfNeeded()
+      ema
+    }
 
-  @deprecated("don't update the average yourself")
-  def run() {
-    var updated = false
-    while (!updated) {
-      val oldCas = weightedRate.get
-      updated = weightedRate.compareAndSet(oldCas, toLong(calculateEMA(fromLong(oldCas), uncounted.getAndSet(0))))
+    Rate(r, TimeUnit.NANOSECONDS)
+  }
+
+  private def recalculateIfNeeded() {
+    val gap = Clock.nanoTime - lastRecalculation
+    if (gap >= intervalInTicks) {
+      for (i <- 1 to (gap / intervalInTicks).toInt) {
+        recalculate()
+      }
+      lastRecalculation = Clock.nanoTime
     }
   }
 
-  def unweightedRate = super.rate
+  private def recalculate() {
+    if (initialized) {
+      ema += (factor * ((uncounted.toDouble / intervalInTicks) - ema))
+    } else {
+      ema = uncounted.toDouble / intervalInTicks
+      initialized = true
+    }
+    uncounted = 0L
+    lastRecalculation += intervalInTicks
+  }
 
-  private def calculateEMA(oldEma: Double, n: Long) = oldEma + (factor * (n - oldEma))
-  private def fromLong(n: Long) = java.lang.Double.longBitsToDouble(n)
-  private def toLong(d: Double) = java.lang.Double.doubleToLongBits(d)
+  def unweightedRate = super.rate
 }
--- src/test/scala/com/yammer/metrics/tests/WeightedMeterTest.scala
+++ src/test/scala/com/yammer/metrics/tests/WeightedMeterTest.scala
@@ -1,20 +1,39 @@
 package com.yammer.metrics.tests
 
-import org.scalatest.Spec
 import org.scalatest.matchers.MustMatchers
 import com.yammer.metrics.WeightedMeter
+import com.yammer.time.{Duration, Clock}
+import org.scalatest.{BeforeAndAfterEach, Spec}
 
-class WeightedMeterTest extends Spec with MustMatchers {
-  describe("a one-minute weighted meter") {
-    val meter = WeightedMeter.oneMinute
+class WeightedMeterTest extends Spec with MustMatchers with BeforeAndAfterEach {
+  override protected def afterEach() {
+    Clock.unfreezeTime()
+  }
 
-    it("calculates an exponentially decaying average") {
-      for (i <- 1 to 100) {
+  describe("a load meter") {
+    it("calculates an exponentially decaying average at one, five, and fifteen minute settings") {
+      Clock.freezeTime(0)
+      val meter = WeightedMeter.oneMinute
+      // load the meter up with 1 event per ms
+      for (i <- 1 to 1000) {
         meter.mark()
+        Clock.freezeTime(Clock.nanoTime + Duration.milliseconds(1).ns.value.toLong)
       }
-      meter.rate.s.value must be(0.0 plusOrMinus 0.00001)
-      Thread.sleep(5000)
-      meter.rate.s.value must be(18.0 plusOrMinus 5.0)
+
+      // wait another 5 seconds
+      Clock.freezeTime(Clock.nanoTime + Duration.seconds(5).ns.value.toLong)
+
+      meter.rate.s.value must be(200.0 plusOrMinus 0.1)
+
+      // wait another 5 seconds
+      Clock.freezeTime(Clock.nanoTime + Duration.seconds(5).ns.value.toLong)
+
+      meter.rate.s.value must be(15.99 plusOrMinus 0.1)
+
+      // wait another 5 seconds
+      Clock.freezeTime(Clock.nanoTime + Duration.seconds(5).ns.value.toLong)
+
+      meter.rate.s.value must be(1.28 plusOrMinus 0.1)
     }
   }
 }
