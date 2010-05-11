a93d79fcdda55ded81a536e8e38bf8acd271f3c8
Coda Hale <coda.hale@gmail.com>
2010-05-11 10:04:51 -0700

    Fixed the build-chain for RC2, but there's some deprecations and a test that doesn't pass.

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -30,6 +30,10 @@ class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poste
   /**
    * Dependencies
    */
-  val scalaTest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources()
+  val scalaTest = buildScalaVersion match {
+    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case unknown => error("no known scalatest impl for %s".format(unknown))
+  }
   val mockito = "org.mockito" % "mockito-core" % "1.8.0" % "test" withSources()
 }
