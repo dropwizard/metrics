49a2076ef8a90ef78dd0cff1aed7485f14829d1c
Coda Hale <coda.hale@gmail.com>
2010-06-01 16:12:30 -0700

    Add RC3 as a build target and start using rsync-sbt.

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -1,26 +1,15 @@
-class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish {
-  /**
-   * Home Repo
-   */
-  override def managedStyle = sbt.ManagedStyle.Maven
-  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
-
-  /**
-   * Publish to a local temp repo, then rsync the files over to repo.codahale.com.
-   */
-  def publishToLocalRepoAction = super.publishAction
-  override def publishAction = task {
-    log.info("Uploading to repo.codahale.com")
-    sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
-    None
-  } describedAs("what") dependsOn(test, publishToLocalRepoAction)
-
+class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with posterous.Publish with rsync.RsyncPublishing {
   /**
    * Publish the source as well as the class files.
    */
   override def packageSrcJar= defaultJarPath("-sources.jar")
   val sourceArtifact = sbt.Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+
+  /**
+   * Publish via rsync.
+   */
+  def rsyncRepo = "codahale.com:/home/codahale/repo.codahale.com"
 
   /**
    * Repositories
@@ -33,7 +22,8 @@ class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poste
   val scalaTest = buildScalaVersion match {
     case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC3" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }
-  val mockito = "org.mockito" % "mockito-core" % "1.8.0" % "test" withSources()
+  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,4 +1,7 @@
 class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-  val posterous = "net.databinder" % "posterous-sbt" % "0.1.3"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
+
+  val codasRepo = "codahale.com" at "http://repo.codahale.com/"
+  val rsync = "com.codahale" % "rsync-sbt" % "0.1.0"
 }
