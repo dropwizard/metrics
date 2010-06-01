8ce55eb6388802f8998d2489ea0342244f38765d
Coda Hale <coda.hale@gmail.com>
2010-06-01 16:34:44 -0700

    Upgrade rsync-sbt.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
 
   val codasRepo = "codahale.com" at "http://repo.codahale.com/"
-  val rsync = "com.codahale" % "rsync-sbt" % "0.1.0"
+  val rsync = "com.codahale" % "rsync-sbt" % "0.1.1"
 }
