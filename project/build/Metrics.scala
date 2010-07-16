ec04c85dca171e1ec2bd0fb3bd415da8f9365e08
Coda Hale <coda.hale@gmail.com>
2010-07-15 19:05:49 -0700

    Bump to 1.0.6.

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -19,11 +19,6 @@ class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poste
   /**
    * Dependencies
    */
-  val scalaTest = buildScalaVersion match {
-    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
-    case "2.8.0.RC3" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
-    case unknown => error("no known scalatest impl for %s".format(unknown))
-  }
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
