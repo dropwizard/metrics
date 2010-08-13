dbcc566cadf94cae676170aa64beafa98e32fc7c
Coda Hale <coda.hale@gmail.com>
2010-08-13 10:02:53 -0700

    Bump to scalatest 1.2 final.

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -14,11 +14,11 @@ class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poste
   /**
    * Repositories
    */
-  val scalaToolsSnapshots = "scala-tools.org Snapshots" at "http://scala-tools.org/repo-snapshots"
+  val scalaTools = "scala-tools.org Releases" at "http://scala-tools.org/repo-releases"
 
   /**
    * Dependencies
    */
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
+  val scalaTest = "org.scalatest" % "scalatest" % "1.2" % "test" withSources() intransitive()
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 }
