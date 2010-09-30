c4a771eee876a08379a95ed635df193916f00a85
cliff <cliff@moonpolysoft.com>
2010-09-30 14:00:09 -0400

    bump for 2.8

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -23,7 +23,12 @@ class Metrics(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with poste
     case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC6" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test" withSources() intransitive()
+case "2.8.0"       => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.final-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
+  
+  val log4j = "log4j" % "log4j" % "1.2.16"
+  val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.8"
+  val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
 }
