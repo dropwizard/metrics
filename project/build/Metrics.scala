cdfbd9a2c90ef552f8bb6ec20ed44eacc5b5748e
Coda Hale <coda.hale@gmail.com>
2010-12-10 23:13:34 -0800

    We now depend on Jackson. For JSON.

--- project/build/Metrics.scala
+++ project/build/Metrics.scala
@@ -22,6 +22,11 @@ class Metrics(info: ProjectInfo) extends DefaultProject(info)
   /**
    * Dependencies
    */
+  val jackson = "org.codehaus.jackson" % "jackson-core-asl" % "1.6.3" withSources()
+
+  /**
+   * Test Dependencies
+   */
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test" withSources ()
   val simplespec = "com.codahale" %% "simplespec" % "0.2.0" % "test" withSources ()
   val scalaTest = "org.scalatest" % "scalatest" % "1.2" % "test" withSources() intransitive()
