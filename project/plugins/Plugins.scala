5a7219f6f5f35256465571461beb6485b0e2b81a
Coda Hale <coda.hale@gmail.com>
2010-12-23 14:57:56 -0800

    Bump maven-sbt to 0.0.3.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -6,5 +6,5 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
 
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
-  val mavenSBT = "com.codahale" % "maven-sbt" % "0.0.2"
+  val mavenSBT = "com.codahale" % "maven-sbt" % "0.0.3"
 }
