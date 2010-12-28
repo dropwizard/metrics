479ec2918005ff94919ffc8db771eba729b50832
Coda Hale <coda.hale@gmail.com>
2010-12-27 17:22:44 -0800

    Ditch Counter#-- and #++.
    Their interaction with the Scala AST is, uh, unpredictable.

--- src/test/scala/com/yammer/metrics/tests/CounterSpec.scala
+++ src/test/scala/com/yammer/metrics/tests/CounterSpec.scala
@@ -9,23 +9,11 @@ object CounterSpec extends Spec with Mockito {
   class `A counter` {
     val metric = mock[CounterMetric]
     val counter = new Counter(metric)
-
-    def `should increment the underlying metric` {
-      counter++
-
-      there was one(metric).inc()
-    }
-
+    
     def `should increment the underlying metric by an arbitrary amount` {
       counter += 12
 
       there was one(metric).inc(12)
-    }
-
-    def `should decrement the underlying metric` {
-      counter--
-
-      there was one(metric).dec()
     }
 
     def `should decrement the underlying metric by an arbitrary amount` {
@@ -35,3 +23,4 @@ object CounterSpec extends Spec with Mockito {
     }
   }
 }
+//
