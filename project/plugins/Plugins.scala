f24ad11ef02900c7c46704e64ee4afc62b28d7f6
Coda Hale <coda.hale@gmail.com>
2011-01-10 11:41:29 -0800

    Upgrade to maven-sbt 0.1.0.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -6,5 +6,5 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
 
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
-  val mavenSBT = "com.codahale" % "maven-sbt" % "0.1.0-SNAPSHOT"
+  val mavenSBT = "com.codahale" % "maven-sbt" % "0.1.0"
 }
