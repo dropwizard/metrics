b1e5760f7d3f0f9521a4ec494af4865d3a1a96a8
Coda Hale <coda.hale@gmail.com>
2011-03-12 22:47:39 -0800

    Added linear scaling to avoid significand overflow.

--- src/main/java/com/yammer/metrics/core/ExponentiallyDecayingSample.java
+++ src/main/java/com/yammer/metrics/core/ExponentiallyDecayingSample.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import static java.lang.Math.*;
 
@@ -19,10 +20,13 @@ import static java.lang.Math.*;
  * Data Engineering (2009)</a>
  */
 public class ExponentiallyDecayingSample implements Sample {
+	private static final long RESCALE_THRESHOLD = 1800; // seconds, every 30min
 	private final ConcurrentSkipListMap<Double, Long> values;
+	private final ReentrantReadWriteLock lock;
 	private final double alpha;
 	private final int reservoirSize;
-	private final AtomicLong count = new AtomicLong();
+	private final AtomicLong count = new AtomicLong(0);
+	private volatile long lastScaledAt;
 	private volatile long startTime;
 
 	/**
@@ -35,6 +39,7 @@ public class ExponentiallyDecayingSample implements Sample {
 	 */
 	public ExponentiallyDecayingSample(int reservoirSize, double alpha) {
 		this.values = new ConcurrentSkipListMap<Double, Long>();
+		this.lock = new ReentrantReadWriteLock();
 		this.alpha = alpha;
 		this.reservoirSize = reservoirSize;
 		clear();
@@ -45,6 +50,7 @@ public class ExponentiallyDecayingSample implements Sample {
 		values.clear();
 		count.set(0);
 		this.startTime = tick();
+		this.lastScaledAt = tick();
 	}
 
 	@Override
@@ -64,21 +70,30 @@ public class ExponentiallyDecayingSample implements Sample {
 	 * @param timestamp the epoch timestamp of {@code value} in milliseconds
 	 */
 	public void update(long value, long timestamp) {
-		final double priority = weight(timestamp - startTime) / random();
-		final long newCount = count.incrementAndGet();
+		lock.readLock().lock();
+		try {
+			final double priority = weight(timestamp - startTime) / random();
+			final long newCount = count.incrementAndGet();
 
-		if (newCount <= reservoirSize) {
-			values.put(priority, value);
-		} else {
-			Double first = values.firstKey();
-			if (first < priority) {
+			if (newCount <= reservoirSize) {
 				values.put(priority, value);
+			} else {
+				Double first = values.firstKey();
+				if (first < priority) {
+					values.put(priority, value);
 
-				// ensure we always remove an item
-				while (values.remove(first) == null) {
-					first = values.firstKey();
+					// ensure we always remove an item
+					while (values.remove(first) == null) {
+						first = values.firstKey();
+					}
 				}
 			}
+		} finally {
+			lock.readLock().unlock();
+		}
+
+		if (tick() > lastScaledAt + RESCALE_THRESHOLD) {
+			rescale();
 		}
 	}
 
@@ -91,5 +106,41 @@ public class ExponentiallyDecayingSample implements Sample {
 
 	private double weight(long t) {
 		return exp(alpha * t);
+	}
+
+	/**
+	 * "A common feature of the above techniques—indeed, the key technique that
+	 * allows us to track the decayed weights efficiently—is that they maintain
+	 * counts and other quantities based on g(ti − L), and only scale by g(t − L)
+	 * at query time. But while g(ti −L)/g(t−L) is guaranteed to lie between zero
+	 * and one, the intermediate values of g(ti − L) could become very large. For
+	 * polynomial functions, these values should not grow too large, and should be
+	 * effectively represented in practice by floating point values without loss of
+	 * precision. For exponential functions, these values could grow quite large as
+	 * new values of (ti − L) become large, and potentially exceed the capacity of
+	 * common floating point types. However, since the values stored by the
+	 * algorithms are linear combinations of g values (scaled sums), they can be
+	 * rescaled relative to a new landmark. That is, by the analysis of exponential
+	 * decay in Section III-A, the choice of L does not affect the final result. We
+	 * can therefore multiply each value based on L by a factor of exp(−α(L′ − L)),
+	 * and obtain the correct value as if we had instead computed relative to a new
+	 * landmark L′ (and then use this new L′ at query time). This can be done with
+	 * a linear pass over whatever data structure is being used."
+	 */
+	private void rescale() {
+		lock.writeLock().lock();
+		try {
+			final long newTick = tick();
+			final long oldStartTime = startTime;
+			this.startTime = newTick;
+			final ArrayList<Double> keys = new ArrayList<Double>(values.keySet());
+			for (Double key : keys) {
+				final Long value = values.remove(key);
+				values.put(key * exp(-alpha * (startTime - oldStartTime)), value);
+			}
+			this.lastScaledAt = newTick;
+		} finally {
+			lock.writeLock().unlock();
+		}
 	}
 }
