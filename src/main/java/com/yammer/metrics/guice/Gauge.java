e4600e147826260467b4aeedb04455617afd1480
Coda Hale <coda.hale@gmail.com>
2011-04-01 01:06:55 -0700

    Added Gauge annotation.

---
+++ src/main/java/com/yammer/metrics/guice/Gauge.java
@@ -0,0 +1,26 @@
+package com.yammer.metrics.guice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * An annotation for marking a method of a Guice-provided object as a gauge.
+ * <p/>
+ * Given a method like this:
+ * <pre><code>
+ *     @Gauge(name = "queueSize")
+ *     public int getQueueSize() {
+ *         return queue.size;
+ *     }
+ * </code></pre>
+ * <p/>
+ * A gauge for the defining class with the name {@code queueSize} will be
+ * created which uses the annotated method's return value as its value.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Gauge {
+    String name() default "";
+}
--- src/main/java/com/yammer/metrics/guice/InstrumentationModule.java
+++ src/main/java/com/yammer/metrics/guice/InstrumentationModule.java
@@ -7,15 +7,18 @@ import com.google.inject.matcher.Matchers;
  * A Guice module which instruments methods annotated with the {@link Metered}
  * and {@link Timed} annotations.
  *
+ * @see Gauge
  * @see Metered
  * @see Timed
  * @see MeteredInterceptor
  * @see TimedInterceptor
+ * @see GaugeInjectionListener
  */
 public class InstrumentationModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bindListener(Matchers.any(), new MeteredListener());
 		bindListener(Matchers.any(), new TimedListener());
+        bindListener(Matchers.any(), new GaugeListener());
 	}
 }
