68013fd26a8e3651be0c040d1114bfdcf51b9a20
Coda Hale <coda.hale@gmail.com>
2011-05-01 12:57:38 -0700

    Break metrics project into core, guice, jetty, and servlet.

--- project/build/MetricsProject.scala
+++ project/build/MetricsProject.scala
@@ -1,39 +1,93 @@
 import sbt._
 import maven._
 
-class MetricsProject(info: ProjectInfo) extends DefaultProject(info)
-                                                with MavenDependencies
-                                                with IdeaProject {
-  /**
-   * Publish the source as well as the class files.
-   */
-  override def packageSrcJar = defaultJarPath("-sources.jar")
-  val sourceArtifact = Artifact.sources(artifactID)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+class MetricsProject(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
+  class MetricsModule(info: ProjectInfo) extends DefaultProject(info) with MavenDependencies with IdeaProject {
+    /**
+     * Publish via maven-sbt.
+     */
+    lazy val publishTo = Resolver.sftp("repo.codahale.com",
+      "codahale.com",
+      "/home/codahale/repo.codahale.com/")
 
-  /**
-   * Publish via maven-sbt.
-   */
-  lazy val publishTo = Resolver.sftp("repo.codahale.com",
-                                     "codahale.com",
-                                     "/home/codahale/repo.codahale.com/")
+    /**
+     * Publish the source as well as the class files.
+     */
+    override def packageSrcJar = defaultJarPath("-sources.jar")
+    lazy val sourceArtifact = Artifact.sources(artifactID)
+    override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
 
-  /**
-   * Repositories
-   */
-  val dropWizard = "Coda's Repo" at "http://repo.codahale.com"
+    /**
+     * Repositories
+     */
+    val dropWizard = "Coda's Repo" at "http://repo.codahale.com"
 
-  /**
-   * Optional Dependencies
-   */
-  val jackson = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.5" % "optional"
-  val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "optional"
-  val jetty = "org.eclipse.jetty" % "jetty-server" % "7.4.0.v20110414" % "optional"
-  val guice = "com.google.inject" % "guice" % "3.0" % "optional"
+    /**
+     * Test Dependencies
+     */
+    val simplespec = "com.codahale" % "simplespec_2.8.1" % "0.2.0" % "test"
+    val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
+  }
 
-  /**
-   * Test Dependencies
-   */
-  val simplespec = "com.codahale" % "simplespec_2.8.1" % "0.2.0" % "test"
-  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
+  lazy val core = project("core", "metrics-core", new CoreProject(_))
+
+  lazy val jetty = project("jetty", "metrics-jetty", new JettyProject(_), core)
+
+  lazy val guice = project("guice", "metrics-guice", new GuiceProject(_), core)
+
+  lazy val servlet = project("servlet", "metrics-servlet", new ServletProject(_), core)
+
+  class CoreProject(info: ProjectInfo) extends MetricsModule(info) {
+
+  }
+
+  class JettyProject(info: ProjectInfo) extends MetricsModule(info) {
+    val jetty = "org.eclipse.jetty" % "jetty-server" % "7.4.0.v20110414"
+  }
+
+  class GuiceProject(info: ProjectInfo) extends MetricsModule(info) {
+    val guice = "com.google.inject" % "guice" % "3.0"
+  }
+
+  class ServletProject(info: ProjectInfo) extends MetricsModule(info) {
+    val jackson = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.5"
+    val servletApi = "javax.servlet" % "servlet-api" % "2.5"
+  }
 }
+
+//class DerpMetricsProject(info: ProjectInfo) extends DefaultProject(info)
+//                                                with MavenDependencies
+//                                                with IdeaProject {
+//  /**
+//   * Publish the source as well as the class files.
+//   */
+//  override def packageSrcJar = defaultJarPath("-sources.jar")
+//  val sourceArtifact = Artifact.sources(artifactID)
+//  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+//
+//  /**
+//   * Publish via maven-sbt.
+//   */
+//  lazy val publishTo = Resolver.sftp("repo.codahale.com",
+//                                     "codahale.com",
+//                                     "/home/codahale/repo.codahale.com/")
+//
+//  /**
+//   * Repositories
+//   */
+//  val dropWizard = "Coda's Repo" at "http://repo.codahale.com"
+//
+//  /**
+//   * Optional Dependencies
+//   */
+//  val jackson = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.5" % "optional"
+//  val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "optional"
+//  val jetty = "org.eclipse.jetty" % "jetty-server" % "7.4.0.v20110414" % "optional"
+//  val com.yammer.metrics.guice = "com.google.inject" % "com.yammer.metrics.guice" % "3.0" % "optional"
+//
+//  /**
+//   * Test Dependencies
+//   */
+//  val simplespec = "com.codahale" % "simplespec_2.8.1" % "0.2.0" % "test"
+//  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
+//}
