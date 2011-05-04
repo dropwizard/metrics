c040799ced99a5dc44c2fb1181ec43265c4d16ac
Coda Hale <coda.hale@gmail.com>
2011-05-03 18:01:46 -0700

    Added scopes to MetricNames.

--- core/src/main/java/com/yammer/metrics/core/MetricName.java
+++ core/src/main/java/com/yammer/metrics/core/MetricName.java
@@ -5,18 +5,30 @@ package com.yammer.metrics.core;
  */
 public class MetricName {
 	private final Class<?> klass;
-	private final String name;
+	private final String name, scope;
 
 	/**
-	 * Creates a new {@link MetricName}.
+	 * Creates a new {@link MetricName} without a scope.
 	 *
 	 * @param klass the {@link Class} to which the {@link Metric} belongs
 	 * @param name the name of the {@link Metric}
 	 */
 	public MetricName(Class<?> klass, String name) {
-		this.klass = klass;
-		this.name = name;
-	}
+        this(klass, name, null);
+    }
+
+    /**
+     * Creates a new {@link MetricName} without a scope.
+     *
+     * @param klass the {@link Class} to which the {@link Metric} belongs
+     * @param name  the name of the {@link Metric}
+     * @param scope the scope of the {@link Metric}
+     */
+    public MetricName(Class<?> klass, String name, String scope) {
+        this.klass = klass;
+        this.name = name;
+        this.scope = scope;
+    }
 
 	/**
 	 * Returns the {@link Class} to which the {@link Metric} belongs.
@@ -36,22 +48,43 @@ public class MetricName {
 		return name;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) { return true; }
-		if (o == null || getClass() != o.getClass()) { return false; }
+    /**
+     * Returns the scope of the {@link Metric}.
+     *
+     * @return the scope of the {@link Metric}
+     */
+    public String getScope() {
+        return scope;
+    }
 
-		final MetricName that = (MetricName) o;
+    /**
+     * Returns {@code true} if the {@link Metric} has a scope, {@code false}
+     * otherwise.
+     *
+     * @return {@code true} if the {@link Metric} has a scope
+     */
+    public boolean hasScope() {
+        return scope != null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MetricName that = (MetricName) o;
 
         return !(klass != null ? !klass.equals(that.klass) : that.klass != null)
-                && !(name != null ? !name.equals(that.name) : that.name != null);
+                && !(name != null ? !name.equals(that.name) : that.name != null)
+                && !(scope != null ? !scope.equals(that.scope) : that.scope != null);
 
     }
 
-	@Override
-	public int hashCode() {
-		int result = klass != null ? klass.hashCode() : 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = klass != null ? klass.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (scope != null ? scope.hashCode() : 0);
+        return result;
+    }
 }
