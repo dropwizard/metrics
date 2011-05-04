178dd0e5d4ddc1071f43ae13d3a72e068ffa639d
Coda Hale <coda.hale@gmail.com>
2011-05-03 18:18:18 -0700

    Added scoped creators for all metrics.

--- core/src/main/scala/com/yammer/metrics/MetricsGroup.scala
+++ core/src/main/scala/com/yammer/metrics/MetricsGroup.scala
@@ -13,10 +13,11 @@ class MetricsGroup(val klass: Class[_]) {
   /**
    * Registers a new gauge metric.
    *
-   * @param name the name of the gauge
+   * @param name  the name of the gauge
+   * @param scope the scope of the gauge
    */
-  def gauge[A](name: String)(f: => A) = {
-    Metrics.newGauge(klass, name, new GaugeMetric[A] {
+  def gauge[A](name: String, scope: String = null)(f: => A) = {
+    Metrics.newGauge(klass, name, scope, new GaugeMetric[A] {
       def value = f
     })
   }
@@ -24,18 +25,23 @@ class MetricsGroup(val klass: Class[_]) {
   /**
    * Creates a new counter metric.
    *
-   * @param name the name of the counter
+   * @param name  the name of the counter
+   * @param scope the scope of the gauge
    */
-  def counter(name: String) = new Counter(Metrics.newCounter(klass, name))
+  def counter(name: String, scope: String = null) =
+    new Counter(Metrics.newCounter(klass, name, scope))
 
   /**
    * Creates a new histogram metrics.
    *
-   * @param name the name of the histogram
+   * @param name   the name of the histogram
+   * @param scope  the scope of the histogram
    * @param biased whether or not to use a biased sample
    */
-  def histogram(name: String, biased: Boolean = false) =
-    new Histogram(Metrics.newHistogram(klass, name, biased))
+  def histogram(name: String,
+                scope: String = null,
+                biased: Boolean = false) =
+    new Histogram(Metrics.newHistogram(klass, name, scope, biased))
 
   /**
    * Creates a new meter metric.
@@ -43,22 +49,26 @@ class MetricsGroup(val klass: Class[_]) {
    * @param name the name of the meter
    * @param eventType the plural name of the type of events the meter is
    *                  measuring (e.g., {@code "requests"})
+   * @param scope the scope of the meter
    * @param unit the time unit of the meter
    */
   def meter(name: String,
             eventType: String,
+            scope: String = null,
             unit: TimeUnit = TimeUnit.SECONDS) =
-    new Meter(Metrics.newMeter(klass, name, eventType, unit))
+    new Meter(Metrics.newMeter(klass, name, scope, eventType, unit))
 
   /**
    * Creates a new timer metric.
    *
    * @param name the name of the timer
+   * @param scope the scope of the timer
    * @param durationUnit the time unit for measuring duration
    * @param rateUnit the time unit for measuring rate
    */
   def timer(name: String,
+            scope: String = null,
             durationUnit: TimeUnit = TimeUnit.MILLISECONDS,
             rateUnit: TimeUnit = TimeUnit.SECONDS) =
-    new Timer(Metrics.newTimer(klass, name, durationUnit, rateUnit))
+    new Timer(Metrics.newTimer(klass, name, scope, durationUnit, rateUnit))
 }
