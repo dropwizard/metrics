d870a0f64a6268022098db259e258ef7ae448812
Mårten Gustafson <marten.gustafson@gmail.com>
2011-05-10 11:08:09 +0200

    Initial stab at Eclipse support

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -7,4 +7,6 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
 
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
   val mavenSBT = "com.codahale" % "maven-sbt" % "0.1.1"
+
+  val SBTeclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
 }
