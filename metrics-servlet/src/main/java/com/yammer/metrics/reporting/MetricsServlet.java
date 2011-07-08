d8cb0ca5951cc2980463575e1901e7b67db71b90
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-07-08 23:52:22 +0100

    Added current_time to metrics json

--- metrics-servlet/src/main/java/com/yammer/metrics/reporting/MetricsServlet.java
+++ metrics-servlet/src/main/java/com/yammer/metrics/reporting/MetricsServlet.java
@@ -307,6 +307,7 @@ public class MetricsServlet extends HttpServlet {
 
             json.writeNumberField("daemon_thread_count", daemonThreadCount());
             json.writeNumberField("thread_count", threadCount());
+            json.writeNumberField("current_time", System.currentTimeMillis());
             json.writeNumberField("uptime", uptime());
             json.writeNumberField("fd_usage", fileDescriptorUsage());
 
