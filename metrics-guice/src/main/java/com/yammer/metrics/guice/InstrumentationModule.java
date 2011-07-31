17740f16fd6bf7dac5410f15100786f38e5f695a
Thomas Dudziak <thomas@ning.com>
2011-07-31 13:26:01 -0700

    Need to bind the jmx reporter in guice, otherwise the mbeans won't be exported

--- metrics-guice/src/main/java/com/yammer/metrics/guice/InstrumentationModule.java
+++ metrics-guice/src/main/java/com/yammer/metrics/guice/InstrumentationModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
 import com.yammer.metrics.HealthCheckRegistry;
 import com.yammer.metrics.MetricsRegistry;
+import com.yammer.metrics.reporting.JmxReporter;
 
 /**
  * A Guice module which instruments methods annotated with the {@link Metered}
@@ -22,6 +23,7 @@ public class InstrumentationModule extends AbstractModule {
         MetricsRegistry metricsRegistry = new MetricsRegistry();
         bind(MetricsRegistry.class).toInstance(metricsRegistry);
         bind(HealthCheckRegistry.class).asEagerSingleton();
+        bind(JmxReporter.class).toProvider(JmxReporterProvider.class).asEagerSingleton();
         bindListener(Matchers.any(), new MeteredListener(metricsRegistry));
         bindListener(Matchers.any(), new TimedListener(metricsRegistry));
         bindListener(Matchers.any(), new GaugeListener(metricsRegistry));
