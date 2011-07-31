23069a084df31f71f6044d36fae7b1f7d16dba07
Thomas Dudziak <thomas@ning.com>
2011-07-31 13:26:01 -0700

    Need to bind the jmx reporter in guice, otherwise the mbeans won't be exported
    Conflicts:
    
    	metrics-guice/src/main/java/com/yammer/metrics/guice/InstrumentationModule.java

---
+++ metrics-guice/src/main/java/com/yammer/metrics/guice/JmxReporterProvider.java
@@ -0,0 +1,21 @@
+package com.yammer.metrics.guice;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.yammer.metrics.MetricsRegistry;
+import com.yammer.metrics.reporting.JmxReporter;
+
+public class JmxReporterProvider implements Provider<JmxReporter>
+{
+    private final MetricsRegistry metricsRegistry;
+
+    @Inject
+    public JmxReporterProvider(MetricsRegistry metricsRegistry) {
+        this.metricsRegistry = metricsRegistry;
+    }
+
+    @Override
+    public JmxReporter get() {
+        return new JmxReporter(metricsRegistry);
+    }
+}
