3b29a45553fa647258b45f51002db0cae79c9ccb
Coda Hale <coda.hale@gmail.com>
2011-09-23 17:29:50 -0700

    Reuse thread pools.

--- metrics-core/src/main/java/com/yammer/metrics/util/ThreadPools.java
+++ metrics-core/src/main/java/com/yammer/metrics/util/ThreadPools.java
@@ -1,34 +1,44 @@
 package com.yammer.metrics.util;
 
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 public class ThreadPools
 {
-    private final Set<ExecutorService> threadPools = new CopyOnWriteArraySet<ExecutorService>();
+    private final ConcurrentMap<String, ScheduledExecutorService> threadPools =
+            new ConcurrentHashMap<String, ScheduledExecutorService>(100);
 
     /**
-     * Creates a new scheduled thread pool of a given size with the given name.
+     * Creates a new scheduled thread pool of a given size with the given name,
+     * or returns an existing thread pool if one was already created with the
+     * same name.
      *
      * @param poolSize the number of threads to create
      * @param name the name of the pool
      * @return a new {@link ScheduledExecutorService}
      */
     public ScheduledExecutorService newScheduledThreadPool(int poolSize, String name) {
-        final ScheduledExecutorService service = Executors.newScheduledThreadPool(poolSize, new NamedThreadFactory(name));
-        threadPools.add(service);
-        return service;
+        final ScheduledExecutorService existing = threadPools.get(name);
+        if (existing == null) {
+            // We lock here because executors are expensive to create.
+            synchronized (threadPools) {
+                final ScheduledExecutorService service = Executors.newScheduledThreadPool(poolSize, new NamedThreadFactory(name));
+                threadPools.put(name, service);
+                return service;
+            }
+        } else {
+            return existing;
+        }
     }
 
     /**
      * Shuts down all thread pools created by this class in an orderly fashion.
      */
     public void shutdownThreadPools() {
-        for (ExecutorService executor : threadPools) {
-            executor.shutdown();
+        synchronized (threadPools) {
+            for (ExecutorService executor : threadPools.values()) {
+                executor.shutdown();
+            }
+            threadPools.clear();
         }
     }
 
