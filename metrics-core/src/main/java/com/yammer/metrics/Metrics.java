5d0a87da6d6be2c4e790933bf94cf399ab933df5
Coda Hale <coda.hale@gmail.com>
2011-10-18 15:45:16 -0700

    Formatting fixes.

--- metrics-core/src/main/java/com/yammer/metrics/Metrics.java
+++ metrics-core/src/main/java/com/yammer/metrics/Metrics.java
@@ -17,11 +17,10 @@ public class Metrics {
         JmxReporter.startDefault(DEFAULT_REGISTRY);
         // make sure we initialize this so it can monitor GC etc
         VirtualMachineMetrics.daemonThreadCount();
-        
-        Runtime.getRuntime().addShutdownHook(new Thread(){
-        	public void run(){
-        		JmxReporter.shutdownDefault();
-        	}
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                JmxReporter.shutdownDefault();
+            }
         });
     }}
 
