8d45ea1822fa75b9c49b7b74502abeb6268176fe
Coda Hale <coda.hale@gmail.com>
2011-11-15 14:04:14 -0800

    Formatted and styled WebappMetricsFilter.

--- metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
+++ metrics-web/src/main/java/com/yammer/metrics/web/WebappMetricsFilter.java
@@ -1,51 +1,57 @@
 package com.yammer.metrics.web;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.CounterMetric;
+import com.yammer.metrics.core.MeterMetric;
+import com.yammer.metrics.core.TimerMetric;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
-
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.CounterMetric;
-import com.yammer.metrics.core.MeterMetric;
-import com.yammer.metrics.core.TimerMetric;
-
 /**
- * {@link Filter} implementation which captures request information and a breakdown of the response codes being returned.
+ * {@link Filter} implementation which captures request information and a breakdown of the response
+ * codes being returned.
  */
 public abstract class WebappMetricsFilter implements Filter {
-	
-    private final Map<Integer, MeterMetric> metersByStatusCode = new HashMap<Integer, MeterMetric>();
+
+    private final Map<Integer, MeterMetric> metersByStatusCode;
     private final MeterMetric otherMeterMetric;
     private final CounterMetric activeRequests;
     private final TimerMetric requestTimer;
-    
+
     /**
      * Creates a new instance of the filter.
-     * 
-     * @param meterNamesByStatusCode A map, keyed by status code, of meter names that we are interested in. 
-     * @param otherMetricName The name used for the catch-all meter.
+     *
+     * @param meterNamesByStatusCode A map, keyed by status code, of meter names that we are
+     *                               interested in.
+     * @param otherMetricName        The name used for the catch-all meter.
      */
-    public WebappMetricsFilter(Map<Integer, String> meterNamesByStatusCode, String otherMetricName) {
-        
+    public WebappMetricsFilter(Map<Integer, String> meterNamesByStatusCode,
+                               String otherMetricName) {
+        this.metersByStatusCode = new HashMap<Integer, MeterMetric>(meterNamesByStatusCode.size());
         for (Entry<Integer, String> entry : meterNamesByStatusCode.entrySet()) {
-            metersByStatusCode.put(entry.getKey(), Metrics.newMeter(WebappMetricsFilter.class, entry.getValue(), "responses", TimeUnit.SECONDS));
+            metersByStatusCode.put(entry.getKey(),
+                                   Metrics.newMeter(WebappMetricsFilter.class,
+                                                    entry.getValue(),
+                                                    "responses",
+                                                    TimeUnit.SECONDS));
         }
-        
-        otherMeterMetric = Metrics.newMeter(WebappMetricsFilter.class, otherMetricName, "responses", TimeUnit.SECONDS);
-        activeRequests = Metrics.newCounter(WebappMetricsFilter.class, "activeRequests");
-        requestTimer = Metrics.newTimer(WebappMetricsFilter.class, "requests", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
-        
+        this.otherMeterMetric = Metrics.newMeter(WebappMetricsFilter.class,
+                                                 otherMetricName,
+                                                 "responses",
+                                                 TimeUnit.SECONDS);
+        this.activeRequests = Metrics.newCounter(WebappMetricsFilter.class, "activeRequests");
+        this.requestTimer = Metrics.newTimer(WebappMetricsFilter.class,
+                                             "requests",
+                                             TimeUnit.MILLISECONDS,
+                                             TimeUnit.SECONDS);
+
     }
 
     @Override
@@ -57,14 +63,13 @@ public abstract class WebappMetricsFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        
-        long start = System.currentTimeMillis();
-
-        StatusExposingServletResponse wrappedResponse = new StatusExposingServletResponse((HttpServletResponse) response);
-        
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        final long start = System.currentTimeMillis();
+        final StatusExposingServletResponse wrappedResponse =
+                new StatusExposingServletResponse((HttpServletResponse) response);
         activeRequests.inc();
-
         try {
             chain.doFilter(request, wrappedResponse);
         } finally {
@@ -72,19 +77,15 @@ public abstract class WebappMetricsFilter implements Filter {
             markMeterForStatusCode(wrappedResponse.getStatus());
             requestTimer.update(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
         }
-
     }
 
     private void markMeterForStatusCode(int status) {
-
-        MeterMetric metric = metersByStatusCode.get(status);
-
+        final MeterMetric metric = metersByStatusCode.get(status);
         if (metric != null) {
             metric.mark();
         } else {
             otherMeterMetric.mark();
         }
-
     }
 
     private static class StatusExposingServletResponse extends HttpServletResponseWrapper {
@@ -116,7 +117,5 @@ public abstract class WebappMetricsFilter implements Filter {
         public int getStatus() {
             return httpStatus;
         }
-
     }
-
 }
