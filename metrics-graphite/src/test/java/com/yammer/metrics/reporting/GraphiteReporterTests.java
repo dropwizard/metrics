16036191a49159445ec85de6e82db3e64c96478b
Mårten Gustafson <marten.gustafson@gmail.com>
2011-11-19 13:32:29 +0100

    Move common assert logic to separate method

--- metrics-graphite/src/test/java/com/yammer/metrics/reporting/GraphiteReporterTests.java
+++ metrics-graphite/src/test/java/com/yammer/metrics/reporting/GraphiteReporterTests.java
@@ -24,13 +24,13 @@ public class GraphiteReporterTests
     private MetricsRegistry registry;
     private GraphiteReporter reporter;
     private OutputStream out;
-    
+
     @Before
     public void init() throws Exception
     {
         registry = new MetricsRegistry();
         out = new ByteArrayOutputStream();
-        
+
         final Clock clock = mock(Clock.class);
         when(clock.time()).thenReturn(123456L);
 
@@ -40,96 +40,88 @@ public class GraphiteReporterTests
         final SocketProvider provider = mock(SocketProvider.class);
         when(provider.get()).thenReturn(socket);
 
-        GraphiteReporter graphiteReporter = new GraphiteReporter(registry, "prefix", MetricPredicate.ALL, provider, clock);
-        graphiteReporter.printVMMetrics = false;
-        reporter = graphiteReporter;
+        reporter = new GraphiteReporter(registry, "prefix", MetricPredicate.ALL, provider, clock);
+        reporter.printVMMetrics = false;
     }
 
     @Test
     public void canRenderCounter() throws Exception
     {
-        String expected = "prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.count 11 123\n";
+        final String expected = "prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.count 11 123\n";
 
         CounterMetric metric = registry.newCounter(getClass(), "test");
         metric.inc(11);
-        reporter.run();
-        assertEquals(expected, out.toString());
+        assertOutput(expected);
     }
 
     @Test
     public void canRenderHistogram() throws Exception
     {
-        StringBuilder expected = new StringBuilder();
-
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.min 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.max 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.mean 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.stddev 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.median 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.75percentile 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.95percentile 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.98percentile 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.99percentile 10.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.999percentile 10.00 123\n");
+        final String expected = new StringBuilder()
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.min 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.max 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.mean 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.stddev 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.median 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.75percentile 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.95percentile 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.98percentile 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.99percentile 10.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.999percentile 10.00 123\n")
+                .toString();
 
         HistogramMetric metric = registry.newHistogram(getClass(), "test");
         metric.update(10);
 
-        reporter.run();
-
-        assertEquals(expected.toString(), out.toString());
+        assertOutput(expected);
     }
 
     @Test
     public void canRendererTimed() throws Exception
     {
-        StringBuilder expected = new StringBuilder();
-
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.count 0 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.meanRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.1MinuteRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.5MinuteRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.15MinuteRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.min 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.max 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.mean 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.stddev 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.median 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.75percentile 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.95percentile 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.98percentile 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.99percentile 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.999percentile 0.00 123\n");
+        final String expected = new StringBuilder()
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.count 0 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.meanRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.1MinuteRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.5MinuteRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.15MinuteRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.min 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.max 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.mean 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.stddev 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.median 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.75percentile 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.95percentile 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.98percentile 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.99percentile 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.testevent.test.999percentile 0.00 123\n")
+                .toString();
 
         registry.newTimer(getClass(), "test", "testevent");
 
-        reporter.run();
-
-        assertEquals(expected.toString(), out.toString());
+        assertOutput(expected);
     }
 
     @Test
     public void canRendererMetered() throws Exception
     {
-        StringBuilder expected = new StringBuilder();
-
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.count 0 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.meanRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.1MinuteRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.5MinuteRate 0.00 123\n");
-        expected.append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.15MinuteRate 0.00 123\n");
+        final String expected = new StringBuilder()
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.count 0 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.meanRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.1MinuteRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.5MinuteRate 0.00 123\n")
+                .append("prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.15MinuteRate 0.00 123\n")
+                .toString();
 
         registry.newMeter(getClass(), "test", "testevent", TimeUnit.SECONDS);
 
-        reporter.run();
-
-        assertEquals(expected.toString(), out.toString());
+        assertOutput(expected);
     }
 
     @Test
     public void canRendererGauge() throws Exception
     {
-        String expected = "prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.value 5 123\n";
+        final String expected = "prefix.com.yammer.metrics.reporting.GraphiteReporterTests.test.value 5 123\n";
 
         registry.newGauge(getClass(), "test", new GaugeMetric<Long>()
         {
@@ -140,8 +132,11 @@ public class GraphiteReporterTests
             }
         });
 
+        assertOutput(expected);
+    }
+    
+    private void assertOutput(String expected) {
         reporter.run();
-
         assertEquals(expected, out.toString());
     }
 }
