7b5e3f4a7df79ce40091b0ec6706f10dd461dba8
Coda Hale <coda.hale@gmail.com>
2011-12-13 22:44:45 -0800

    Get picky about formatting for metrics-jdbi.

--- metrics-jdbi/src/main/java/com/yammer/metrics/jdbi/strategies/BasicSqlNameStrategy.java
+++ metrics-jdbi/src/main/java/com/yammer/metrics/jdbi/strategies/BasicSqlNameStrategy.java
@@ -1,9 +1,7 @@
 package com.yammer.metrics.jdbi.strategies;
 
-public class BasicSqlNameStrategy extends DelegatingStatementNameStrategy
-{
-    public BasicSqlNameStrategy()
-    {
+public class BasicSqlNameStrategy extends DelegatingStatementNameStrategy {
+    public BasicSqlNameStrategy() {
         super(NameStrategies.CHECK_EMPTY,
               NameStrategies.SQL_OBJECT);
     }
