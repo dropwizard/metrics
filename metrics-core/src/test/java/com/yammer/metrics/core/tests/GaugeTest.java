f318c98845c67669970befb9d87fe67a2050ae72
Coda Hale <coda.hale@gmail.com>
2012-01-02 15:24:36 -0800

    Added test for Gauge processing.

--- metrics-core/src/test/java/com/yammer/metrics/core/tests/GaugeTest.java
+++ metrics-core/src/test/java/com/yammer/metrics/core/tests/GaugeTest.java
@@ -1,10 +1,14 @@
 package com.yammer.metrics.core.tests;
 
 import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.core.MetricsProcessor;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class GaugeTest {
     final Gauge<String> gauge = new Gauge<String>() {
@@ -19,5 +23,17 @@ public class GaugeTest {
         assertThat("a gauge returns a value",
                    gauge.value(),
                    is("woo"));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void isProcessedAsAGauge() throws Exception {
+        final MetricName name = new MetricName(GaugeTest.class, "gauge");
+        final Object context = new Object();
+        final MetricsProcessor<Object> processor = mock(MetricsProcessor.class);
+
+        gauge.processWith(processor, name, context);
+
+        verify(processor).processGauge(name, gauge, context);
     }
 }
