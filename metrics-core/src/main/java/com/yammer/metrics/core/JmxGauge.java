541a15ac73c1ec55d022a4b41bd1c3039701f8d0
Coda Hale <coda.hale@gmail.com>
2012-01-02 15:32:26 -0800

    Added docs and tests for JmxGauge.

--- metrics-core/src/main/java/com/yammer/metrics/core/JmxGauge.java
+++ metrics-core/src/main/java/com/yammer/metrics/core/JmxGauge.java
@@ -1,28 +1,45 @@
 package com.yammer.metrics.core;
 
-import java.lang.management.ManagementFactory;
-
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 
 /**
  * A gauge which exposes an attribute of a JMX MBean.
  */
 public class JmxGauge extends Gauge<Object> {
     private static final MBeanServer SERVER = ManagementFactory.getPlatformMBeanServer();
-    private ObjectName name;
-    private String attribute;
+    private final ObjectName objectName;
+    private final String attribute;
 
-    public JmxGauge(String name, String attribute) throws MalformedObjectNameException {
-        this.name = new ObjectName(name);
+    /**
+     * Creates a new {@link JmxGauge} for the given attribute of the given MBean.
+     *
+     * @param objectName    the string value of the MBean's {@link ObjectName}
+     * @param attribute     the MBean attribute's name
+     *
+     * @throws MalformedObjectNameException if {@code objectName} is malformed
+     */
+    public JmxGauge(String objectName, String attribute) throws MalformedObjectNameException {
+        this(new ObjectName(objectName), attribute);
+    }
+
+    /**
+     * Creates a new {@link JmxGauge} for the given attribute of the given MBean.
+     *
+     * @param objectName    the MBean's {@link ObjectName}
+     * @param attribute     the MBean attribute's name
+     */
+    public JmxGauge(ObjectName objectName, String attribute) {
+        this.objectName = objectName;
         this.attribute = attribute;
     }
 
     @Override
     public Object value() {
         try {
-            return SERVER.getAttribute(name, attribute);
+            return SERVER.getAttribute(objectName, attribute);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
---
+++ metrics-core/src/test/java/com/yammer/metrics/core/tests/JmxGaugeTest.java
@@ -0,0 +1,25 @@
+package com.yammer.metrics.core.tests;
+
+import com.yammer.metrics.core.JmxGauge;
+import org.junit.Before;
+import org.junit.Test;
+
+import static java.lang.management.ManagementFactory.getCompilationMXBean;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JmxGaugeTest {
+    private JmxGauge gauge;
+
+    @Before
+    public void setUp() throws Exception {
+        this.gauge = new JmxGauge("java.lang:type=Compilation",
+                                  "CompilationTimeMonitoringSupported");
+    }
+
+    @Test
+    public void queriesJmxForGaugeValues() throws Exception {
+        assertThat(gauge.value(),
+                   is((Object) getCompilationMXBean().isCompilationTimeMonitoringSupported()));
+    }
+}
