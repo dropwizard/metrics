b4cccc6c907c6797defc8e08539eb076c149cffa
Coda Hale <coda.hale@gmail.com>
2012-01-02 16:45:38 -0800

    Fix Gauge javadocs.

--- metrics-core/src/main/java/com/yammer/metrics/core/Gauge.java
+++ metrics-core/src/main/java/com/yammer/metrics/core/Gauge.java
@@ -5,13 +5,12 @@ package com.yammer.metrics.core;
  * A gauge metric is an instantaneous reading of a particular value. To instrument a queue's depth,
  * for example:<br>
  * <pre><code>
- * Queue<String> queue = new ConcurrentLinkedQueue<String>();
- * Gauge<Integer> queueDepth = new Gauge<Integer>() {
+ * final Queue&lt;String&gt; queue = new ConcurrentLinkedQueue&lt;String&gt;();
+ * final Gauge&lt;Integer&gt; queueDepth = new Gauge&lt;Integer&gt;() {
  *     public Integer value() {
  *         return queue.size();
  *     }
  * };
- * <p/>
  * </code></pre>
  *
  * @param <T> the type of the metric's value
