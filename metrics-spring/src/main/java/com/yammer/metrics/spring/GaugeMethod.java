c53537ed96159109ee4434f72b639bbc09e791b3
Ryan W Tenney <ryan@10e.us>
2012-01-18 15:43:49 -0500

    Rewrote the BeanPostProcessors.

---
+++ metrics-spring/src/main/java/com/yammer/metrics/spring/GaugeMethod.java
@@ -0,0 +1,29 @@
+package com.yammer.metrics.spring;
+
+import java.lang.reflect.Method;
+
+import org.springframework.util.ReflectionUtils;
+
+import com.yammer.metrics.core.Gauge;
+
+public class GaugeMethod extends Gauge<Object> {
+
+	private final Object bean;
+	private final Method method;
+
+	public GaugeMethod(Object bean, Method method) {
+		this.bean = bean;
+		this.method = method;
+	}
+
+	@Override
+	public Object value() {
+		try {
+			return method.invoke(bean);
+		} catch (Exception e) {
+			ReflectionUtils.rethrowRuntimeException(e);
+			return null;
+		}
+	}
+
+}
