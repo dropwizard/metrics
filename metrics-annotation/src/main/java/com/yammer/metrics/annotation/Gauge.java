6c771498baf25eff44acc54f4126fb6d5c3f6332
Coda Hale <coda.hale@gmail.com>
2012-02-07 16:19:16 -0800

    Add some docs to metrics-annotation.

--- metrics-annotation/src/main/java/com/yammer/metrics/annotation/Gauge.java
+++ metrics-annotation/src/main/java/com/yammer/metrics/annotation/Gauge.java
@@ -22,5 +22,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Gauge {
+    /**
+     * The gauge's name.
+     */
     String name() default "";
 }
