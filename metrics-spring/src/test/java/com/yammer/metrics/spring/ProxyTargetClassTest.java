9d17a4b45bf01cfcdde34de089711af7ac284117
Ryan Tenney <ryan@10e.us>
2012-02-20 14:16:44 -0500

    Add a test for the `proxy-target-class` attribute.

---
+++ metrics-spring/src/test/java/com/yammer/metrics/spring/ProxyTargetClassTest.java
@@ -0,0 +1,24 @@
+package com.yammer.metrics.spring;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:proxy-target-class.xml" })
+public class ProxyTargetClassTest {
+
+	@Autowired
+	ProxyTargetClass target;
+
+	@Test
+	public void loads() {
+		System.out.println(target.getClass());
+		Assert.assertNotNull(target);
+	}
+
+}
