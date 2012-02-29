ff11f702550fdad8f92f1a66f55846a43fbda20b
Ryan Tenney <ryan@10e.us>
2012-02-29 14:14:03 -0500

    Update to use new `MetricName.chooseName`, and improve metric creation in `GaugeAnnotationBeanPostProcessor`.

--- metrics-spring/src/main/java/com/yammer/metrics/spring/TimedMethodInterceptor.java
+++ metrics-spring/src/main/java/com/yammer/metrics/spring/TimedMethodInterceptor.java
@@ -48,16 +48,15 @@ public class TimedMethodInterceptor implements MethodInterceptor, MethodCallback
     public void doWith(Method method) throws IllegalArgumentException, IllegalAccessException {
         final Timed timed = method.getAnnotation(Timed.class);
 
-        final String methodName = method.getName();
         final String group = MetricName.chooseGroup(timed.group(), targetClass);
         final String type = MetricName.chooseType(timed.type(), targetClass);
-        final String name = timed.name() == null || timed.name().equals("") ? methodName : timed.name();
+        final String name = MetricName.chooseName(timed.name(), method);
         final MetricName metricName = new MetricName(group, type, name, scope);
 
         final Timer timer = metrics.newTimer(metricName,
                                              timed.durationUnit(),
                                              timed.rateUnit());
-        timers.put(methodName, timer);
+        timers.put(method.getName(), timer);
     }
 
     @Override
