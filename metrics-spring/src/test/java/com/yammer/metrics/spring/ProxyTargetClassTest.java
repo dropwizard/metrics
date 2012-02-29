44dddfaaf4ddfaf14d02ac19be77b456b11204f3
Ryan Tenney <ryan@10e.us>
2012-02-29 15:34:33 -0500

    Modify the ProxyTargetClassTest to avoid component-scanning.
    (Will get messy as the number of tests increases.)

--- metrics-spring/src/test/java/com/yammer/metrics/spring/ProxyTargetClassTest.java
+++ metrics-spring/src/test/java/com/yammer/metrics/spring/ProxyTargetClassTest.java
@@ -21,6 +21,7 @@ public class ProxyTargetClassTest extends Assert {
     public void positiveContextLoadingTest() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:proxy-target-class.xml");
         assertNotNull("Expected to be able to get ProxyTargetClass by class.",ctx.getBean(ProxyTargetClass.class));
+        assertNotNull("Expected to be able to get ProxyTargetClass from AutowiredCollaborator.",ctx.getBean(AutowiredCollaborator.class).getDependency());
 	}
 
 }
--- metrics-spring/src/test/java/com/yammer/metrics/spring/UselessInterface.java
+++ metrics-spring/src/test/java/com/yammer/metrics/spring/UselessInterface.java
@@ -3,5 +3,4 @@ package com.yammer.metrics.spring;
 /**
  * Empty interface to trick Spring.
  */
-public interface UselessInterface {
-}
+public interface UselessInterface {}
