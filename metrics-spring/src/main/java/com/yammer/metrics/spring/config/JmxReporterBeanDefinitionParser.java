391359253160b537f62c0d4790bc1e2e432813ba
Ryan Tenney <ryan@10e.us>
2012-03-01 16:16:21 -0500

    Revert "If jmx-reporter isn't provided with a metrics registry, use the default."
    This reverts commit 6191e22cd675e3af20ade9fa20131ca2d5e7180a.
    It turns out a JmxReporter is always created for the default metrics registry.

--- metrics-spring/src/main/java/com/yammer/metrics/spring/config/JmxReporterBeanDefinitionParser.java
+++ metrics-spring/src/main/java/com/yammer/metrics/spring/config/JmxReporterBeanDefinitionParser.java
@@ -2,10 +2,8 @@ package com.yammer.metrics.spring.config;
 
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
-import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
-import com.yammer.metrics.Metrics;
 import com.yammer.metrics.spring.JmxReporterFactory;
 
 public class JmxReporterBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
@@ -23,12 +21,7 @@ public class JmxReporterBeanDefinitionParser extends AbstractSingleBeanDefinitio
     @Override
     protected void doParse(Element element, BeanDefinitionBuilder builder) {
         builder.setFactoryMethod("createInstance");
-        String registry = element.getAttribute("metrics-registry");
-        if (StringUtils.hasText(registry)) {
-        	builder.addConstructorArgReference(registry);
-        } else {
-        	builder.addConstructorArgValue(Metrics.defaultRegistry());
-        }
+        builder.addConstructorArgReference(element.getAttribute("metrics-registry"));
     }
 
 }
