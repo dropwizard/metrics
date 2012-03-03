e1777de3b8e85103e524dc2eb8d8c1bf875de6ed
Ryan Tenney <ryan@10e.us>
2012-03-03 18:03:17 -0500

    Add `clock` attribute to `<metrics-registry/>` element (+ a test)

--- metrics-spring/src/main/java/com/yammer/metrics/spring/config/MetricsRegistryBeanDefinitionParser.java
+++ metrics-spring/src/main/java/com/yammer/metrics/spring/config/MetricsRegistryBeanDefinitionParser.java
@@ -1,6 +1,8 @@
 package com.yammer.metrics.spring.config;
 
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
 import com.yammer.metrics.core.MetricsRegistry;
@@ -10,6 +12,14 @@ public class MetricsRegistryBeanDefinitionParser extends AbstractSingleBeanDefin
     @Override
     protected Class<?> getBeanClass(Element element) {
         return MetricsRegistry.class;
+    }
+
+    @Override
+    protected void doParse(Element element, BeanDefinitionBuilder builder) {
+        String clock = element.getAttribute("clock");
+        if (StringUtils.hasText(clock)) {
+            builder.addConstructorArgReference(clock);
+        }
     }
 
 }
