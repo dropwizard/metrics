a845ba27e2d8e27e501681a290608dfb61b173a9
Coda Hale <coda.hale@gmail.com>
2012-03-13 14:59:37 -0700

    Fixed InstrumentedHttpClient.
    It will now measure any and all HTTP requests, not just those done via the non-final methods.

--- metrics-httpclient/src/main/java/com/yammer/metrics/httpclient/InstrumentedHttpClient.java
+++ metrics-httpclient/src/main/java/com/yammer/metrics/httpclient/InstrumentedHttpClient.java
@@ -1,35 +1,19 @@
 package com.yammer.metrics.httpclient;
 
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.TimerContext;
-import com.yammer.metrics.core.Timer;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.ConnectionReuseStrategy;
+import org.apache.http.client.*;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HttpContext;
-
-import java.io.IOException;
+import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpRequestExecutor;
 
 public class InstrumentedHttpClient extends DefaultHttpClient {
-    private final static String GET = "GET", POST = "POST", HEAD = "HEAD", PUT = "PUT",
-                                OPTIONS = "OPTIONS", DELETE = "DELETE", TRACE = "TRACE",
-                                CONNECT = "CONNECT", MOVE = "MOVE", PATCH = "PATCH";
-
-    private static final Timer GET_TIMER = Metrics.newTimer(HttpClient.class, "get-requests");
-    private static final Timer POST_TIMER = Metrics.newTimer(HttpClient.class, "post-requests");
-    private static final Timer HEAD_TIMER = Metrics.newTimer(HttpClient.class, "head-requests");
-    private static final Timer PUT_TIMER = Metrics.newTimer(HttpClient.class, "put-requests");
-    private static final Timer DELETE_TIMER = Metrics.newTimer(HttpClient.class, "delete-requests");
-    private static final Timer OPTIONS_TIMER = Metrics.newTimer(HttpClient.class, "options-requests");
-    private static final Timer TRACE_TIMER = Metrics.newTimer(HttpClient.class, "trace-requests");
-    private static final Timer CONNECT_TIMER = Metrics.newTimer(HttpClient.class, "connect-requests");
-    private static final Timer MOVE_TIMER = Metrics.newTimer(HttpClient.class, "move-requests");
-    private static final Timer PATCH_TIMER = Metrics.newTimer(HttpClient.class, "patch-requests");
-    private static final Timer OTHER_TIMER = Metrics.newTimer(HttpClient.class, "other-requests");
+    private final Log log = LogFactory.getLog(getClass());
 
     public InstrumentedHttpClient(InstrumentedClientConnManager manager, HttpParams params) {
         super(manager, params);
@@ -44,77 +28,31 @@ public class InstrumentedHttpClient extends DefaultHttpClient {
     }
 
     @Override
-    public <T> T execute(HttpUriRequest request,
-                         ResponseHandler<? extends T> responseHandler) throws IOException {
-        final TimerContext context = timer(request).time();
-        try {
-            return super.execute(request, responseHandler);
-        } finally {
-            context.stop();
-        }
-    }
-
-    @Override
-    public <T> T execute(HttpUriRequest request,
-                         ResponseHandler<? extends T> responseHandler,
-                         HttpContext ctxt) throws IOException {
-        final TimerContext context = timer(request).time();
-        try {
-            return super.execute(request, responseHandler, ctxt);
-        } finally {
-            context.stop();
-        }
-    }
-
-    @Override
-    public <T> T execute(HttpHost target,
-                         HttpRequest request,
-                         ResponseHandler<? extends T> responseHandler) throws IOException {
-        final TimerContext context = timer(request).time();
-        try {
-            return super.execute(target, request, responseHandler);
-        } finally {
-            context.stop();
-        }
-    }
-
-    @Override
-    public <T> T execute(HttpHost target,
-                         HttpRequest request,
-                         ResponseHandler<? extends T> responseHandler,
-                         HttpContext ctxt) throws IOException {
-        final TimerContext context = timer(request).time();
-        try {
-            return super.execute(target, request, responseHandler, ctxt);
-        } finally {
-            context.stop();
-        }
-
-    }
-
-    private Timer timer(HttpRequest request) {
-        final String method = request.getRequestLine().getMethod();
-        if (GET.equalsIgnoreCase(method)) {
-            return GET_TIMER;
-        } else if (POST.equalsIgnoreCase(method)) {
-            return POST_TIMER;
-        } else if (PUT.equalsIgnoreCase(method)) {
-            return PUT_TIMER;
-        } else if (HEAD.equalsIgnoreCase(method)) {
-            return HEAD_TIMER;
-        } else if (DELETE.equalsIgnoreCase(method)) {
-            return DELETE_TIMER;
-        } else if (OPTIONS.equalsIgnoreCase(method)) {
-            return OPTIONS_TIMER;
-        } else if (TRACE.equalsIgnoreCase(method)) {
-            return TRACE_TIMER;
-        } else if (CONNECT.equalsIgnoreCase(method)) {
-            return CONNECT_TIMER;
-        } else if (PATCH.equalsIgnoreCase(method)) {
-            return PATCH_TIMER;
-        } else if (MOVE.equalsIgnoreCase(method)) {
-            return MOVE_TIMER;
-        }
-        return OTHER_TIMER;
+    protected RequestDirector createClientRequestDirector(HttpRequestExecutor requestExec,
+                                                          ClientConnectionManager conman,
+                                                          ConnectionReuseStrategy reustrat,
+                                                          ConnectionKeepAliveStrategy kastrat,
+                                                          HttpRoutePlanner rouplan,
+                                                          HttpProcessor httpProcessor,
+                                                          HttpRequestRetryHandler retryHandler,
+                                                          RedirectStrategy redirectStrategy,
+                                                          AuthenticationHandler targetAuthHandler,
+                                                          AuthenticationHandler proxyAuthHandler,
+                                                          UserTokenHandler stateHandler,
+                                                          HttpParams params) {
+        return new InstrumentedRequestDirector(
+                log,
+                requestExec,
+                conman,
+                reustrat,
+                kastrat,
+                rouplan,
+                httpProcessor,
+                retryHandler,
+                redirectStrategy,
+                targetAuthHandler,
+                proxyAuthHandler,
+                stateHandler,
+                params);
     }
 }
