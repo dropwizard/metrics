1a3368eb63bfdf3720e277f8fbd514ab0d46b9a7
Coda Hale <coda.hale@gmail.com>
2012-04-17 13:10:42 -0700

    Clean up ExponentiallyDecayingSample and its tests.

--- metrics-core/src/test/java/com/yammer/metrics/stats/tests/ExponentiallyDecayingSampleTest.java
+++ metrics-core/src/test/java/com/yammer/metrics/stats/tests/ExponentiallyDecayingSampleTest.java
@@ -1,20 +1,14 @@
 package com.yammer.metrics.stats.tests;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.lessThan;
-import static org.junit.Assert.assertThat;
-
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
-
 import com.yammer.metrics.core.Clock;
 import com.yammer.metrics.stats.ExponentiallyDecayingSample;
 import com.yammer.metrics.stats.Snapshot;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class ExponentiallyDecayingSampleTest {
     @Test
@@ -82,7 +76,6 @@ public class ExponentiallyDecayingSampleTest {
         }
 
 
-
         assertThat("the sample has a size of 100",
                    sample.size(),
                    is(100));
@@ -102,100 +95,78 @@ public class ExponentiallyDecayingSampleTest {
                        )));
         }
     }
-    
+
     @Test
     public void longPeriodsOfInactivityShouldNotCorruptSamplingState() {
-    	ManualClock clock = new ManualClock();
-    	 final ExponentiallyDecayingSample sample = new ExponentiallyDecayingSample(10, 0.015,clock);
-        
-    	 // add 1000 values at a rate of 10 values/second
-    	 for (int i = 0; i < 1000; i++) {
-             sample.update(1000+i);
-             clock.addMillis(100);
-         }
-         debugPrintValues(sample.getValues());
-    	 assertThat("the sample has 10 elements", sample.getSnapshot().size(), is(10));
-       	 assertAllValuesBetween(sample,1000,2000);
+        final ManualClock clock = new ManualClock();
+        final ExponentiallyDecayingSample sample = new ExponentiallyDecayingSample(10,
+                                                                                   0.015,
+                                                                                   clock);
 
-    	 // wait for 15 hours and add another value.
-    	 // this should trigger a rescale. Note that the number of samples will be reduced to 2 
-    	 // because of the very small scaling factor that will make all existing priorities equal to 
-    	 // zero after rescale.
-         clock.addHours(15);
-         sample.update(2000);
-         debugPrintValues(sample.getValues());
-    	 assertThat("the sample has 2 elements", sample.getSnapshot().size(), is(2));         
-    	 assertAllValuesBetween(sample,1000,3000);
+        // add 1000 values at a rate of 10 values/second
+        for (int i = 0; i < 1000; i++) {
+            sample.update(1000 + i);
+            clock.addMillis(100);
+        }
+        assertThat("the sample has 10 elements", sample.getSnapshot().size(), is(10));
+        assertAllValuesBetween(sample, 1000, 2000);
 
-    	 
-    	 // add 1000 values at a rate of 10 values/second
-    	 for (int i = 0; i < 1000; i++) {
-             sample.update(3000+i);
-             clock.addMillis(100);
-         }
-         debugPrintValues(sample.getValues());
-    	 assertThat("the sample has 10 elements", sample.getSnapshot().size(), is(10));
-    	 assertAllValuesBetween(sample,3000,4000);
-    	 
-    	    
+        // wait for 15 hours and add another value.
+        // this should trigger a rescale. Note that the number of samples will be reduced to 2
+        // because of the very small scaling factor that will make all existing priorities equal to
+        // zero after rescale.
+        clock.addHours(15);
+        sample.update(2000);
+        assertThat("the sample has 2 elements", sample.getSnapshot().size(), is(2));
+        assertAllValuesBetween(sample, 1000, 3000);
+
+
+        // add 1000 values at a rate of 10 values/second
+        for (int i = 0; i < 1000; i++) {
+            sample.update(3000 + i);
+            clock.addMillis(100);
+        }
+        assertThat("the sample has 10 elements", sample.getSnapshot().size(), is(10));
+        assertAllValuesBetween(sample, 3000, 4000);
+
+
     }
-    
+
+    @SuppressWarnings("unchecked")
     private void assertAllValuesBetween(ExponentiallyDecayingSample sample,
-			double min, double max) {
-    	 for (double i : sample.getSnapshot().getValues()) {
-             assertThat("the sample only contains elements from the population",
-                        i,
-                        is(allOf(
-                                lessThan(max),
-                                greaterThanOrEqualTo(min)
-                        )));
-         }
-		
-	}
+                                        double min, double max) {
+        for (double i : sample.getSnapshot().getValues()) {
+            assertThat("the sample only contains elements from the population",
+                       i,
+                       is(allOf(
+                               lessThan(max),
+                               greaterThanOrEqualTo(min)
+                       )));
+        }
 
-	private void debugPrintValues(Map<Double, Long> values) {
-    	System.out.println("-----------------");
-    	for (Entry<Double, Long> e: values.entrySet()) {
-			System.out.println(e);
-		}
-      	System.out.println("-----------------");
-        	
-	}
-
-	class ManualClock extends Clock {
-
-    	long ticksInNanos = 0;
-    	
-    	public void addMillis(long millis) {
-    		ticksInNanos += TimeUnit.MILLISECONDS.toNanos(millis);
-    	}
-   
-    	public void addSeconds(long seconds) {
-    		ticksInNanos += TimeUnit.SECONDS.toNanos(seconds);
-    	}
-    	
-    	public void addMinutes(long minutes) {
-    		ticksInNanos += TimeUnit.MINUTES.toNanos(minutes);
-    	}
-
-    	public void addHours(long hours) {
-    		ticksInNanos += TimeUnit.HOURS.toNanos(hours);
-    	}
-
-    	public void addDays(long days) {
-    		ticksInNanos += TimeUnit.DAYS.toNanos(days);
-    	}
-
-    	@Override
-		public long tick() {
-			return ticksInNanos;
-		}
-		
-		@Override
-		public long time() {
-			return TimeUnit.NANOSECONDS.toMillis(ticksInNanos);
-		}
-    	
     }
-    
+
+    class ManualClock extends Clock {
+        long ticksInNanos = 0;
+
+        public void addMillis(long millis) {
+            ticksInNanos += TimeUnit.MILLISECONDS.toNanos(millis);
+        }
+
+        public void addHours(long hours) {
+            ticksInNanos += TimeUnit.HOURS.toNanos(hours);
+        }
+
+        @Override
+        public long tick() {
+            return ticksInNanos;
+        }
+
+        @Override
+        public long time() {
+            return TimeUnit.NANOSECONDS.toMillis(ticksInNanos);
+        }
+
+    }
+
 }
