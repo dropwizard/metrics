898168a2dab3027bab59f7b24c26b08b1ed4c98b
Michal Minicki <martel@post.pl>
2012-05-02 10:03:22 +0200

    Preliminary Hibernate statistics support

---
+++ contribs/metrics-hibernate/src/test/java/com/yammer/metrics/hibernate/HibernateStatisticsCollectorTest.java
@@ -0,0 +1,35 @@
+package com.yammer.metrics.hibernate;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Gauge;
+import org.hibernate.stat.Statistics;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class HibernateStatisticsCollectorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+        Statistics statistics = Mockito.mock(Statistics.class);
+        Mockito.when(statistics.getQueryExecutionCount()).thenReturn(125L);
+
+        HibernateStatisticsCollector.register(statistics);
+
+    }
+
+    @Test
+    public void measuresQueryExecutionCount() throws Exception {
+
+        final Gauge gauge = Metrics.newGauge(Statistics.class, "query-execution-count", new Gauge<Long>() {
+            public Long getValue() { return -1L; }
+        });
+
+        assertEquals(gauge.getValue(), 125L);
+
+    }
+
+}
