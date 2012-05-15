b52f815ede4e3a3161e05da25511718dca64be7f
Michał Minicki <martel@post.pl>
2012-05-15 17:42:58 +0200

    HealthCheck Registry/Runner refactoring

--- metrics-core/src/main/java/com/yammer/metrics/core/HealthCheckRegistry.java
+++ metrics-core/src/main/java/com/yammer/metrics/core/HealthCheckRegistry.java
@@ -2,10 +2,7 @@ package com.yammer.metrics.core;
 
 import com.yammer.metrics.core.HealthCheck.Result;
 
-import java.util.Collections;
-import java.util.Map.Entry;
 import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -43,16 +40,32 @@ public class HealthCheckRegistry {
     }
 
     /**
+     * Returns registered health check count
+     *
+     * @return size of internal registry
+     */
+    public int size() {
+        return healthChecks.size();
+    }
+
+    /**
+     * Returns {@link HealthCheckRunner} Runner
+     *
+     * This implementation creates the default {@link SequentialHealthCheckRunner}
+     *
+     * @return health check runner
+     */
+    protected HealthCheckRunner getHealthCheckRunner() {
+        return new SequentialHealthCheckRunner(healthChecks);
+    }
+
+    /**
      * Runs the registered health checks and returns a map of the results.
      *
      * @return a map of the health check results
      */
     public SortedMap<String, Result> runHealthChecks() {
-        final SortedMap<String, Result> results = new TreeMap<String, Result>();
-        for (Entry<String, HealthCheck> entry : healthChecks.entrySet()) {
-            final Result result = entry.getValue().execute();
-            results.put(entry.getKey(), result);
-        }
-        return Collections.unmodifiableSortedMap(results);
+        return getHealthCheckRunner().run();
     }
+
 }
