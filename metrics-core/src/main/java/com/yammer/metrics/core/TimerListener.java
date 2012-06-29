e4d3f30eba66cf289c3f87b4093a20bf180203bb
Josh Devins <josh@soundcloud.com>
2012-06-29 12:00:26 +0200

    Formatting fixes

--- metrics-core/src/main/java/com/yammer/metrics/core/TimerListener.java
+++ metrics-core/src/main/java/com/yammer/metrics/core/TimerListener.java
@@ -7,16 +7,16 @@ import java.util.concurrent.TimeUnit;
  */
 public interface TimerListener extends MetricListener {
 
-	/**
-	 * Called after any of timing events, {@link Timer#update(long, TimeUnit)}
-	 * or {@link Timer#time(java.util.concurrent.Callable)}.
-	 * 
-	 * @param timer
-	 *            the {@link Timer} whose state has changed
-	 * @param duration
-	 *            the length of the duration
-	 * @param unit
-	 *            the scale unit of {@code duration}
-	 */
-	void onUpdate(Timer timer, long duration, TimeUnit unit);
+    /**
+     * Called after any of timing events, {@link Timer#update(long, TimeUnit)}
+     * or {@link Timer#time(java.util.concurrent.Callable)}.
+     * 
+     * @param timer
+     *        the {@link Timer} whose state has changed
+     * @param duration
+     *        the length of the duration
+     * @param unit
+     *        the scale unit of {@code duration}
+     */
+    void onUpdate(Timer timer, long duration, TimeUnit unit);
 }
