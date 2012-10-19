90c4d929ad052d2d3b39581328bbb8f7de3f00c3
Coda Hale <coda.hale@gmail.com>
2012-10-19 14:02:37 -0700

    Drop the health check singleton methods.

--- metrics-core/src/main/java/com/yammer/metrics/HealthChecks.java
+++ metrics-core/src/main/java/com/yammer/metrics/HealthChecks.java
@@ -1,36 +1,14 @@
 package com.yammer.metrics;
 
-import com.yammer.metrics.core.HealthCheck;
-import com.yammer.metrics.core.HealthCheck.Result;
 import com.yammer.metrics.core.HealthCheckRegistry;
 
-import java.util.Map;
-
 /**
- * A manager class for health checks.
+ * A default health check registry.
  */
 public class HealthChecks {
     private static final HealthCheckRegistry DEFAULT_REGISTRY = new HealthCheckRegistry();
 
     private HealthChecks() { /* unused */ }
-
-    /**
-     * Registers an application {@link HealthCheck} with a given name.
-     *
-     * @param healthCheck the {@link HealthCheck} instance
-     */
-    public static void register(HealthCheck healthCheck) {
-        DEFAULT_REGISTRY.register(healthCheck);
-    }
-
-    /**
-     * Runs the registered health checks and returns a map of the results.
-     *
-     * @return a map of the health check results
-     */
-    public static Map<String, Result> runHealthChecks() {
-        return DEFAULT_REGISTRY.runHealthChecks();
-    }
 
     /**
      * Returns the (static) default registry.
