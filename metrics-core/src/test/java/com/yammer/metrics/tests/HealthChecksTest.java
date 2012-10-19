2991f905fe7d4a1bd6e3769a8fea50942c91057c
Coda Hale <coda.hale@gmail.com>
2012-10-19 14:02:37 -0700

    Drop the health check singleton methods.

--- metrics-core/src/test/java/com/yammer/metrics/tests/HealthChecksTest.java
+++ metrics-core/src/test/java/com/yammer/metrics/tests/HealthChecksTest.java
@@ -8,11 +8,12 @@ import org.junit.Test;
 
 import java.util.Map;
 
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class HealthChecksTest {
+    private final HealthCheckRegistry registry = HealthChecks.defaultRegistry();
+
     private static class ExampleHealthCheck extends HealthCheck {
         private ExampleHealthCheck() {
             super("example");
@@ -26,20 +27,14 @@ public class HealthChecksTest {
 
     @Before
     public void setUp() throws Exception {
-        HealthChecks.register(new ExampleHealthCheck());
+        registry.register(new ExampleHealthCheck());
     }
 
     @Test
     public void runsRegisteredHealthChecks() throws Exception {
-        final Map<String, HealthCheck.Result> results = HealthChecks.runHealthChecks();
+        final Map<String, HealthCheck.Result> results = registry.runHealthChecks();
 
         assertThat(results.get("example"),
                    is(HealthCheck.Result.healthy("whee")));
-    }
-
-    @Test
-    public void hasADefaultRegistry() throws Exception {
-        assertThat(HealthChecks.defaultRegistry(),
-                   is(instanceOf(HealthCheckRegistry.class)));
     }
 }
