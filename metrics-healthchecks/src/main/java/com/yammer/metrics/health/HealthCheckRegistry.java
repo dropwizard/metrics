4d916c712cc554a0644ff473cc832bae49511776
Coda Hale <coda.hale@gmail.com>
2013-03-10 12:57:22 -0700

    Added metrics-healthchecks.

---
+++ metrics-healthchecks/src/main/java/com/yammer/metrics/health/HealthCheckRegistry.java
@@ -0,0 +1,85 @@
+package com.yammer.metrics.health;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+import static com.yammer.metrics.health.HealthCheck.Result;
+
+/**
+ * A registry for health checks.
+ */
+public class HealthCheckRegistry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckRegistry.class);
+
+    private final ConcurrentMap<String, HealthCheck> healthChecks;
+
+    /**
+     * Creates a new {@link HealthCheckRegistry}.
+     */
+    public HealthCheckRegistry() {
+        this.healthChecks = new ConcurrentHashMap<String, HealthCheck>();
+    }
+
+    /**
+     * Registers an application {@link HealthCheck}.
+     *
+     * @param name        the name of the health check
+     * @param healthCheck the {@link HealthCheck} instance
+     */
+    public void register(String name, HealthCheck healthCheck) {
+        healthChecks.putIfAbsent(name, healthCheck);
+    }
+
+    /**
+     * Unregisters the application {@link HealthCheck} with the given name.
+     *
+     * @param name the name of the {@link HealthCheck} instance
+     */
+    public void unregister(String name) {
+        healthChecks.remove(name);
+    }
+
+    /**
+     * Runs the registered health checks and returns a map of the results.
+     *
+     * @return a map of the health check results
+     */
+    public SortedMap<String, HealthCheck.Result> runHealthChecks() {
+        final SortedMap<String, HealthCheck.Result> results = new TreeMap<String, HealthCheck.Result>();
+        for (Map.Entry<String, HealthCheck> entry : healthChecks.entrySet()) {
+            final Result result = entry.getValue().execute();
+            results.put(entry.getKey(), result);
+        }
+        return Collections.unmodifiableSortedMap(results);
+    }
+
+    /**
+     * Runs the registered health checks in parallel and returns a map of the results.
+     *
+     * @return a map of the health check results
+     */
+    public SortedMap<String, HealthCheck.Result> runHealthChecks(ExecutorService executor) {
+        final Map<String, Future<HealthCheck.Result>> futures = new HashMap<String, Future<Result>>();
+        for (final Map.Entry<String, HealthCheck> entry : healthChecks.entrySet()) {
+            futures.put(entry.getKey(), executor.submit(new Callable<Result>() {
+                @Override
+                public Result call() throws Exception {
+                    return entry.getValue().execute();
+                }
+            }));
+        }
+
+        final SortedMap<String, HealthCheck.Result> results = new TreeMap<String, HealthCheck.Result>();
+        for (Map.Entry<String, Future<Result>> entry : futures.entrySet()) {
+            try {
+                results.put(entry.getKey(), entry.getValue().get());
+            } catch (Exception e) {
+                LOGGER.warn("Error executing health check {}", entry.getKey(), e);
+            }
+        }
+        return Collections.unmodifiableSortedMap(results);
+    }
+}
