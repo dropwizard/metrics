8f5366451c6cd610f78abdada83f33a33e5d5e34
Coda Hale <coda.hale@gmail.com>
2013-03-12 09:56:01 -0700

    Revamped metrics-log4j.

--- metrics-log4j/src/main/java/com/yammer/metrics/log4j/InstrumentedAppender.java
+++ metrics-log4j/src/main/java/com/yammer/metrics/log4j/InstrumentedAppender.java
@@ -1,14 +1,13 @@
 package com.yammer.metrics.log4j;
 
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Meter;
-import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.Meter;
+import com.yammer.metrics.MetricRegistry;
 import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
-import java.util.concurrent.TimeUnit;
+import static com.yammer.metrics.MetricRegistry.name;
 
 /**
  * A Log4J {@link Appender} delegate which has seven meters, one for each logging level and one for
@@ -23,19 +22,19 @@ public class InstrumentedAppender extends AppenderSkeleton {
     private final Meter error;
     private final Meter fatal;
 
-    public InstrumentedAppender() {
-        this(Metrics.defaultRegistry());
+    public InstrumentedAppender(MetricRegistry registry) {
+        this(registry, null);
     }
 
-    public InstrumentedAppender(MetricsRegistry registry) {
+    public InstrumentedAppender(MetricRegistry registry, String name) {
         super();
-        this.all = registry.newMeter(Appender.class, "all", "statements", TimeUnit.SECONDS);
-        this.trace = registry.newMeter(Appender.class, "trace", "statements", TimeUnit.SECONDS);
-        this.debug = registry.newMeter(Appender.class, "debug", "statements", TimeUnit.SECONDS);
-        this.info = registry.newMeter(Appender.class, "info", "statements", TimeUnit.SECONDS);
-        this.warn = registry.newMeter(Appender.class, "warn", "statements", TimeUnit.SECONDS);
-        this.error = registry.newMeter(Appender.class, "error", "statements", TimeUnit.SECONDS);
-        this.fatal = registry.newMeter(Appender.class, "fatal", "statements", TimeUnit.SECONDS);
+        this.all = registry.meter(name(Appender.class, name, "all"));
+        this.trace = registry.meter(name(Appender.class, name, "trace"));
+        this.debug = registry.meter(name(Appender.class, name, "debug"));
+        this.info = registry.meter(name(Appender.class, name, "info"));
+        this.warn = registry.meter(name(Appender.class, name, "warn"));
+        this.error = registry.meter(name(Appender.class, name, "error"));
+        this.fatal = registry.meter(name(Appender.class, name, "fatal"));
     }
 
     @Override
