b273690dd54240dbe41fea8a719837eb34138a83
Coda Hale <coda.hale@gmail.com>
2013-03-12 11:40:37 -0700

    Added metrics-json.

---
+++ metrics-json/src/main/java/com/yammer/metrics/json/MetricsModule.java
@@ -0,0 +1,224 @@
+package com.yammer.metrics.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleSerializers;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.yammer.metrics.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MetricsModule extends Module {
+    private static final Version VERSION = new Version(3, 0, 0, "", "com.yammer.metrics", "metrics-json");
+
+    private static class GaugeSerializer extends StdSerializer<Gauge> {
+        private GaugeSerializer() {
+            super(Gauge.class);
+        }
+
+        @Override
+        public void serialize(Gauge gauge,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            final Object value = gauge.getValue();
+            json.writeStartObject();
+            json.writeObjectField("value", value);
+            json.writeEndObject();
+        }
+    }
+
+    private static class CounterSerializer extends StdSerializer<Counter> {
+        private CounterSerializer() {
+            super(Counter.class);
+        }
+
+        @Override
+        public void serialize(Counter counter,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            json.writeStartObject();
+            json.writeNumberField("count", counter.getCount());
+            json.writeEndObject();
+        }
+    }
+
+    private static class HistogramSerializer extends StdSerializer<Histogram> {
+        private final boolean showSamples;
+
+        private HistogramSerializer(boolean showSamples) {
+            super(Histogram.class);
+            this.showSamples = showSamples;
+        }
+
+        @Override
+        public void serialize(Histogram histogram,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            json.writeStartObject();
+            json.writeNumberField("count", histogram.getCount());
+            json.writeNumberField("max", histogram.getMax());
+            json.writeNumberField("mean", histogram.getMean());
+            json.writeNumberField("min", histogram.getMin());
+
+            final Snapshot snapshot = histogram.getSnapshot();
+            json.writeNumberField("p50", snapshot.getMedian());
+            json.writeNumberField("p75", snapshot.get75thPercentile());
+            json.writeNumberField("p95", snapshot.get95thPercentile());
+            json.writeNumberField("p98", snapshot.get98thPercentile());
+            json.writeNumberField("p99", snapshot.get99thPercentile());
+            json.writeNumberField("p999", snapshot.get999thPercentile());
+
+            if (showSamples) {
+                json.writeObjectField("values", snapshot.getValues());
+            }
+
+            json.writeNumberField("stddev", histogram.getStdDev());
+            json.writeEndObject();
+        }
+    }
+
+    private static class MeterSerializer extends StdSerializer<Meter> {
+        private final String rateUnit;
+        private final double rateFactor;
+
+        public MeterSerializer(TimeUnit rateUnit) {
+            super(Meter.class);
+            this.rateFactor = rateUnit.toSeconds(1);
+            this.rateUnit = calculateRateUnit(rateUnit, "events");
+        }
+
+        @Override
+        public void serialize(Meter meter,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            json.writeStartObject();
+            json.writeNumberField("count", meter.getCount());
+            json.writeNumberField("m15_rate", meter.getOneMinuteRate() * rateFactor);
+            json.writeNumberField("m1_rate", meter.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m5_rate", meter.getFiveMinuteRate() * rateFactor);
+            json.writeNumberField("mean_rate", meter.getMeanRate() * rateFactor);
+            json.writeStringField("units", rateUnit);
+            json.writeEndObject();
+        }
+    }
+
+    private static class TimerSerializer extends StdSerializer<Timer> {
+        private final String rateUnit;
+        private final double rateFactor;
+        private final String durationUnit;
+        private final double durationFactor;
+        private final boolean showSamples;
+
+        private TimerSerializer(TimeUnit rateUnit,
+                                TimeUnit durationUnit,
+                                boolean showSamples) {
+            super(Timer.class);
+            this.rateUnit = calculateRateUnit(rateUnit, "calls");
+            this.rateFactor = rateUnit.toSeconds(1);
+            this.durationUnit = durationUnit.toString().toLowerCase(Locale.US);
+            this.durationFactor = 1.0 / durationUnit.toNanos(1);
+            this.showSamples = showSamples;
+        }
+
+        @Override
+        public void serialize(Timer timer,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            json.writeStartObject();
+            json.writeNumberField("count", timer.getCount());
+            json.writeNumberField("max", timer.getMax() * durationFactor);
+            json.writeNumberField("mean", timer.getMean() * durationFactor);
+            json.writeNumberField("min", timer.getMin() * durationFactor);
+
+            final Snapshot snapshot = timer.getSnapshot();
+            json.writeNumberField("p50", snapshot.getMedian() * durationFactor);
+            json.writeNumberField("p75", snapshot.get75thPercentile() * durationFactor);
+            json.writeNumberField("p95", snapshot.get95thPercentile() * durationFactor);
+            json.writeNumberField("p98", snapshot.get98thPercentile() * durationFactor);
+            json.writeNumberField("p99", snapshot.get99thPercentile() * durationFactor);
+            json.writeNumberField("p999", snapshot.get999thPercentile() * durationFactor);
+
+            if (showSamples) {
+                final long[] values = snapshot.getValues();
+                final double[] scaledValues = new double[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    scaledValues[i] = values[i] * durationFactor;
+                }
+                json.writeObjectField("values", scaledValues);
+            }
+
+            json.writeNumberField("stddev", timer.getStdDev() * durationFactor);
+            json.writeNumberField("m15_rate", timer.getOneMinuteRate() * rateFactor);
+            json.writeNumberField("m1_rate", timer.getFifteenMinuteRate() * rateFactor);
+            json.writeNumberField("m5_rate", timer.getFiveMinuteRate() * rateFactor);
+            json.writeNumberField("mean_rate", timer.getMeanRate() * rateFactor);
+            json.writeStringField("duration_units", durationUnit);
+            json.writeStringField("rate_units", rateUnit);
+            json.writeEndObject();
+        }
+    }
+
+    private static class MetricRegistrySerializer extends StdSerializer<MetricRegistry> {
+        private MetricRegistrySerializer() {
+            super(MetricRegistry.class);
+        }
+
+        @Override
+        public void serialize(MetricRegistry registry,
+                              JsonGenerator json,
+                              SerializerProvider provider) throws IOException {
+            json.writeStartObject();
+            json.writeStringField("name", registry.getName());
+            json.writeStringField("version", VERSION.toString());
+            json.writeObjectField("gauges", registry.getGauges());
+            json.writeObjectField("counters", registry.getCounters());
+            json.writeObjectField("histograms", registry.getHistograms());
+            json.writeObjectField("meters", registry.getMeters());
+            json.writeObjectField("timers", registry.getTimers());
+            json.writeEndObject();
+        }
+    }
+
+    private final TimeUnit rateUnit;
+    private final TimeUnit durationUnit;
+    private final boolean showSamples;
+
+    public MetricsModule(TimeUnit rateUnit, TimeUnit durationUnit, boolean showSamples) {
+        this.rateUnit = rateUnit;
+        this.durationUnit = durationUnit;
+        this.showSamples = showSamples;
+    }
+
+    @Override
+    public String getModuleName() {
+        return "metrics";
+    }
+
+    @Override
+    public Version version() {
+        return VERSION;
+    }
+
+    @Override
+    public void setupModule(SetupContext context) {
+        context.addSerializers(new SimpleSerializers(Arrays.<JsonSerializer<?>>asList(
+                new GaugeSerializer(),
+                new CounterSerializer(),
+                new HistogramSerializer(showSamples),
+                new MeterSerializer(rateUnit),
+                new TimerSerializer(rateUnit, durationUnit, showSamples),
+                new MetricRegistrySerializer()
+        )));
+    }
+
+    private static String calculateRateUnit(TimeUnit unit, String name) {
+        final String s = unit.toString().toLowerCase(Locale.US);
+        return name + '/' + s.substring(0, s.length() - 1);
+    }
+}
---
+++ metrics-json/src/test/java/com/yammer/metrics/json/tests/MetricsModuleTest.java
@@ -0,0 +1,201 @@
+package com.yammer.metrics.json.tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yammer.metrics.*;
+import com.yammer.metrics.json.MetricsModule;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MetricsModuleTest {
+    private final ObjectMapper mapper = new ObjectMapper().registerModule(
+            new MetricsModule(TimeUnit.SECONDS, TimeUnit.MILLISECONDS, false));
+
+    @Test
+    public void serializesGauges() throws Exception {
+        final Gauge<Integer> gauge = new Gauge<Integer>() {
+            @Override
+            public Integer getValue() {
+                return 100;
+            }
+        };
+
+        assertThat(mapper.writeValueAsString(gauge))
+                .isEqualTo("{\"value\":100}");
+    }
+
+    @Test
+    public void serializesCounters() throws Exception {
+        final Counter counter = mock(Counter.class);
+        when(counter.getCount()).thenReturn(100L);
+
+        assertThat(mapper.writeValueAsString(counter))
+                .isEqualTo("{\"count\":100}");
+    }
+
+    @Test
+    public void serializesHistograms() throws Exception {
+        final Histogram histogram = mock(Histogram.class);
+        when(histogram.getCount()).thenReturn(1L);
+        when(histogram.getMax()).thenReturn(2L);
+        when(histogram.getMean()).thenReturn(3.0);
+        when(histogram.getMin()).thenReturn(4L);
+        when(histogram.getStdDev()).thenReturn(5.0);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMedian()).thenReturn(6.0);
+        when(snapshot.get75thPercentile()).thenReturn(7.0);
+        when(snapshot.get95thPercentile()).thenReturn(8.0);
+        when(snapshot.get98thPercentile()).thenReturn(9.0);
+        when(snapshot.get99thPercentile()).thenReturn(10.0);
+        when(snapshot.get999thPercentile()).thenReturn(11.0);
+        when(snapshot.getValues()).thenReturn(new long[]{ 1, 2, 3 });
+
+        when(histogram.getSnapshot()).thenReturn(snapshot);
+
+        assertThat(mapper.writeValueAsString(histogram))
+                .isEqualTo("{" +
+                                   "\"count\":1," +
+                                   "\"max\":2," +
+                                   "\"mean\":3.0," +
+                                   "\"min\":4," +
+                                   "\"p50\":6.0," +
+                                   "\"p75\":7.0," +
+                                   "\"p95\":8.0," +
+                                   "\"p98\":9.0," +
+                                   "\"p99\":10.0," +
+                                   "\"p999\":11.0," +
+                                   "\"stddev\":5.0}");
+
+        final ObjectMapper fullMapper = new ObjectMapper().registerModule(
+                new MetricsModule(TimeUnit.SECONDS, TimeUnit.MILLISECONDS, true));
+
+        assertThat(fullMapper.writeValueAsString(histogram))
+                .isEqualTo("{" +
+                                   "\"count\":1," +
+                                   "\"max\":2," +
+                                   "\"mean\":3.0," +
+                                   "\"min\":4," +
+                                   "\"p50\":6.0," +
+                                   "\"p75\":7.0," +
+                                   "\"p95\":8.0," +
+                                   "\"p98\":9.0," +
+                                   "\"p99\":10.0," +
+                                   "\"p999\":11.0," +
+                                   "\"values\":[1,2,3]," +
+                                   "\"stddev\":5.0}");
+    }
+
+    @Test
+    public void serializesMeters() throws Exception {
+        final Meter meter = mock(Meter.class);
+        when(meter.getCount()).thenReturn(1L);
+        when(meter.getMeanRate()).thenReturn(2.0);
+        when(meter.getOneMinuteRate()).thenReturn(3.0);
+        when(meter.getFiveMinuteRate()).thenReturn(4.0);
+        when(meter.getFifteenMinuteRate()).thenReturn(5.0);
+
+        assertThat(mapper.writeValueAsString(meter))
+                .isEqualTo("{" +
+                                   "\"count\":1," +
+                                   "\"m15_rate\":3.0," +
+                                   "\"m1_rate\":5.0," +
+                                   "\"m5_rate\":4.0," +
+                                   "\"mean_rate\":2.0," +
+                                   "\"units\":\"events/second\"}");
+    }
+
+    @Test
+    public void serializesTimers() throws Exception {
+        final Timer timer = mock(Timer.class);
+        when(timer.getCount()).thenReturn(1L);
+        when(timer.getMax()).thenReturn(TimeUnit.MILLISECONDS.toNanos(100));
+        when(timer.getMean()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(200));
+        when(timer.getMin()).thenReturn(TimeUnit.MILLISECONDS.toNanos(300));
+        when(timer.getStdDev()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(400));
+
+        when(timer.getMeanRate()).thenReturn(2.0);
+        when(timer.getOneMinuteRate()).thenReturn(3.0);
+        when(timer.getFiveMinuteRate()).thenReturn(4.0);
+        when(timer.getFifteenMinuteRate()).thenReturn(5.0);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMedian()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(500));
+        when(snapshot.get75thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(600));
+        when(snapshot.get95thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(700));
+        when(snapshot.get98thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(800));
+        when(snapshot.get99thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(900));
+        when(snapshot.get999thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(1000));
+
+        when(snapshot.getValues()).thenReturn(new long[]{
+                TimeUnit.MILLISECONDS.toNanos(1),
+                TimeUnit.MILLISECONDS.toNanos(2),
+                TimeUnit.MILLISECONDS.toNanos(3)
+        });
+
+        when(timer.getSnapshot()).thenReturn(snapshot);
+
+        assertThat(mapper.writeValueAsString(timer))
+                .isEqualTo("{" +
+                                   "\"count\":1," +
+                                   "\"max\":100.0," +
+                                   "\"mean\":200.0," +
+                                   "\"min\":300.0," +
+                                   "\"p50\":500.0," +
+                                   "\"p75\":600.0," +
+                                   "\"p95\":700.0," +
+                                   "\"p98\":800.0," +
+                                   "\"p99\":900.0," +
+                                   "\"p999\":1000.0," +
+                                   "\"stddev\":400.0," +
+                                   "\"m15_rate\":3.0," +
+                                   "\"m1_rate\":5.0," +
+                                   "\"m5_rate\":4.0," +
+                                   "\"mean_rate\":2.0," +
+                                   "\"duration_units\":\"milliseconds\"," +
+                                   "\"rate_units\":\"calls/second\"}");
+
+        final ObjectMapper fullMapper = new ObjectMapper().registerModule(
+                new MetricsModule(TimeUnit.SECONDS, TimeUnit.MILLISECONDS, true));
+
+        assertThat(fullMapper.writeValueAsString(timer))
+                .isEqualTo("{" +
+                                   "\"count\":1," +
+                                   "\"max\":100.0," +
+                                   "\"mean\":200.0," +
+                                   "\"min\":300.0," +
+                                   "\"p50\":500.0," +
+                                   "\"p75\":600.0," +
+                                   "\"p95\":700.0," +
+                                   "\"p98\":800.0," +
+                                   "\"p99\":900.0," +
+                                   "\"p999\":1000.0," +
+                                   "\"values\":[1.0,2.0,3.0]," +
+                                   "\"stddev\":400.0," +
+                                   "\"m15_rate\":3.0," +
+                                   "\"m1_rate\":5.0," +
+                                   "\"m5_rate\":4.0," +
+                                   "\"mean_rate\":2.0," +
+                                   "\"duration_units\":\"milliseconds\"," +
+                                   "\"rate_units\":\"calls/second\"}");
+    }
+
+    @Test
+    public void serializesMetricRegistries() throws Exception {
+        final MetricRegistry registry = new MetricRegistry("metrics");
+
+        assertThat(mapper.writeValueAsString(registry))
+                .isEqualTo("{" +
+                                   "\"name\":\"metrics\"," +
+                                   "\"version\":\"3.0.0\"," +
+                                   "\"gauges\":{}," +
+                                   "\"counters\":{}," +
+                                   "\"histograms\":{}," +
+                                   "\"meters\":{}," +
+                                   "\"timers\":{}}");
+    }
+}
