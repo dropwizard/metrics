8316ec069d391ba1a6362a3818dbfe47920c6e41
Coda Hale <coda.hale@gmail.com>
2013-03-12 09:58:57 -0700

    Revamped metrics-logback.

--- metrics-logback/src/main/java/com/yammer/metrics/logback/InstrumentedAppender.java
+++ metrics-logback/src/main/java/com/yammer/metrics/logback/InstrumentedAppender.java
@@ -4,11 +4,10 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.AppenderBase;
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Meter;
-import com.yammer.metrics.core.MetricsRegistry;
+import com.yammer.metrics.Meter;
+import com.yammer.metrics.MetricRegistry;
 
-import java.util.concurrent.TimeUnit;
+import static com.yammer.metrics.MetricRegistry.name;
 
 /**
  * A Logback {@link AppenderBase} which has six meters, one for each logging level and one for the
@@ -22,17 +21,17 @@ public class InstrumentedAppender extends AppenderBase<ILoggingEvent> {
     private final Meter warn;
     private final Meter error;
 
-    public InstrumentedAppender() {
-        this(Metrics.defaultRegistry());
+    public InstrumentedAppender(MetricRegistry registry) {
+        this(registry, null);
     }
 
-    public InstrumentedAppender(MetricsRegistry registry) {
-        this.all = registry.newMeter(Appender.class, "all", "statements", TimeUnit.SECONDS);
-        this.trace = registry.newMeter(Appender.class, "trace", "statements", TimeUnit.SECONDS);
-        this.debug = registry.newMeter(Appender.class, "debug", "statements", TimeUnit.SECONDS);
-        this.info = registry.newMeter(Appender.class, "info", "statements", TimeUnit.SECONDS);
-        this.warn = registry.newMeter(Appender.class, "warn", "statements", TimeUnit.SECONDS);
-        this.error = registry.newMeter(Appender.class, "error", "statements", TimeUnit.SECONDS);
+    public InstrumentedAppender(MetricRegistry registry, String name) {
+        this.all = registry.meter(name(Appender.class, name, "all"));
+        this.trace = registry.meter(name(Appender.class, name, "trace"));
+        this.debug = registry.meter(name(Appender.class, name, "debug"));
+        this.info = registry.meter(name(Appender.class, name, "info"));
+        this.warn = registry.meter(name(Appender.class, name, "warn"));
+        this.error = registry.meter(name(Appender.class, name, "error"));
     }
 
     @Override
