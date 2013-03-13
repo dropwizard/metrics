fd12a1b89d8a8b8603417a760363d0460f2790b2
Coda Hale <coda.hale@gmail.com>
2013-03-12 21:50:44 -0700

    Fleshed out BufferPoolMetricSet.
    Closes #346.

--- metrics-jvm/src/main/java/com/yammer/metrics/jvm/BufferPoolMetricSet.java
+++ metrics-jvm/src/main/java/com/yammer/metrics/jvm/BufferPoolMetricSet.java
@@ -1,24 +1,36 @@
 package com.yammer.metrics.jvm;
 
-import com.yammer.metrics.Gauge;
-import com.yammer.metrics.JmxAttributeGauge;
+import com.yammer.metrics.Metric;
+import com.yammer.metrics.MetricSet;
 
-import javax.management.*;
-import java.util.*;
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-public class BufferPoolGaugeMap {
+import static com.yammer.metrics.MetricRegistry.name;
+
+/**
+ * A set of gauges for the count, usage, and capacity of the JVM's direct and mapped buffer pools.
+ * <p>
+ * These JMX objects are only available on Java 7 and above.
+ */
+public class BufferPoolMetricSet implements MetricSet {
     private static final String[] ATTRIBUTES = { "Count", "MemoryUsed", "TotalCapacity" };
     private static final String[] NAMES = { "count", "used", "capacity" };
     private static final String[] POOLS = { "direct", "mapped" };
 
     private final MBeanServer mBeanServer;
 
-    public BufferPoolGaugeMap(MBeanServer mBeanServer) {
+    public BufferPoolMetricSet(MBeanServer mBeanServer) {
         this.mBeanServer = mBeanServer;
     }
 
-    public Map<String, Gauge<?>> getGauges() {
-        final Map<String, Gauge<?>> gauges = new HashMap<String, Gauge<?>>();
+    @Override
+    public Map<String, Metric> getMetrics() {
+        final Map<String, Metric> gauges = new HashMap<String, Metric>();
         for (String pool : POOLS) {
             for (int i = 0; i < ATTRIBUTES.length; i++) {
                 final String attribute = ATTRIBUTES[i];
@@ -26,7 +38,7 @@ public class BufferPoolGaugeMap {
                 try {
                     final ObjectName on = new ObjectName("java.nio:type=BufferPool,name=" + pool);
                     mBeanServer.getMBeanInfo(on);
-                    gauges.put("jvm.buffers." + pool + "." + name,
+                    gauges.put(name(pool, name),
                                new JmxAttributeGauge(mBeanServer, on, attribute));
                 } catch (JMException ignored) {
 
