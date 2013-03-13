2f909dc4b594f4782ee6a53df7c82d1ccd2b18b5
Coda Hale <coda.hale@gmail.com>
2013-03-12 18:33:56 -0700

    Don't need the old thread dump logic.

--- metrics-jvm/src/main/java/com/yammer/metrics/jvm/VirtualMachineMetrics.java
+++ metrics-jvm/src/main/java/com/yammer/metrics/jvm/VirtualMachineMetrics.java
@@ -1,8 +1,6 @@
 package com.yammer.metrics.jvm;
 
 import javax.management.*;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.lang.Thread.State;
 import java.lang.management.*;
 import java.lang.reflect.InvocationTargetException;
@@ -398,76 +396,6 @@ public class VirtualMachineMetrics {
         return Collections.unmodifiableMap(conditions);
     }
 
-    /**
-     * Dumps all of the threads' current information to an output stream.
-     *
-     * @param out an output stream
-     */
-    public void getThreadDump(OutputStream out) {
-        final ThreadInfo[] threads = this.threads.dumpAllThreads(true, true);
-        final PrintWriter writer = new PrintWriter(out, true);
-
-        for (int ti = threads.length - 1; ti >= 0; ti--) {
-            final ThreadInfo t = threads[ti];
-            writer.printf("%s id=%d state=%s",
-                          t.getThreadName(),
-                          t.getThreadId(),
-                          t.getThreadState());
-            final LockInfo lock = t.getLockInfo();
-            if (lock != null && t.getThreadState() != Thread.State.BLOCKED) {
-                writer.printf("\n    - waiting on <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
-                writer.printf("\n    - locked <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
-            } else if (lock != null && t.getThreadState() == Thread.State.BLOCKED) {
-                writer.printf("\n    - waiting to lock <0x%08x> (a %s)",
-                              lock.getIdentityHashCode(),
-                              lock.getClassName());
-            }
-
-            if (t.isSuspended()) {
-                writer.print(" (suspended)");
-            }
-
-            if (t.isInNative()) {
-                writer.print(" (running in native)");
-            }
-
-            writer.println();
-            if (t.getLockOwnerName() != null) {
-                writer.printf("     owned by %s id=%d\n", t.getLockOwnerName(), t.getLockOwnerId());
-            }
-
-            final StackTraceElement[] elements = t.getStackTrace();
-            final MonitorInfo[] monitors = t.getLockedMonitors();
-
-            for (int i = 0; i < elements.length; i++) {
-                final StackTraceElement element = elements[i];
-                writer.printf("    at %s\n", element);
-                for (int j = 1; j < monitors.length; j++) {
-                    final MonitorInfo monitor = monitors[j];
-                    if (monitor.getLockedStackDepth() == i) {
-                        writer.printf("      - locked %s\n", monitor);
-                    }
-                }
-            }
-            writer.println();
-
-            final LockInfo[] locks = t.getLockedSynchronizers();
-            if (locks.length > 0) {
-                writer.printf("    Locked synchronizers: count = %d\n", locks.length);
-                for (LockInfo l : locks) {
-                    writer.printf("      - %s\n", l);
-                }
-                writer.println();
-            }
-        }
-
-        writer.println();
-        writer.flush();
-    }
 
     public Map<String, BufferPoolStats> getBufferPoolStats() {
         try {
