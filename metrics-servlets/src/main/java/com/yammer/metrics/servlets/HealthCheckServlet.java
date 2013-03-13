3d7ac6d02b6fcd8b23c0bee313a458787357eea6
Coda Hale <coda.hale@gmail.com>
2013-03-12 18:27:53 -0700

    Revamp metrics-servlets.
    Now, with real tests!

---
+++ metrics-servlets/src/main/java/com/yammer/metrics/servlets/HealthCheckServlet.java
@@ -0,0 +1,95 @@
+package com.yammer.metrics.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.yammer.metrics.health.HealthCheck;
+import com.yammer.metrics.health.HealthCheckRegistry;
+import com.yammer.metrics.json.HealthCheckModule;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.concurrent.ExecutorService;
+
+public class HealthCheckServlet extends HttpServlet {
+    public static final String HEALTH_CHECK_REGISTRY = HealthCheckServlet.class.getCanonicalName() + ".registry";
+    public static final String HEALTH_CHECK_EXECUTOR = HealthCheckServlet.class.getCanonicalName() + ".executor";
+
+    private static final long serialVersionUID = -8432996484889177321L;
+    private static final String CONTENT_TYPE = "application/json";
+
+    private transient HealthCheckRegistry registry;
+    private transient ExecutorService executorService;
+    private transient ObjectMapper mapper;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        final Object registryAttr = config.getServletContext().getAttribute(HEALTH_CHECK_REGISTRY);
+        if (registryAttr instanceof HealthCheckRegistry) {
+            this.registry = (HealthCheckRegistry) registryAttr;
+        } else {
+            throw new ServletException("Couldn't find a HealthCheckRegistry instance.");
+        }
+
+        final Object executorAttr = config.getServletContext().getAttribute(HEALTH_CHECK_EXECUTOR);
+        if (executorAttr instanceof ExecutorService) {
+            this.executorService = (ExecutorService) executorAttr;
+        }
+
+        this.mapper = new ObjectMapper().registerModule(new HealthCheckModule());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        final SortedMap<String, HealthCheck.Result> results = runHealthChecks();
+        resp.setContentType(CONTENT_TYPE);
+        resp.setHeader("Cache-Control", "must-revalidate,no-cache,no-store");
+        if (results.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+        } else {
+            if (isAllHealthy(results)) {
+                resp.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        final OutputStream output = resp.getOutputStream();
+        try {
+            getWriter(req).writeValue(output, results);
+        } finally {
+            output.close();
+        }
+    }
+
+    private ObjectWriter getWriter(HttpServletRequest request) {
+        final boolean prettyPrint = Boolean.parseBoolean(request.getParameter("pretty"));
+        if (prettyPrint) {
+            return mapper.writerWithDefaultPrettyPrinter();
+        }
+        return mapper.writer();
+    }
+
+    private SortedMap<String, HealthCheck.Result> runHealthChecks() {
+        if (executorService == null) {
+            return registry.runHealthChecks();
+        }
+        return registry.runHealthChecks(executorService);
+    }
+
+    private static boolean isAllHealthy(Map<String, HealthCheck.Result> results) {
+        for (HealthCheck.Result result : results.values()) {
+            if (!result.isHealthy()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- metrics-servlets/src/main/java/com/yammer/metrics/servlets/ThreadDumpServlet.java
+++ metrics-servlets/src/main/java/com/yammer/metrics/servlets/ThreadDumpServlet.java
@@ -1,38 +1,29 @@
-package com.yammer.metrics.servlet;
+package com.yammer.metrics.servlets;
 
-import com.yammer.metrics.core.VirtualMachineMetrics;
+import com.yammer.metrics.jvm.ThreadDump;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
 
 /**
- * An HTTP servlet which outputs a {@code text/plain} dump of all threads in the VM. Only responds
- * to {@code GET} requests.
- */
+* An HTTP servlets which outputs a {@code text/plain} dump of all threads in the VM. Only responds
+* to {@code GET} requests.
+*/
 public class ThreadDumpServlet extends HttpServlet {
+    private static final long serialVersionUID = -2690343532336103046L;
     private static final String CONTENT_TYPE = "text/plain";
 
-    private final VirtualMachineMetrics vm;
+    private transient ThreadDump threadDump;
 
-    /**
-     * Creates a new {@link ThreadDumpServlet}.
-     */
-    public ThreadDumpServlet() {
-        this(VirtualMachineMetrics.getInstance());
-    }
-
-    /**
-     * Creates a new {@link ThreadDumpServlet} with the given {@link VirtualMachineMetrics}
-     * instance.
-     *
-     * @param vm    a {@link VirtualMachineMetrics} instance
-     */
-    public ThreadDumpServlet(VirtualMachineMetrics vm) {
-        this.vm = vm;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.threadDump = new ThreadDump(ManagementFactory.getThreadMXBean());
     }
 
     @Override
@@ -43,7 +34,7 @@ public class ThreadDumpServlet extends HttpServlet {
         resp.setHeader("Cache-Control", "must-revalidate,no-cache,no-store");
         final OutputStream output = resp.getOutputStream();
         try {
-            vm.threadDump(output);
+            threadDump.dump(output);
         } finally {
             output.close();
         }
