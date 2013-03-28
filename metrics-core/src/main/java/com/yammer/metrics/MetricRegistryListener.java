6dd22e288f053af39df12817182566af735de11a
Coda Hale <coda.hale@gmail.com>
2013-03-28 19:40:18 -0400

    Added docs for MetricRegistryListener.

--- metrics-core/src/main/java/com/yammer/metrics/MetricRegistryListener.java
+++ metrics-core/src/main/java/com/yammer/metrics/MetricRegistryListener.java
@@ -6,23 +6,78 @@ import java.util.EventListener;
  * Listeners for events from the registry.  Listeners must be thread-safe.
  */
 public interface MetricRegistryListener extends EventListener {
+    /**
+     * Called when a {@link Gauge} is added to the registry.
+     *
+     * @param name  the gauge's name
+     * @param gauge the gauge
+     */
     void onGaugeAdded(String name, Gauge<?> gauge);
 
+    /**
+     * Called when a {@link Gauge} is removed from the registry.
+     *
+     * @param name the gauge's name
+     */
     void onGaugeRemoved(String name);
 
+    /**
+     * Called when a {@link Counter} is added to the registry.
+     *
+     * @param name    the counter's name
+     * @param counter the counter
+     */
     void onCounterAdded(String name, Counter counter);
 
+    /**
+     * Called when a {@link Counter} is removed from the registry.
+     *
+     * @param name the counter's name
+     */
     void onCounterRemoved(String name);
 
+    /**
+     * Called when a {@link Histogram} is added to the registry.
+     *
+     * @param name      the histogram's name
+     * @param histogram the histogram
+     */
     void onHistogramAdded(String name, Histogram histogram);
 
+    /**
+     * Called when a {@link Histogram} is removed from the registry.
+     *
+     * @param name the histogram's name
+     */
     void onHistogramRemoved(String name);
 
+    /**
+     * Called when a {@link Meter} is added to the registry.
+     *
+     * @param name  the meter's name
+     * @param meter the meter
+     */
     void onMeterAdded(String name, Meter meter);
 
+    /**
+     * Called when a {@link Meter} is removed from the registry.
+     *
+     * @param name the meter's name
+     */
     void onMeterRemoved(String name);
 
+    /**
+     * Called when a {@link Timer} is added to the registry.
+     *
+     * @param name  the timer's name
+     * @param timer the timer
+     */
     void onTimerAdded(String name, Timer timer);
 
+    /**
+     * Called when a {@link Timer} is removed from the registry.
+     *
+     * @param name the timer's name
+     */
     void onTimerRemoved(String name);
 }
