a771907b570bca88e770337a866233f7d7b8d9be
Coda Hale <coda.hale@gmail.com>
2013-03-28 20:26:54 -0400

    Added docs for GraphiteReporter and Graphite.

--- metrics-graphite/src/main/java/com/yammer/metrics/graphite/Graphite.java
+++ metrics-graphite/src/main/java/com/yammer/metrics/graphite/Graphite.java
@@ -7,6 +7,9 @@ import java.net.Socket;
 import java.nio.charset.Charset;
 import java.util.regex.Pattern;
 
+/**
+ * A client to a Carbon server.
+ */
 public class Graphite implements Closeable {
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]+");
     // this may be optimistic about Carbon/Graphite
@@ -19,12 +22,34 @@ public class Graphite implements Closeable {
     private Writer writer;
     private int failures;
 
+    /**
+     * Creates a new client which connects to the given address using the default
+     * {@link SocketFactory}.
+     *
+     * @param address the address of the Carbon server
+     */
+    public Graphite(InetSocketAddress address) {
+        this(address, SocketFactory.getDefault());
+    }
+
+    /**
+     * Creates a new client which connects to the given address and socket factory.
+     *
+     * @param address the address of the Carbon server
+     * @param socketFactory the socket factory
+     */
     public Graphite(InetSocketAddress address, SocketFactory socketFactory) {
         this.address = address;
         this.socketFactory = socketFactory;
     }
 
-    public void connect() throws IOException {
+    /**
+     * Connects to the server.
+     *
+     * @throws IllegalStateException if the client is already connected
+     * @throws IOException if there is an error connecting
+     */
+    public void connect() throws IllegalStateException, IOException {
         if (socket != null) {
             throw new IllegalStateException("Already connected");
         }
@@ -33,7 +58,15 @@ public class Graphite implements Closeable {
         this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8));
     }
 
-    public void write(String name, String value, long timestamp) throws IOException {
+    /**
+     * Sends the given measurement to the server.
+     *
+     * @param name         the name of the metric
+     * @param value        the value of the metric
+     * @param timestamp    the timestamp of the metric
+     * @throws IOException if there was an error sending the metric
+     */
+    public void send(String name, String value, long timestamp) throws IOException {
         try {
             writer.write(sanitize(name));
             writer.write(' ');
@@ -49,6 +82,11 @@ public class Graphite implements Closeable {
         }
     }
 
+    /**
+     * Returns the number of failed writes to the server.
+     *
+     * @return the number of failed writes to the server
+     */
     public int getFailures() {
         return failures;
     }
