8189fb953f11168cf4c180963854d752765bd5f2
Coda Hale <coda.hale@gmail.com>
2013-03-29 16:42:36 -0700

    Improve tests for Meter.
    Closes #348.

--- metrics-core/src/test/java/com/yammer/metrics/tests/MeterTest.java
+++ metrics-core/src/test/java/com/yammer/metrics/tests/MeterTest.java
@@ -1,30 +1,60 @@
 package com.yammer.metrics.tests;
 
+import com.yammer.metrics.Clock;
 import com.yammer.metrics.Meter;
-import com.yammer.metrics.MetricRegistry;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.offset;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MeterTest {
-    private final MetricRegistry registry = new MetricRegistry("test");
-    private final Meter meter = registry.meter("things");
+    private final Clock clock = mock(Clock.class);
+    private final Meter meter = new Meter(clock);
+
+    @Before
+    public void setUp() throws Exception {
+        when(clock.getTick()).thenReturn(0L, TimeUnit.SECONDS.toNanos(10));
+
+    }
 
     @Test
-    public void aBlankMeter() throws Exception {
+    public void startsOutWithNoRatesOrCount() throws Exception {
         assertThat(meter.getCount())
                 .isZero();
 
         assertThat(meter.getMeanRate())
                 .isEqualTo(0.0, offset(0.001));
+
+        assertThat(meter.getOneMinuteRate())
+                .isEqualTo(0.0, offset(0.001));
+
+        assertThat(meter.getFiveMinuteRate())
+                .isEqualTo(0.0, offset(0.001));
+
+        assertThat(meter.getFifteenMinuteRate())
+                .isEqualTo(0.0, offset(0.001));
     }
 
     @Test
-    public void aMeterWithThreeEvents() throws Exception {
-        meter.mark(3);
+    public void marksEventsAndUpdatesRatesAndCount() throws Exception {
+        meter.mark();
+        meter.mark(2);
 
-        assertThat(meter.getCount())
-                .isEqualTo(3);
+        assertThat(meter.getMeanRate())
+                .isEqualTo(0.3, offset(0.001));
+
+        assertThat(meter.getOneMinuteRate())
+                .isEqualTo(0.1840, offset(0.001));
+
+        assertThat(meter.getFiveMinuteRate())
+                .isEqualTo(0.1966, offset(0.001));
+
+        assertThat(meter.getFifteenMinuteRate())
+                .isEqualTo(0.1988, offset(0.001));
     }
 }
