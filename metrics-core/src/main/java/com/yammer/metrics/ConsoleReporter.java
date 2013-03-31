68d458de232e9ecd5984ed906cd2bd1edba7f73a
Coda Hale <coda.hale@gmail.com>
2013-03-30 17:39:15 -0700

    Added a builder for ConsoleReporter.
    Closes #361.

--- metrics-core/src/main/java/com/yammer/metrics/ConsoleReporter.java
+++ metrics-core/src/main/java/com/yammer/metrics/ConsoleReporter.java
@@ -6,9 +6,139 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A reporter which outputs measurements to a {@link PrintStream}, like STDOUT.
+ * A reporter which outputs measurements to a {@link PrintStream}, like {@code System.out}.
  */
 public class ConsoleReporter extends AbstractPollingReporter {
+    /**
+     * Returns a new {@link Builder} for {@link ConsoleReporter}.
+     *
+     * @param registry the registry to report
+     * @return a {@link Builder} instance for a {@link ConsoleReporter}
+     */
+    public static Builder forRegistry(MetricRegistry registry) {
+        return new Builder(registry);
+    }
+
+    /**
+     * A builder for {@link ConsoleReporter} instances. Defaults to using the default locale and
+     * time zone, writing to {@code System.out}, converting rates to events/second, converting
+     * durations to milliseconds, and not filtering metrics.
+     */
+    public static class Builder {
+        private final MetricRegistry registry;
+        private PrintStream output;
+        private Locale locale;
+        private Clock clock;
+        private TimeZone timeZone;
+        private TimeUnit rateUnit;
+        private TimeUnit durationUnit;
+        private MetricFilter filter;
+
+        private Builder(MetricRegistry registry) {
+            this.registry = registry;
+            this.output = System.out;
+            this.locale = Locale.getDefault();
+            this.clock = Clock.defaultClock();
+            this.timeZone = TimeZone.getDefault();
+            this.rateUnit = TimeUnit.SECONDS;
+            this.durationUnit = TimeUnit.MILLISECONDS;
+            this.filter = MetricFilter.ALL;
+        }
+
+        /**
+         * Write to the given {@link PrintStream}.
+         *
+         * @param output a {@link PrintStream} instance.
+         * @return {@code this}
+         */
+        public Builder outputTo(PrintStream output) {
+            this.output = output;
+            return this;
+        }
+
+        /**
+         * Format numbers for the given {@link Locale}.
+         *
+         * @param locale a {@link Locale}
+         * @return {@code this}
+         */
+        public Builder formattedFor(Locale locale) {
+            this.locale = locale;
+            return this;
+        }
+
+        /**
+         * Use the given {@link Clock} instance for the time.
+         *
+         * @param clock a {@link Clock} instance
+         * @return {@code this}
+         */
+        public Builder withClock(Clock clock) {
+            this.clock = clock;
+            return this;
+        }
+
+        /**
+         * Use the given {@link TimeZone} for the time.
+         *
+         * @param timeZone a {@link TimeZone}
+         * @return {@code this}
+         */
+        public Builder formattedFor(TimeZone timeZone) {
+            this.timeZone = timeZone;
+            return this;
+        }
+
+        /**
+         * Convert rates to the given time unit.
+         *
+         * @param rateUnit a unit of time
+         * @return {@code this}
+         */
+        public Builder convertRatesTo(TimeUnit rateUnit) {
+            this.rateUnit = rateUnit;
+            return this;
+        }
+
+        /**
+         * Convert durations to the given time unit.
+         *
+         * @param durationUnit a unit of time
+         * @return {@code this}
+         */
+        public Builder convertDurationsTo(TimeUnit durationUnit) {
+            this.durationUnit = durationUnit;
+            return this;
+        }
+
+        /**
+         * Only report metrics which match the given filter.
+         *
+         * @param filter a {@link MetricFilter}
+         * @return {@code this}
+         */
+        public Builder filter(MetricFilter filter) {
+            this.filter = filter;
+            return this;
+        }
+
+        /**
+         * Builds a {@link ConsoleReporter} with the given properties.
+         *
+         * @return a {@link ConsoleReporter}
+         */
+        public ConsoleReporter build() {
+            return new ConsoleReporter(registry,
+                                       output,
+                                       locale,
+                                       clock,
+                                       timeZone,
+                                       rateUnit,
+                                       durationUnit,
+                                       filter);
+        }
+    }
+
     private static final int CONSOLE_WIDTH = 80;
 
     private final PrintStream output;
@@ -20,26 +150,14 @@ public class ConsoleReporter extends AbstractPollingReporter {
     private final double rateFactor;
     private final String rateUnit;
 
-    /**
-     * Creates a new {@link ConsoleReporter}.
-     *
-     * @param registry        the registry containing the metrics to report
-     * @param output          the print stream to be written to
-     * @param locale          the local in which data should be formatted
-     * @param clock           a clock
-     * @param timeZone        the time zone in which dates should be presented
-     * @param rateUnit        the unit in which rates should be presented
-     * @param durationUnit    the unit in which durations should be presented
-     * @param filter          the metric filter to match
-     */
-    public ConsoleReporter(MetricRegistry registry,
-                           PrintStream output,
-                           Locale locale,
-                           Clock clock,
-                           TimeZone timeZone,
-                           TimeUnit rateUnit,
-                           TimeUnit durationUnit,
-                           MetricFilter filter) {
+    private ConsoleReporter(MetricRegistry registry,
+                            PrintStream output,
+                            Locale locale,
+                            Clock clock,
+                            TimeZone timeZone,
+                            TimeUnit rateUnit,
+                            TimeUnit durationUnit,
+                            MetricFilter filter) {
         super(registry, "console-reporter", filter);
         this.output = output;
         this.locale = locale;
