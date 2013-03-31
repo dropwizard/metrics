b2f4102e0c218ce1f012316ac20688726653ffd5
Coda Hale <coda.hale@gmail.com>
2013-03-31 11:21:45 -0700

    Added TimeSlidingWindowSample.

---
+++ metrics-core/src/main/java/com/yammer/metrics/TimeSlidingWindowSample.java
@@ -0,0 +1,82 @@
+package com.yammer.metrics;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A {@link Sample} implementation which stores only the measurements made in the last {@code N}
+ * seconds (or other time unit).
+ */
+public class TimeSlidingWindowSample implements Sample {
+    // allow for this many duplicate ticks before overwriting measurements
+    private static final int COLLISION_BUFFER = 100;
+
+    private final Clock clock;
+    private final ConcurrentSkipListMap<Long, Long> measurements;
+    private final long window;
+    private final AtomicLong lastTick;
+
+    /**
+     * Creates a new {@link TimeSlidingWindowSample} with the given window of time.
+     *
+     * @param window     the window of time
+     * @param windowUnit the unit of {@code window}
+     */
+    public TimeSlidingWindowSample(long window, TimeUnit windowUnit) {
+        this(window, windowUnit, Clock.defaultClock());
+    }
+
+    /**
+     * Creates a new {@link TimeSlidingWindowSample} with the given clock and window of time.
+     *
+     * @param window     the window of time
+     * @param windowUnit the unit of {@code window}
+     * @param clock      the {@link Clock} to use
+     */
+    public TimeSlidingWindowSample(long window, TimeUnit windowUnit, Clock clock) {
+        this.clock = clock;
+        this.measurements = new ConcurrentSkipListMap<Long, Long>();
+        this.window = windowUnit.toNanos(window) * COLLISION_BUFFER;
+        this.lastTick = new AtomicLong();
+    }
+
+    @Override
+    public void clear() {
+        measurements.clear();
+    }
+
+    @Override
+    public int size() {
+        trim();
+        return measurements.size();
+    }
+
+    @Override
+    public void update(long value) {
+        measurements.put(getTick(), value);
+        trim();
+    }
+
+    @Override
+    public Snapshot getSnapshot() {
+        trim();
+        return new Snapshot(measurements.values());
+    }
+
+    private long getTick() {
+        for (; ; ) {
+            final long oldTick = lastTick.get();
+            final long tick = clock.getTick() * COLLISION_BUFFER;
+            // ensure the tick is strictly incrementing even if there are duplicate ticks
+            final long newTick = tick > oldTick ? tick : oldTick + 1;
+            if (lastTick.compareAndSet(oldTick, newTick)) {
+                return newTick;
+            }
+        }
+    }
+
+    private void trim() {
+        measurements.headMap(getTick() - window).clear();
+    }
+}
---
+++ metrics-core/src/test/java/com/yammer/metrics/tests/TimeSlidingWindowSampleTest.java
@@ -0,0 +1,48 @@
+package com.yammer.metrics.tests;
+
+import com.yammer.metrics.Clock;
+import com.yammer.metrics.TimeSlidingWindowSample;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TimeSlidingWindowSampleTest {
+    private final Clock clock = mock(Clock.class);
+    private final TimeSlidingWindowSample sample = new TimeSlidingWindowSample(10, TimeUnit.NANOSECONDS, clock);
+
+    @Test
+    public void storesMeasurementsWithDuplicateTicks() throws Exception {
+        when(clock.getTick()).thenReturn(20L);
+
+        sample.update(1);
+        sample.update(2);
+
+        assertThat(sample.getSnapshot().getValues())
+                .containsOnly(1, 2);
+    }
+
+    @Test
+    public void boundsMeasurementsToATimeWindow() throws Exception {
+        when(clock.getTick()).thenReturn(0L);
+        sample.update(1);
+
+        when(clock.getTick()).thenReturn(5L);
+        sample.update(2);
+
+        when(clock.getTick()).thenReturn(10L);
+        sample.update(3);
+
+        when(clock.getTick()).thenReturn(15L);
+        sample.update(4);
+
+        when(clock.getTick()).thenReturn(20L);
+        sample.update(5);
+
+        assertThat(sample.getSnapshot().getValues())
+                .containsOnly(4, 5);
+    }
+}
