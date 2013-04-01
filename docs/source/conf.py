fb0c3d9ae697f96af87d06c554b9fb530fa3c5fe
Coda Hale <coda.hale@gmail.com>
2013-03-31 20:25:35 -0700

    It is the future now.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Metrics'
-copyright = u'2010-2012, Coda Hale, Yammer Inc.'
+copyright = u'2010-2013, Coda Hale, Yammer Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
