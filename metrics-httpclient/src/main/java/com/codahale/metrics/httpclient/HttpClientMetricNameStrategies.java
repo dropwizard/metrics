60a94d340cf86c45be0c73f016b9e4396dd91e48
Coda Hale <coda.hale@gmail.com>
2013-05-13 14:20:07 -0700

    More HttpClient tidying.

--- metrics-httpclient/src/main/java/com/codahale/metrics/httpclient/HttpClientMetricNameStrategies.java
+++ metrics-httpclient/src/main/java/com/codahale/metrics/httpclient/HttpClientMetricNameStrategies.java
@@ -12,39 +12,45 @@ import static com.codahale.metrics.MetricRegistry.name;
 
 public class HttpClientMetricNameStrategies {
 
-    public static final HttpClientMetricNameStrategy METHOD_ONLY = new HttpClientMetricNameStrategy() {
-        @Override
-        public String getNameFor(String name, HttpRequest request) {
-            return name(HttpClient.class,
-                        name,
-                        methodNameString(request));
-        }
-    };
+    public static final HttpClientMetricNameStrategy METHOD_ONLY =
+            new HttpClientMetricNameStrategy() {
+                @Override
+                public String getNameFor(String name, HttpRequest request) {
+                    return name(HttpClient.class,
+                                name,
+                                methodNameString(request));
+                }
+            };
 
-    public static final HttpClientMetricNameStrategy HOST_AND_METHOD = new HttpClientMetricNameStrategy() {
-        @Override
-        public String getNameFor(String name, HttpRequest request) {
-            RequestLine requestLine = request.getRequestLine();
-            URI uri = URI.create(requestLine.getUri());
-            return name(HttpClient.class,
-                        name,
-                        uri.getHost(),
-                        methodNameString(request));
-        }
-    };
+    public static final HttpClientMetricNameStrategy HOST_AND_METHOD =
+            new HttpClientMetricNameStrategy() {
+                @Override
+                public String getNameFor(String name, HttpRequest request) {
+                    final RequestLine requestLine = request.getRequestLine();
+                    final URI uri = URI.create(requestLine.getUri());
+                    return name(HttpClient.class,
+                                name,
+                                uri.getHost(),
+                                methodNameString(request));
+                }
+            };
 
-    public static final HttpClientMetricNameStrategy QUERYLESS_URL_AND_METHOD = new HttpClientMetricNameStrategy() {
-        @Override
-        public String getNameFor(String name, HttpRequest request) {
-            try {
-                RequestLine requestLine = request.getRequestLine();
-                String querylessUrl = new URIBuilder(requestLine.getUri()).removeQuery().build().toString();
-                return name(HttpClient.class, name, querylessUrl, methodNameString(request));
-            } catch (URISyntaxException e) {
-                throw new IllegalArgumentException(e);
-            }
-        }
-    };
+    public static final HttpClientMetricNameStrategy QUERYLESS_URL_AND_METHOD =
+            new HttpClientMetricNameStrategy() {
+                @Override
+                public String getNameFor(String name, HttpRequest request) {
+                    try {
+                        final RequestLine requestLine = request.getRequestLine();
+                        final URIBuilder url = new URIBuilder(requestLine.getUri());
+                        return name(HttpClient.class,
+                                    name,
+                                    url.removeQuery().build().toString(),
+                                    methodNameString(request));
+                    } catch (URISyntaxException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            };
 
     private static String methodNameString(HttpRequest request) {
         return request.getRequestLine().getMethod().toLowerCase() + "-requests";
