3d2f3646cd66fa89796b09c11ce59b18ebf6e470
Coda Hale <coda.hale@gmail.com>
2013-05-13 14:09:18 -0700

    Switch to a two-barrier SlidingWindowReservoir.
    This should prevent phantom reads but also not require full locking.
    
    Closes #395.

--- metrics-core/src/main/java/com/codahale/metrics/SlidingWindowReservoir.java
+++ metrics-core/src/main/java/com/codahale/metrics/SlidingWindowReservoir.java
@@ -1,42 +1,52 @@
 package com.codahale.metrics;
 
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLongArray;
+
 import static java.lang.Math.min;
 
 /**
- * A {@link Reservoir} implementation backed by a sliding window that stores the last {@code N}
- * measurements.
+ * A {@link Reservoir} implementation backed by a sliding window that stores the
+ * last {@code N} measurements.
  */
 public class SlidingWindowReservoir implements Reservoir {
-    private final long[] measurements;
-    private long count;
+    private final AtomicLong index;
+    private final AtomicLong count;
+    private final AtomicLongArray measurements;
 
     /**
-     * Creates a new {@link SlidingWindowReservoir} which stores the last {@code size} measurements.
+     * Creates a new {@link SlidingWindowReservoir} which stores the last
+     * {@code size} measurements.
      *
      * @param size the number of measurements to store
      */
     public SlidingWindowReservoir(int size) {
-        this.measurements = new long[size];
-        this.count = 0;
+        this.index = new AtomicLong();
+        this.count = new AtomicLong();
+        this.measurements = new AtomicLongArray(size);
     }
 
     @Override
-    public synchronized int size() {
-        return (int) min(count, measurements.length);
+    public int size() {
+        return (int) min(count.get(), measurements.length());
     }
 
     @Override
-    public synchronized void update(long value) {
-        measurements[((int) count++ % measurements.length)] = value;
+    public void update(long value) {
+        // first, get and increment the index
+        final int n = (int) (index.getAndIncrement() % measurements.length());
+        // second, set the measurement
+        measurements.set(n, value);
+        // third, increment the count of written measurements
+        count.incrementAndGet();
     }
 
     @Override
     public Snapshot getSnapshot() {
+        // use size, not index, to prevent phantom reads
         final long[] values = new long[size()];
         for (int i = 0; i < values.length; i++) {
-            synchronized (this) {
-                values[i] = measurements[i];
-            }
+            values[i] = measurements.get(i);
         }
         return new Snapshot(values);
     }
