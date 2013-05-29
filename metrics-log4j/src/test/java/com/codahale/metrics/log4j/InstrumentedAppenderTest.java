09a0313d1a67ba06959c82ba0b4acfb1dfd65d13
Coda Hale <coda.hale@gmail.com>
2013-05-29 15:30:04 -0700

    Make metrics-log4j configurable by XML.
    Closes #369.

--- metrics-log4j/src/test/java/com/codahale/metrics/log4j/InstrumentedAppenderTest.java
+++ metrics-log4j/src/test/java/com/codahale/metrics/log4j/InstrumentedAppenderTest.java
@@ -1,97 +1,114 @@
 package com.codahale.metrics.log4j;
 
-import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
-import org.apache.log4j.Appender;
+import com.codahale.metrics.SharedMetricRegistries;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.codahale.metrics.MetricRegistry.name;
-import static org.mockito.Mockito.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class InstrumentedAppenderTest {
-    private Meter all, trace, debug, info, warn, error, fatal;
-    private LoggingEvent event;
-    private InstrumentedAppender instrumented;
+    private final MetricRegistry registry = new MetricRegistry();
+    private final InstrumentedAppender appender = new InstrumentedAppender(registry);
+    private final LoggingEvent event = mock(LoggingEvent.class);
 
     @Before
     public void setUp() throws Exception {
-        this.all = mock(Meter.class);
-        this.trace = mock(Meter.class);
-        this.debug = mock(Meter.class);
-        this.info = mock(Meter.class);
-        this.warn = mock(Meter.class);
-        this.error = mock(Meter.class);
-        this.fatal = mock(Meter.class);
-        
-        this.event = mock(LoggingEvent.class);
+        appender.activateOptions();
+
         when(event.getLevel()).thenReturn(Level.INFO);
+    }
 
-        final MetricRegistry registry = mock(MetricRegistry.class);
-        when(registry.meter(name(Appender.class, "all"))).thenReturn(all);
-        when(registry.meter(name(Appender.class, "trace"))).thenReturn(trace);
-        when(registry.meter(name(Appender.class, "debug"))).thenReturn(debug);
-        when(registry.meter(name(Appender.class, "info"))).thenReturn(info);
-        when(registry.meter(name(Appender.class, "warn"))).thenReturn(warn);
-        when(registry.meter(name(Appender.class, "error"))).thenReturn(error);
-        when(registry.meter(name(Appender.class, "fatal"))).thenReturn(fatal);
-
-        this.instrumented = new InstrumentedAppender(registry);
+    @After
+    public void tearDown() throws Exception {
+        SharedMetricRegistries.clear();
     }
 
     @Test
     public void metersTraceEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.TRACE);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(trace).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.trace").getCount())
+                .isEqualTo(1);
     }
 
     @Test
     public void metersDebugEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.DEBUG);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(debug).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.debug").getCount())
+                .isEqualTo(1);
     }
 
     @Test
     public void metersInfoEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.INFO);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(info).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.info").getCount())
+                .isEqualTo(1);
     }
 
     @Test
     public void metersWarnEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.WARN);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(warn).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.warn").getCount())
+                .isEqualTo(1);
     }
 
     @Test
     public void metersErrorEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.ERROR);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(error).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.error").getCount())
+                .isEqualTo(1);
     }
 
     @Test
     public void metersFatalEvents() throws Exception {
         when(event.getLevel()).thenReturn(Level.FATAL);
-        instrumented.doAppend(event);
+        appender.doAppend(event);
 
-        verify(fatal).mark();
-        verify(all).mark();
+        assertThat(registry.meter("org.apache.log4j.Appender.all").getCount())
+                .isEqualTo(1);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.fatal").getCount())
+                .isEqualTo(1);
+    }
+
+    @Test
+    public void usesSharedRegistries() throws Exception {
+        SharedMetricRegistries.add("reg", registry);
+        final InstrumentedAppender shared = new InstrumentedAppender("reg");
+        shared.activateOptions();
+        shared.doAppend(event);
+
+        assertThat(registry.meter("org.apache.log4j.Appender.info").getCount())
+                .isEqualTo(1);
     }
 }
