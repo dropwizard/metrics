79f9486a090c933e16ae0f6d53f40c16a4f72a97
smartrics <smartrics@gmail.com>
2013-06-30 01:29:43 +0100

    Added the ability to injecy an ObjectName factory to allow clients to decide how to register mbeans exposing metrics. maintained existing behaviour via DefaultObjectNameFactory

---
+++ metrics-core/src/main/java/com/codahale/metrics/DefaultObjectNameFactory.java
@@ -0,0 +1,27 @@
+package com.codahale.metrics;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultObjectNameFactory implements ObjectNameFactory {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JmxReporter.class);
+
+	@Override
+	public ObjectName createName(String type, String domain, String name) {
+		try {
+			return new ObjectName(domain, "name", name);
+		} catch (MalformedObjectNameException e) {
+			try {
+				return new ObjectName(domain, "name", ObjectName.quote(name));
+			} catch (MalformedObjectNameException e1) {
+				LOGGER.warn("Unable to register {} {}", type, name, e1);
+				throw new RuntimeException(e1);
+			}
+		}
+	}
+
+}
