ee6e8f2b520ad43d09e6bd217a30dcb80d08dc91
smartrics <smartrics@gmail.com>
2013-06-30 01:29:43 +0100

    Added the ability to injecy an ObjectName factory to allow clients to decide how to register mbeans exposing metrics. maintained existing behaviour via DefaultObjectNameFactory

---
+++ metrics-core/src/test/java/com/codahale/metrics/DefaultObjectNameFactoryTest.java
@@ -0,0 +1,24 @@
+package com.codahale.metrics;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import javax.management.ObjectName;
+
+import org.junit.Test;
+
+public class DefaultObjectNameFactoryTest {
+
+	@Test
+	public void createsObjectNameWithDomainInInput() {
+		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
+		ObjectName on = f.createName("type", "com.domain", "something.with.dots");
+		assertThat(on.getDomain()).isEqualTo("com.domain");
+	}
+
+	@Test
+	public void createsObjectNameWithNameAsKeyPropertyName() {
+		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
+		ObjectName on = f.createName("type", "com.domain", "something.with.dots");
+		assertThat(on.getKeyProperty("name")).isEqualTo("something.with.dots");
+	}
+}
