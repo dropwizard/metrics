e60576803d638a27ee9abdf996a6128ae1dfa31b
Rohan Soni <rsoni@yammer-inc.com>
2013-09-17 10:26:01 -0700

    Add utilization-max to jetty8

--- metrics-jetty8/src/main/java/com/codahale/metrics/jetty8/InstrumentedQueuedThreadPool.java
+++ metrics-jetty8/src/main/java/com/codahale/metrics/jetty8/InstrumentedQueuedThreadPool.java
@@ -37,5 +37,11 @@ public class InstrumentedQueuedThreadPool extends QueuedThreadPool {
                 return getQueue().size();
             }
         });
+        registry.register(name(QueuedThreadPool.class, "utilization-max"), new RatioGauge() {
+            @Override
+            protected Ratio getRatio() {
+                return Ratio.of(getThreads() - getIdleThreads(), getMaxThreads());
+            }
+        });
     }
 }
