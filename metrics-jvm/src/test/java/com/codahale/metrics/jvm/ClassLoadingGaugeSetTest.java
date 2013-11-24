935e0675bae93641b87f5f0448f7c104e98e5622
Larry Shatzer, Jr <larrys@gmail.com>
2013-11-24 09:58:08 -0700

    Switch to a mocking way of testing.

--- metrics-jvm/src/test/java/com/codahale/metrics/jvm/ClassLoadingGaugeSetTest.java
+++ metrics-jvm/src/test/java/com/codahale/metrics/jvm/ClassLoadingGaugeSetTest.java
@@ -1,42 +1,25 @@
 package com.codahale.metrics.jvm;
 
 import com.codahale.metrics.Gauge;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.management.ClassLoadingMXBean;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ClassLoadingGaugeSetTest {
 
-    private final ClassLoadingMXBean cl = new ClassLoadingMXBean() {
-        @Override
-        public long getTotalLoadedClassCount() {
-            return 2;
-        }
-
-        @Override
-        public int getLoadedClassCount() {
-            return 3;
-        }
-
-        @Override
-        public long getUnloadedClassCount() {
-            return 1;
-        }
-
-        @Override
-        public boolean isVerbose() {
-            return false;
-        }
-
-        @Override
-        public void setVerbose(boolean value) {
-
-        }
-    };
-
+    private final ClassLoadingMXBean cl = mock(ClassLoadingMXBean.class);
     private final ClassLoadingGaugeSet gauges = new ClassLoadingGaugeSet(cl);
+
+    @Before
+    public void setUp() throws Exception {
+        when(cl.getTotalLoadedClassCount()).thenReturn(2L);
+        when(cl.getUnloadedClassCount()).thenReturn(1L);
+    }
 
     @Test
     public void loadedGauge() throws Exception {
