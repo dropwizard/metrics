427eaad53e8d6d42324d43993906238af4884659
Andrei Kolosok <a_kalasok@adamantium.net>
2013-11-28 14:16:04 +0300

    fix #492

--- metrics-core/src/main/java/com/codahale/metrics/JmxReporter.java
+++ metrics-core/src/main/java/com/codahale/metrics/JmxReporter.java
@@ -618,7 +618,7 @@ public class JmxReporter implements Closeable {
             try {
                 final ObjectName objectName = createName("timers", name);
                 mBeanServer.unregisterMBean(objectName);
-                registered.add(objectName);
+                registered.remove(objectName);
             } catch (InstanceNotFoundException e) {
                 LOGGER.debug("Unable to unregister timer", e);
             } catch (MBeanRegistrationException e) {
