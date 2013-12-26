5e067311efe249cf5e4b1682a2ac20a68a59e4df
Jochen Schalanda <jochen@schalanda.name>
2013-12-26 04:10:12 +0100

    Add support for Jersey 2.x

---
+++ metrics-jersey2/src/main/java/com/codahale/metrics/jersey2/MetricsApplicationEventListener.java
@@ -0,0 +1,148 @@
+package com.codahale.metrics.jersey2;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Metered;
+import com.codahale.metrics.annotation.Timed;
+import org.glassfish.jersey.server.ExtendedUriInfo;
+import org.glassfish.jersey.server.monitoring.ApplicationEvent;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.glassfish.jersey.server.monitoring.RequestEvent;
+import org.glassfish.jersey.server.monitoring.RequestEventListener;
+
+import javax.ws.rs.ext.Provider;
+import java.lang.reflect.Method;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+@Provider
+public class MetricsApplicationEventListener implements ApplicationEventListener {
+    private final MetricRegistry registry;
+
+    public MetricsApplicationEventListener(MetricRegistry registry) {
+        this.registry = registry;
+    }
+
+    /**
+     * Process the application {@code event}. This method is called when new event occurs.
+     *
+     * @param event Application event.
+     */
+    @Override
+    public void onEvent(ApplicationEvent event) {
+        // NOP
+    }
+
+    /**
+     * Process a new request and return a {@link org.glassfish.jersey.server.monitoring.RequestEventListener request event listener} if
+     * listening to {@link org.glassfish.jersey.server.monitoring.RequestEvent request events} is required. The method is called once for
+     * each new incoming request. If listening to the request is required then request event must be returned
+     * from the method. Such a request event listener will receive all request events that one request. If listening
+     * to request event for the request is not required then {@code null} must be returned
+     * from the method (do not return empty mock listener in these
+     * cases as it will have negative performance impact).
+     *
+     * @param requestEvent Event of type {@link org.glassfish.jersey.server.monitoring.RequestEvent.Type#START}.
+     * @return Request event listener that will monitor the events of the request
+     *         connected with {@code requestEvent}; null otherwise.
+     */
+    @Override
+    public RequestEventListener onRequest(RequestEvent requestEvent) {
+        switch (requestEvent.getType()) {
+            case START:
+                return new MetricsRequestEventListener(registry);
+            default:
+                return null;
+        }
+    }
+
+    private static class MetricsRequestEventListener implements RequestEventListener {
+        private final MetricRegistry registry;
+
+        private volatile Timer.Context timerContext;
+        private volatile Meter meter;
+        private volatile Meter exceptionMeter;
+
+        private MetricsRequestEventListener(final MetricRegistry registry) {
+            this.registry = registry;
+        }
+
+        @Override
+        public void onEvent(RequestEvent event) {
+            switch (event.getType()) {
+                case REQUEST_MATCHED: {
+                    final ExtendedUriInfo uriInfo = event.getUriInfo();
+                    final Method method = uriInfo.getMatchedResourceMethod().getInvocable().getHandlingMethod();
+                    final Class<?> resourceClass = method.getDeclaringClass();
+
+                    if (method.isAnnotationPresent(Timed.class)) {
+                        final Timed annotation = method.getAnnotation(Timed.class);
+                        final String name = chooseName(annotation.name(),
+                                annotation.absolute(),
+                                resourceClass.getName(),
+                                method.getName());
+                        timerContext = registry.timer(name).time();
+                    }
+
+                    if (method.isAnnotationPresent(Metered.class)) {
+                        final Metered annotation = method.getAnnotation(Metered.class);
+                        final String name = chooseName(annotation.name(),
+                                annotation.absolute(),
+                                resourceClass.getName(),
+                                method.getName());
+                        meter = registry.meter(name);
+                        meter.mark();
+                    }
+                }
+                break;
+                case ON_EXCEPTION: {
+                    final ExtendedUriInfo uriInfo = event.getUriInfo();
+                    final Method method = uriInfo.getMatchedResourceMethod().getInvocable().getHandlingMethod();
+                    final Class<?> resourceClass = method.getDeclaringClass();
+
+                    if (method.isAnnotationPresent(ExceptionMetered.class)) {
+                        final ExceptionMetered annotation = method.getAnnotation(ExceptionMetered.class);
+                        final Class<? extends Throwable> exceptionClass = annotation.cause();
+                        final String name = chooseName(annotation.name(),
+                                annotation.absolute(),
+                                resourceClass.getName(),
+                                method.getName(),
+                                ExceptionMetered.DEFAULT_NAME_SUFFIX);
+                        exceptionMeter = registry.meter(name);
+
+                        final Throwable e = event.getException();
+                        if (exceptionClass.isAssignableFrom(e.getClass()) ||
+                                (e.getCause() != null && exceptionClass.isAssignableFrom(e.getCause().getClass()))) {
+                            exceptionMeter.mark();
+                        }
+                    }
+                }
+                break;
+                case FINISHED:
+                    final ExtendedUriInfo uriInfo = event.getUriInfo();
+                    final Method method = uriInfo.getMatchedResourceMethod().getInvocable().getHandlingMethod();
+
+                    if (method.isAnnotationPresent(Timed.class)) {
+                        timerContext.stop();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private String chooseName(final String explicitName, final boolean absolute,
+                                  final String resourceClass, final String resourceMethod,
+                                  final String... suffixes) {
+            if (explicitName != null && !explicitName.isEmpty()) {
+                if (absolute) {
+                    return explicitName;
+                }
+                return name(resourceClass, explicitName);
+            }
+            return name(name(resourceClass, resourceMethod), suffixes);
+        }
+    }
+}
---
+++ metrics-jersey2/src/test/java/com/codahale/metrics/jersey2/SingletonMetricsJerseyTest.java
@@ -0,0 +1,80 @@
+package com.codahale.metrics.jersey2;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.jersey2.resources.InstrumentedResource;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.core.Application;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
+
+/**
+ * Tests registering {@link MetricsApplicationEventListener} as a singleton
+ * in a Jersey {@link ResourceConfig}.
+ */
+public class SingletonMetricsJerseyTest extends JerseyTest {
+    static {
+        Logger.getLogger("org.glassfish.jersey").setLevel(Level.OFF);
+    }
+
+    private MetricRegistry registry;
+
+    @Override
+    protected Application configure() {
+        this.registry = new MetricRegistry();
+
+        final ResourceConfig config = new ResourceConfig();
+        config.register(new MetricsFeature(registry));
+        config.register(InstrumentedResource.class);
+
+        return config;
+    }
+
+    @Test
+    public void timedMethodsAreTimed() {
+        assertThat(target().path("timed").request().get(String.class)).isEqualTo("yay");
+
+        final Timer timer = registry.timer(name(InstrumentedResource.class, "timed"));
+
+        assertThat(timer.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void meteredMethodsAreMetered() {
+        assertThat(target().path("metered").request().get(String.class)).isEqualTo("woo");
+
+        final Meter meter = registry.meter(name(InstrumentedResource.class, "metered"));
+
+        assertThat(meter.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void exceptionMeteredMethodsAreExceptionMetered() {
+        final Meter meter = registry.meter(name(InstrumentedResource.class,
+                "exceptionMetered",
+                "exceptions"));
+
+        assertThat(target().path("exception-metered").request().get(String.class)).isEqualTo("fuh");
+
+        assertThat(meter.getCount()).isZero();
+
+        try {
+            target().path("exception-metered").queryParam("splode", "true").request().get(String.class);
+            failBecauseExceptionWasNotThrown(ProcessingException.class);
+        } catch (ProcessingException e) {
+            assertThat(e.getCause()).isInstanceOf(IOException.class);
+        }
+
+        assertThat(meter.getCount()).isEqualTo(1);
+    }
+}
