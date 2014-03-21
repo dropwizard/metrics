83e6f698615fc490484cb43c57d5fa5166044673
Michal <michal.kowaliszyn@teamaol.com>
2014-03-21 17:29:17 +0000

    Remove empty lines

--- metrics-graphite/src/test/java/com/codahale/metrics/graphite/GraphiteTest.java
+++ metrics-graphite/src/test/java/com/codahale/metrics/graphite/GraphiteTest.java
@@ -27,10 +27,7 @@ public class GraphiteTest {
     @Before
     public void setUp() throws Exception {
         when(socket.getOutputStream()).thenReturn(output);
-
-        when(socketFactory.createSocket(any(InetAddress.class),
-                                        anyInt())).thenReturn(socket);
-
+        when(socketFactory.createSocket(any(InetAddress.class), anyInt())).thenReturn(socket);
     }
 
     @Test
