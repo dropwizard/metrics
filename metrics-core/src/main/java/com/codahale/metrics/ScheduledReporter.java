9f5a146a4fd924621ec196af7b168925ab379ea0
Ryan Tenney <ryan@10e.us>
2014-03-28 19:26:42 -0400

    Improves the ScheduledReporter#stop method.
    Re: report of scheduled reporters not shutting down (#551).

--- metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
+++ metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
@@ -93,14 +93,26 @@ public abstract class ScheduledReporter implements Closeable, Reporter {
 
     /**
      * Stops the reporter and shuts down its thread of execution.
+     *
+     * Uses the shutdown pattern from http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html
      */
     public void stop() {
-        executor.shutdown();
-        try {
-            executor.awaitTermination(1, TimeUnit.SECONDS);
-        } catch (InterruptedException ignored) {
-            // do nothing
-        }
+		executor.shutdown(); // Disable new tasks from being submitted
+		try {
+			// Wait a while for existing tasks to terminate
+			if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+				executor.shutdownNow(); // Cancel currently executing tasks
+				// Wait a while for tasks to respond to being cancelled
+				if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+					System.err.println(getClass().getSimpleName() + ": ScheduledExecutorService did not terminate");
+				}
+			}
+		} catch (InterruptedException ie) {
+			// (Re-)Cancel if current thread also interrupted
+			executor.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
     }
 
     /**
