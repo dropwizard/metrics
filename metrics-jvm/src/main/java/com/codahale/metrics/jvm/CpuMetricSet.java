ea82da62607c61302478360aa8c6e9f151876562
Wendel Schultz <wendel.schultz@gmail.com>
2014-06-11 22:15:20 -0700

    Bump version to 3.1.0-FORKED-12. CPU and Runtime metric sets.

--- metrics-jvm/src/main/java/com/codahale/metrics/jvm/CpuMetricSet.java
+++ metrics-jvm/src/main/java/com/codahale/metrics/jvm/CpuMetricSet.java
@@ -3,7 +3,6 @@ package com.codahale.metrics.jvm;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricSet;
-import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
@@ -13,11 +12,10 @@ import java.util.Map;
 /**
  * A block-level metric set including CPU metrics.
  */
-@Component
 public class CpuMetricSet implements MetricSet {
 
     public Map<String, Metric> getMetrics() {
-        final Map<String, Metric> metrics = new HashMap<>();
+        final Map<String, Metric> metrics = new HashMap<String, Metric>();
         final OperatingSystemMXBean mxBean = ManagementFactory.getOperatingSystemMXBean();
 
         metrics.put("numCores", new Gauge<Integer>(){
--- metrics-jvm/src/main/java/com/codahale/metrics/jvm/RuntimeMetricSet.java
+++ metrics-jvm/src/main/java/com/codahale/metrics/jvm/RuntimeMetricSet.java
@@ -3,7 +3,6 @@ package com.codahale.metrics.jvm;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricSet;
-import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
@@ -13,11 +12,10 @@ import java.util.Map;
 /**
  * A block-level metric set including runtime metrics.
  */
-@Component
 public class RuntimeMetricSet implements MetricSet {
 
     public Map<String, Metric> getMetrics() {
-        final Map<String, Metric> metrics = new HashMap<>();
+        final Map<String, Metric> metrics = new HashMap<String, Metric>();
         final RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
 
         metrics.put("startTime", new Gauge<Long>(){
