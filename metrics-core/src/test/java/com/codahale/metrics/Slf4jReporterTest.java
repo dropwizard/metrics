0043a39f650ef9054f1a3abb9df74b831cf6efe7
Steve Fosdal <steve.fosdal@here.com>
2014-08-27 12:09:43 -0500

    fixed and added tests

--- metrics-core/src/test/java/com/codahale/metrics/Slf4jReporterTest.java
+++ metrics-core/src/test/java/com/codahale/metrics/Slf4jReporterTest.java
@@ -8,31 +8,183 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
-import static com.codahale.metrics.Slf4jReporter.LoggingLevel;
 import static org.mockito.Mockito.*;
 
 public class Slf4jReporterTest {
     private final Logger logger = mock(Logger.class);
     private final Marker marker = mock(Marker.class);
     private final MetricRegistry registry = mock(MetricRegistry.class);
-    private final Slf4jReporter reporter = Slf4jReporter.forRegistry(registry)
+    private final Slf4jReporter infoReporter = Slf4jReporter.forRegistry(registry)
             .outputTo(logger)
             .markWith(marker)
             .convertRatesTo(TimeUnit.SECONDS)
             .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .withLoggingLevel(Slf4jReporter.LoggingLevel.INFO)
             .filter(MetricFilter.ALL)
-            .withLoggingLevel(LoggingLevel.INFO)
+            .build();
+
+    private final Slf4jReporter errorReporter = Slf4jReporter.forRegistry(registry)
+            .outputTo(logger)
+            .markWith(marker)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .withLoggingLevel(Slf4jReporter.LoggingLevel.ERROR)
+            .filter(MetricFilter.ALL)
             .build();
 
     @Test
-    public void reportsGaugeValues() throws Exception {
-        reporter.report(map("gauge", gauge("value")),
+    public void reportsGaugeValuesAtError() throws Exception {
+        errorReporter.report(map("gauge", gauge("value")),
                 this.<Counter>map(),
                 this.<Histogram>map(),
                 this.<Meter>map(),
                 this.<Timer>map());
 
-        verify(logger).info(marker, "type=GAUGE, name={}, value={}", "gauge", "value");
+        verify(logger).error(marker, "type=GAUGE, name={}, value={}", new Object[]{"gauge", "value"});
+    }
+
+    @Test
+    public void reportsCounterValuesAtError() throws Exception {
+        final Counter counter = mock(Counter.class);
+        when(counter.getCount()).thenReturn(100L);
+
+        errorReporter.report(this.<Gauge>map(),
+                map("test.counter", counter),
+                this.<Histogram>map(),
+                this.<Meter>map(),
+                this.<Timer>map());
+
+        verify(logger).error(marker, "type=COUNTER, name={}, count={}", new Object[]{"test.counter", 100L});
+    }
+
+    @Test
+    public void reportsHistogramValuesAtError() throws Exception {
+        final Histogram histogram = mock(Histogram.class);
+        when(histogram.getCount()).thenReturn(1L);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMax()).thenReturn(2L);
+        when(snapshot.getMean()).thenReturn(3.0);
+        when(snapshot.getMin()).thenReturn(4L);
+        when(snapshot.getStdDev()).thenReturn(5.0);
+        when(snapshot.getMedian()).thenReturn(6.0);
+        when(snapshot.get75thPercentile()).thenReturn(7.0);
+        when(snapshot.get95thPercentile()).thenReturn(8.0);
+        when(snapshot.get98thPercentile()).thenReturn(9.0);
+        when(snapshot.get99thPercentile()).thenReturn(10.0);
+        when(snapshot.get999thPercentile()).thenReturn(11.0);
+
+        when(histogram.getSnapshot()).thenReturn(snapshot);
+
+        errorReporter.report(this.<Gauge>map(),
+                this.<Counter>map(),
+                map("test.histogram", histogram),
+                this.<Meter>map(),
+                this.<Timer>map());
+
+        verify(logger).error(marker,
+                "type=HISTOGRAM, name={}, count={}, min={}, max={}, mean={}, stddev={}, median={}, p75={}, p95={}, p98={}, p99={}, p999={}",
+                "test.histogram",
+                1L,
+                4L,
+                2L,
+                3.0,
+                5.0,
+                6.0,
+                7.0,
+                8.0,
+                9.0,
+                10.0,
+                11.0);
+    }
+
+    @Test
+    public void reportsMeterValuesAtError() throws Exception {
+        final Meter meter = mock(Meter.class);
+        when(meter.getCount()).thenReturn(1L);
+        when(meter.getMeanRate()).thenReturn(2.0);
+        when(meter.getOneMinuteRate()).thenReturn(3.0);
+        when(meter.getFiveMinuteRate()).thenReturn(4.0);
+        when(meter.getFifteenMinuteRate()).thenReturn(5.0);
+
+        errorReporter.report(this.<Gauge>map(),
+                this.<Counter>map(),
+                this.<Histogram>map(),
+                map("test.meter", meter),
+                this.<Timer>map());
+
+        verify(logger).error(marker,
+                "type=METER, name={}, count={}, mean_rate={}, m1={}, m5={}, m15={}, rate_unit={}",
+                "test.meter",
+                1L,
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+                "events/second");
+    }
+
+    @Test
+    public void reportsTimerValuesAtError() throws Exception {
+        final Timer timer = mock(Timer.class);
+        when(timer.getCount()).thenReturn(1L);
+
+        when(timer.getMeanRate()).thenReturn(2.0);
+        when(timer.getOneMinuteRate()).thenReturn(3.0);
+        when(timer.getFiveMinuteRate()).thenReturn(4.0);
+        when(timer.getFifteenMinuteRate()).thenReturn(5.0);
+
+        final Snapshot snapshot = mock(Snapshot.class);
+        when(snapshot.getMax()).thenReturn(TimeUnit.MILLISECONDS.toNanos(100));
+        when(snapshot.getMean()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(200));
+        when(snapshot.getMin()).thenReturn(TimeUnit.MILLISECONDS.toNanos(300));
+        when(snapshot.getStdDev()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(400));
+        when(snapshot.getMedian()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(500));
+        when(snapshot.get75thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(600));
+        when(snapshot.get95thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(700));
+        when(snapshot.get98thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(800));
+        when(snapshot.get99thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS.toNanos(900));
+        when(snapshot.get999thPercentile()).thenReturn((double) TimeUnit.MILLISECONDS
+                .toNanos(1000));
+
+        when(timer.getSnapshot()).thenReturn(snapshot);
+
+        errorReporter.report(this.<Gauge>map(),
+                this.<Counter>map(),
+                this.<Histogram>map(),
+                this.<Meter>map(),
+                map("test.another.timer", timer));
+
+        verify(logger).error(marker,
+                "type=TIMER, name={}, count={}, min={}, max={}, mean={}, stddev={}, median={}, p75={}, p95={}, p98={}, p99={}, p999={}, mean_rate={}, m1={}, m5={}, m15={}, rate_unit={}, duration_unit={}",
+                "test.another.timer",
+                1L,
+                300.0,
+                100.0,
+                200.0,
+                400.0,
+                500.0,
+                600.0,
+                700.0,
+                800.0,
+                900.0,
+                1000.0,
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+                "events/second",
+                "milliseconds");
+    }
+    @Test
+    public void reportsGaugeValues() throws Exception {
+        infoReporter.report(map("gauge", gauge("value")),
+                this.<Counter>map(),
+                this.<Histogram>map(),
+                this.<Meter>map(),
+                this.<Timer>map());
+
+        verify(logger).info(marker, "type=GAUGE, name={}, value={}", new Object[]{"gauge", "value"});
     }
 
     @Test
@@ -40,13 +192,13 @@ public class Slf4jReporterTest {
         final Counter counter = mock(Counter.class);
         when(counter.getCount()).thenReturn(100L);
 
-        reporter.report(this.<Gauge>map(),
+        infoReporter.report(this.<Gauge>map(),
                 map("test.counter", counter),
                 this.<Histogram>map(),
                 this.<Meter>map(),
                 this.<Timer>map());
 
-        verify(logger, times(1)).info(marker, "type=COUNTER, name={}, count={}", "test.counter", 100L);
+        verify(logger).info(marker, "type=COUNTER, name={}, count={}", new Object[]{"test.counter", 100L});
     }
 
     @Test
@@ -68,7 +220,7 @@ public class Slf4jReporterTest {
 
         when(histogram.getSnapshot()).thenReturn(snapshot);
 
-        reporter.report(this.<Gauge>map(),
+        infoReporter.report(this.<Gauge>map(),
                 this.<Counter>map(),
                 map("test.histogram", histogram),
                 this.<Meter>map(),
@@ -99,7 +251,7 @@ public class Slf4jReporterTest {
         when(meter.getFiveMinuteRate()).thenReturn(4.0);
         when(meter.getFifteenMinuteRate()).thenReturn(5.0);
 
-        reporter.report(this.<Gauge>map(),
+        infoReporter.report(this.<Gauge>map(),
                 this.<Counter>map(),
                 this.<Histogram>map(),
                 map("test.meter", meter),
@@ -141,7 +293,7 @@ public class Slf4jReporterTest {
 
         when(timer.getSnapshot()).thenReturn(snapshot);
 
-        reporter.report(this.<Gauge>map(),
+        infoReporter.report(this.<Gauge>map(),
                 this.<Counter>map(),
                 this.<Histogram>map(),
                 this.<Meter>map(),
