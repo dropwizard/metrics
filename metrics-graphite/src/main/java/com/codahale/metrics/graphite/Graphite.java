e21e71be99636a4ed294a16e0a1580e7dac5378c
Ryan Tenney <ryan@10e.us>
2014-10-01 17:33:07 -0400

    Proposed fix for #669 Persistent connections in Graphite implementation are not robust enough

--- metrics-graphite/src/main/java/com/codahale/metrics/graphite/Graphite.java
+++ metrics-graphite/src/main/java/com/codahale/metrics/graphite/Graphite.java
@@ -154,12 +154,19 @@ public class Graphite implements GraphiteSender {
 
     @Override
     public void close() throws IOException {
-    	  flush();
-        if (socket != null) {
-            socket.close();
+        try {
+            flush();
+            if (writer != null) {
+                writer.close();
+            }
+        } catch (IOException ex) {
+            if (socket != null) {
+                socket.close();
+            }
+        } finally {
+            this.socket = null;
+            this.writer = null;
         }
-        this.socket = null;
-        this.writer = null;
     }
 
     protected String sanitize(String s) {
