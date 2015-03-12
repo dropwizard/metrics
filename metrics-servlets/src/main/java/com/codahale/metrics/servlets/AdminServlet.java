cbab8dc51cc963eb1309cac4cf7d33d2aa61dad3
Dmitry Stratiychuk <dstratiychuk@yammer-inc.com>
2015-03-12 09:38:16 +0000

    Add /package_version admin servlet.
    This servlet outputs jar package version as plain text.
    It can be useful to find out which package version is actually running.
    The version is derived from jar package Manifest file via built-in Java mechanism.

--- metrics-servlets/src/main/java/com/codahale/metrics/servlets/AdminServlet.java
+++ metrics-servlets/src/main/java/com/codahale/metrics/servlets/AdminServlet.java
@@ -14,11 +14,13 @@ public class AdminServlet extends HttpServlet {
     public static final String DEFAULT_METRICS_URI = "/metrics";
     public static final String DEFAULT_PING_URI = "/ping";
     public static final String DEFAULT_THREADS_URI = "/threads";
+    public static final String DEFAULT_PACKAGE_VERSION_URI = "/package-version";
 
     public static final String METRICS_URI_PARAM_KEY = "metrics-uri";
     public static final String PING_URI_PARAM_KEY = "ping-uri";
     public static final String THREADS_URI_PARAM_KEY = "threads-uri";
     public static final String HEALTHCHECK_URI_PARAM_KEY = "healthcheck-uri";
+    public static final String PACKAGE_VERSION_URI_PARAM_KEY = "package-version-uri";
     public static final String SERVICE_NAME_PARAM_KEY= "service-name";
 
     private static final String TEMPLATE = String.format(
@@ -26,15 +28,16 @@ public class AdminServlet extends HttpServlet {
                     "        \"http://www.w3.org/TR/html4/loose.dtd\">%n" +
                     "<html>%n" +
                     "<head>%n" +
-                    "  <title>Metrics{8}</title>%n" +
+                    "  <title>Metrics{10}</title>%n" +
                     "</head>%n" +
                     "<body>%n" +
-                    "  <h1>Operational Menu{8}</h1>%n" +
+                    "  <h1>Operational Menu{10}</h1>%n" +
                     "  <ul>%n" +
                     "    <li><a href=\"{0}{1}?pretty=true\">Metrics</a></li>%n" +
                     "    <li><a href=\"{2}{3}\">Ping</a></li>%n" +
                     "    <li><a href=\"{4}{5}\">Threads</a></li>%n" +
                     "    <li><a href=\"{6}{7}?pretty=true\">Healthcheck</a></li>%n" +
+                    "    <li><a href=\"{8}{9}\">Package version</a></li>%n" +
                     "  </ul>%n" +
                     "</body>%n" +
                     "</html>"
@@ -46,10 +49,12 @@ public class AdminServlet extends HttpServlet {
     private transient MetricsServlet metricsServlet;
     private transient PingServlet pingServlet;
     private transient ThreadDumpServlet threadDumpServlet;
+    private transient PackageVersionServlet packageVersionServlet;
     private transient String metricsUri;
     private transient String pingUri;
     private transient String threadsUri;
     private transient String healthcheckUri;
+    private transient String packageVersionUri;
     private transient String serviceName;
 
     @Override
@@ -68,10 +73,14 @@ public class AdminServlet extends HttpServlet {
         this.threadDumpServlet = new ThreadDumpServlet();
         threadDumpServlet.init(config);
 
+        this.packageVersionServlet = new PackageVersionServlet();
+        packageVersionServlet.init(config);
+
         this.metricsUri = getParam(config.getInitParameter(METRICS_URI_PARAM_KEY), DEFAULT_METRICS_URI);
         this.pingUri = getParam(config.getInitParameter(PING_URI_PARAM_KEY), DEFAULT_PING_URI);
         this.threadsUri = getParam(config.getInitParameter(THREADS_URI_PARAM_KEY), DEFAULT_THREADS_URI);
         this.healthcheckUri = getParam(config.getInitParameter(HEALTHCHECK_URI_PARAM_KEY), DEFAULT_HEALTHCHECK_URI);
+        this.packageVersionUri = getParam(config.getInitParameter(PACKAGE_VERSION_URI_PARAM_KEY), DEFAULT_PACKAGE_VERSION_URI);
         this.serviceName = getParam(config.getInitParameter(SERVICE_NAME_PARAM_KEY), null);
     }
 
@@ -85,7 +94,7 @@ public class AdminServlet extends HttpServlet {
         final PrintWriter writer = resp.getWriter();
         try {
             writer.println(MessageFormat.format(TEMPLATE, path, metricsUri, path, pingUri, path,
-                                                threadsUri, path, healthcheckUri,
+                                                threadsUri, path, healthcheckUri, path, packageVersionUri,
                                                 serviceName == null ? "" : " (" + serviceName + ")"));
         } finally {
             writer.close();
@@ -105,6 +114,8 @@ public class AdminServlet extends HttpServlet {
             pingServlet.service(req, resp);
         } else if (uri.equals(threadsUri)) {
             threadDumpServlet.service(req, resp);
+        } else if (uri.equals(packageVersionUri)) {
+            packageVersionServlet.service(req, resp);
         } else {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
