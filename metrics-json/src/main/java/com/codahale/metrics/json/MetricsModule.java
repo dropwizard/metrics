3037f0c648cfd1845f7093d353f12c25b4ad0d20
Ryan Tenney <ryantenney@users.noreply.github.com>
2015-08-25 16:11:49 -0400

    Update MetricsModule.java

--- metrics-json/src/main/java/com/codahale/metrics/json/MetricsModule.java
+++ metrics-json/src/main/java/com/codahale/metrics/json/MetricsModule.java
@@ -15,7 +15,7 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MetricsModule extends Module {
-    static final Version VERSION = new Version(3, 0, 0, "", "com.codahale.metrics", "metrics-json");
+    static final Version VERSION = new Version(3, 1, 3, "", "com.codahale.metrics", "metrics-json");
 
     private static class GaugeSerializer extends StdSerializer<Gauge> {
         private GaugeSerializer() {
