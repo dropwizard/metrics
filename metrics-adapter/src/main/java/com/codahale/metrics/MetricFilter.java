5eb68c914b011a05dc1c0f3226025206d89cf256
Ryan Tenney <ryan@10e.us>
2015-10-19 10:05:59 -0400

    Suppress deprecation warning

--- metrics-adapter/src/main/java/com/codahale/metrics/MetricFilter.java
+++ metrics-adapter/src/main/java/com/codahale/metrics/MetricFilter.java
@@ -1,8 +1,9 @@
 package com.codahale.metrics;
 
+@SuppressWarnings("deprecation")
 public interface MetricFilter {
 	MetricFilter ALL = new MetricFilter() {
-        @Override
+		@Override
         public boolean matches(String name, Metric metric) {
             return true;
         }
