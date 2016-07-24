c25114ccbe5b855568b58a1bfc5fc2d8d0d81668
husseincoder <hussein.009@gmail.com>
2016-07-24 23:49:28 +0200

    Optimizing unit tests

--- metrics-core/src/test/java/com/codahale/metrics/DerivativeGaugeTest.java
+++ metrics-core/src/test/java/com/codahale/metrics/DerivativeGaugeTest.java
@@ -1,22 +1,26 @@
 package com.codahale.metrics;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DerivativeGaugeTest {
-    private final Gauge<String> gauge1 = new Gauge<String>() {
-        @Override
-        public String getValue() {
-            return "woo";
-        }
-    };
+
+    private Gauge<String> gauge1 = mock(Gauge.class);
     private final Gauge<Integer> gauge2 = new DerivativeGauge<String, Integer>(gauge1) {
         @Override
         protected Integer transform(String value) {
             return value.length();
         }
     };
+
+    @Before
+    public void setup() {
+        when(gauge1.getValue()).thenReturn("woo");
+    }
 
     @Test
     public void returnsATransformedValue() throws Exception {
