90d0dc73ccc46b310b3a8396143daded9833421c
Steffen Offermann <steffen.offermann@aixigo.de>
2016-08-16 10:23:44 +0200

    Added fix for GitHub issue 998

--- metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
+++ metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
@@ -123,6 +123,26 @@ public abstract class ScheduledReporter implements Closeable, Reporter {
     }
 
     /**
+     * Starts the reporter polling at the given period.
+     *
+     * @param period       the amount of time between polls
+     * @param initialDelay the time to delay the first execution
+     * @param unit         the unit for {@code period}
+     */
+    public void start(long period, long initialDelay, TimeUnit unit) {
+       executor.scheduleAtFixedRate(new Runnable() {
+           @Override
+           public void run() {
+               try {
+                   report();
+               } catch (RuntimeException ex) {
+                   LOG.error("RuntimeException thrown from {}#report. Exception was suppressed.", ScheduledReporter.this.getClass().getSimpleName(), ex);
+               }
+           }
+       }, initialDelay, period, unit);
+   }
+
+    /**
      * Stops the reporter and shuts down its thread of execution.
      *
      * Uses the shutdown pattern from http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html
