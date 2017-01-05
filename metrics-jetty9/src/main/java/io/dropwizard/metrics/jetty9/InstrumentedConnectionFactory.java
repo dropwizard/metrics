2a1054da2499bc8b529298c2dca1eb2038c96dc3
Artem Prigoda <arteamon@gmail.com>
2017-01-05 16:01:01 +0100

    Upgrade Jetty integration to Jetty 9.4
    This update removes reflection hacks for supporting Jetty 9.3 and higher and
    allows to test the Jetty and Servlets modules against the latest Jetty version
    9.4.0.v20161208

--- metrics-jetty9/src/main/java/io/dropwizard/metrics/jetty9/InstrumentedConnectionFactory.java
+++ metrics-jetty9/src/main/java/io/dropwizard/metrics/jetty9/InstrumentedConnectionFactory.java
@@ -1,32 +1,22 @@
 package io.dropwizard.metrics.jetty9;
 
+import io.dropwizard.metrics.Timer;
 import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.io.EndPoint;
 import org.eclipse.jetty.server.ConnectionFactory;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.util.component.ContainerLifeCycle;
 
-import io.dropwizard.metrics.Timer;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collections;
 import java.util.List;
 
 public class InstrumentedConnectionFactory extends ContainerLifeCycle implements ConnectionFactory {
     private final ConnectionFactory connectionFactory;
     private final Timer timer;
-    private Method getProtocols;
 
     public InstrumentedConnectionFactory(ConnectionFactory connectionFactory, Timer timer) {
         this.connectionFactory = connectionFactory;
         this.timer = timer;
         addBean(connectionFactory);
-        try {
-            getProtocols = connectionFactory.getClass().getMethod("getProtocols");
-        } catch (NoSuchMethodException ignore) {
-            getProtocols = null;
-        }
     }
 
     @Override
@@ -34,15 +24,9 @@ public class InstrumentedConnectionFactory extends ContainerLifeCycle implements
         return connectionFactory.getProtocol();
     }
 
-    @SuppressWarnings("unchecked")
+    @Override
     public List<String> getProtocols() {
-        try {
-            return getProtocols != null ?
-                    (List<String>) getProtocols.invoke(connectionFactory) :
-                    Collections.<String>emptyList();
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalStateException("Unable to invoke `connectionFactory#getProtocols`", e);
-        }
+        return connectionFactory.getProtocols();
     }
 
     @Override
@@ -64,3 +48,4 @@ public class InstrumentedConnectionFactory extends ContainerLifeCycle implements
         return connection;
     }
 }
+
