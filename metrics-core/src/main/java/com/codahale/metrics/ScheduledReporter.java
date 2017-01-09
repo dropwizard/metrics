33c91f907197d3b4786c96575e2033a31c3a1043
Steffen Offermann <steffen.offermann@aixigo.de>
2017-01-09 12:09:30 +0100

    Removed duplicate method

--- metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
+++ metrics-core/src/main/java/com/codahale/metrics/ScheduledReporter.java
@@ -141,30 +141,6 @@ public abstract class ScheduledReporter implements Closeable, Reporter {
     /**
      * Starts the reporter polling at the given period.
      *
-     * @param initialDelay the time to delay the first execution
-     * @param period       the amount of time between polls
-     * @param unit         the unit for {@code period}
-     */
-    synchronized public void start(long initialDelay, long period, TimeUnit unit) {
-      if (this.scheduledFuture != null) {
-          throw new IllegalArgumentException("Reporter already started");
-      }
-
-      this.scheduledFuture = executor.scheduleAtFixedRate(new Runnable() {
-         @Override
-         public void run() {
-             try {
-                 report();
-             } catch (RuntimeException ex) {
-                 LOG.error("RuntimeException thrown from {}#report. Exception was suppressed.", ScheduledReporter.this.getClass().getSimpleName(), ex);
-             }
-         }
-      }, initialDelay, period, unit);
-    }
-
-    /**
-     * Starts the reporter polling at the given period.
-     *
      * @param period       the amount of time between polls
      * @param initialDelay the time to delay the first execution
      * @param unit         the unit for {@code period}
