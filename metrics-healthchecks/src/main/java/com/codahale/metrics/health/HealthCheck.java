4a0b79b240cc6c2d80f28649eaf5627d40ba0774
Gabor Arki <arkigabor@gmail.com>
2017-02-18 20:26:00 +0100

    Fix DateFormat issue

--- metrics-healthchecks/src/main/java/com/codahale/metrics/health/HealthCheck.java
+++ metrics-healthchecks/src/main/java/com/codahale/metrics/health/HealthCheck.java
@@ -16,7 +16,7 @@ public abstract class HealthCheck {
      * or unhealthy (with either an error message or a thrown exception and optional details).
      */
     public static class Result {
-        private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        private static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
         private static final int PRIME = 31;
 
         /**
@@ -115,7 +115,7 @@ public abstract class HealthCheck {
             this.message = message;
             this.error = error;
             this.details = details == null ? null : Collections.unmodifiableMap(details);
-            timestamp = DATE_FORMAT.format(new Date());
+            timestamp = new SimpleDateFormat(DATE_FORMAT_PATTERN).format(new Date());
         }
 
         /**
