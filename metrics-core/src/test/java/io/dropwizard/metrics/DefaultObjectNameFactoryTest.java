ebb7810ee16f770bc1aabd12e2d4da2e31cb940d
Raymond Conn <Raymond.conn90@gmail.com>
2017-02-18 08:21:44 -0500

    change signature on ObjectNameFactory to throw a MalformedObjectNameException

--- metrics-core/src/test/java/io/dropwizard/metrics/DefaultObjectNameFactoryTest.java
+++ metrics-core/src/test/java/io/dropwizard/metrics/DefaultObjectNameFactoryTest.java
@@ -2,6 +2,7 @@ package io.dropwizard.metrics;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
 import org.junit.Test;
@@ -12,14 +13,14 @@ import io.dropwizard.metrics.MetricName;
 public class DefaultObjectNameFactoryTest {
 
 	@Test
-	public void createsObjectNameWithDomainInInput() {
+	public void createsObjectNameWithDomainInInput() throws MalformedObjectNameException {
 		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
 		ObjectName on = f.createName("type", "com.domain", MetricName.build("something.with.dots"));
 		assertThat(on.getDomain()).isEqualTo("com.domain");
 	}
 
 	@Test
-	public void createsObjectNameWithNameAsKeyPropertyName() {
+	public void createsObjectNameWithNameAsKeyPropertyName() throws MalformedObjectNameException {
 		DefaultObjectNameFactory f = new DefaultObjectNameFactory();
 		ObjectName on = f.createName("type", "com.domain", MetricName.build("something.with.dots"));
 		assertThat(on.getKeyProperty("name")).isEqualTo("something.with.dots");
