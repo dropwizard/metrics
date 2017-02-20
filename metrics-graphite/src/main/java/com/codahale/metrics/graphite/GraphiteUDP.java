c4412850ed0a0e893aa1ddde418e75ed74f89e68
Artem Prigoda <arteamon@gmail.com>
2017-02-20 08:31:25 +0100

    Move closing` DatagramChannel` to the close method (#1080)
    We can align `GraphiteUDP` with the TCP based `Graphite` and close the
    socket in the `close` method instead of the connect method. The close
    method will be invoked every time after a portion of metrics has been
    sent, so we can close the socket there. We also do not need to call
    the method `isConnected` during sending metrics because the socket is
    opened right before sending data, so it can't be closed by timeout.
    
    (cherry picked from commit d18b6ab)

--- metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteUDP.java
+++ metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteUDP.java
@@ -48,13 +48,8 @@ public class GraphiteUDP implements GraphiteSender {
 
     @Override
     public void connect() throws IllegalStateException, IOException {
-        // Only open the channel the first time...
         if (isConnected()) {
             throw new IllegalStateException("Already connected");
-        }
-
-        if (datagramChannel != null) {
-            datagramChannel.close();
         }
 
         // Resolve hostname
@@ -72,11 +67,6 @@ public class GraphiteUDP implements GraphiteSender {
 
     @Override
     public void send(String name, String value, long timestamp) throws IOException {
-        // Underlying socket can be closed by ICMP
-        if (!isConnected()) {
-            connect();
-        }
-
         try {
             StringBuilder buf = new StringBuilder();
             buf.append(sanitize(name));
@@ -107,11 +97,32 @@ public class GraphiteUDP implements GraphiteSender {
 
     @Override
     public void close() throws IOException {
-        // Leave channel & socket open for next metrics
+        if (datagramChannel != null) {
+            try {
+                datagramChannel.close();
+            } finally {
+                datagramChannel = null;
+            }
+        }
     }
 
     protected String sanitize(String s) {
         return WHITESPACE.matcher(s).replaceAll("-");
     }
 
+    DatagramChannel getDatagramChannel() {
+        return datagramChannel;
+    }
+
+    void setDatagramChannel(DatagramChannel datagramChannel) {
+        this.datagramChannel = datagramChannel;
+    }
+
+    InetSocketAddress getAddress() {
+        return address;
+    }
+
+    void setAddress(InetSocketAddress address) {
+        this.address = address;
+    }
 }
