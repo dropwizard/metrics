412b0ea5a35c7a5984c41fae80b323ce149fdcda
Artem Prigoda <arteamon@gmail.com>
2017-06-02 10:49:28 +0200

    Meters for httpclient exceptions
    (cherry picked from commit 4695c96bbdd443a66fa7541c4389111d0868cfcf)
    
    # Conflicts:
    #	metrics-httpclient/src/main/java/com/codahale/metrics/httpclient/InstrumentedHttpRequestExecutor.java
    #	metrics-httpclient/src/main/java/io/dropwizard/metrics/httpclient/HttpClientMetricNameStrategy.java
    #	metrics-httpclient/src/test/java/com/codahale/metrics/httpclient/InstrumentedHttpClientsTest.java

--- metrics-httpclient/src/test/java/com/codahale/metrics/httpclient/InstrumentedHttpClientsTest.java
+++ metrics-httpclient/src/test/java/com/codahale/metrics/httpclient/InstrumentedHttpClientsTest.java
@@ -4,13 +4,20 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricRegistryListener;
 import com.codahale.metrics.Timer;
 import org.apache.http.HttpRequest;
+import org.apache.http.NoHttpResponseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
@@ -21,7 +28,7 @@ public class InstrumentedHttpClientsTest {
             mock(MetricRegistryListener.class);
     private final MetricRegistry metricRegistry = new MetricRegistry();
     private final HttpClient client =
-            InstrumentedHttpClients.createDefault(metricRegistry, metricNameStrategy);
+            InstrumentedHttpClients.custom(metricRegistry, metricNameStrategy).disableAutomaticRetries().build();
 
     @Before
     public void setUp() throws Exception {
@@ -33,11 +40,45 @@ public class InstrumentedHttpClientsTest {
         final HttpGet get = new HttpGet("http://example.com?q=anything");
         final String metricName = "some.made.up.metric.name";
 
-        when(metricNameStrategy.getNameFor(anyString(), any(HttpRequest.class)))
+        when(metricNameStrategy.getNameFor(any(), any(HttpRequest.class)))
                 .thenReturn(metricName);
 
         client.execute(get);
 
         verify(registryListener).onTimerAdded(eq(metricName), any(Timer.class));
+    }
+
+    @Test
+    public void registersExpectedExceptionMetrics() throws Exception {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress("localhost", 0));
+        final HttpGet get = new HttpGet("http://localhost:" + server.getLocalPort() + "/");
+        final String requestMetricName = "request";
+        final String exceptionMetricName = "exception";
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                final Socket socket = server.accept();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        serverThread.start();
+
+        when(metricNameStrategy.getNameFor(any(), any(HttpRequest.class)))
+                .thenReturn(requestMetricName);
+        when(metricNameStrategy.getNameFor(any(), any(Exception.class)))
+                .thenReturn(exceptionMetricName);
+
+        try {
+            client.execute(get);
+            fail();
+        } catch (NoHttpResponseException expected) {
+            assertThat(metricRegistry.getMeters()).containsKey("exception");
+        } finally {
+            serverThread.interrupt();
+            serverThread.join();
+        }
     }
 }
