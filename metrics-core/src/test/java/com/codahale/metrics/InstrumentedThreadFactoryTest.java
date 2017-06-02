ad5d6658c270654721d5d56ee18c9554fc74a5de
Artem Prigoda <arteamon@gmail.com>
2017-06-02 14:32:35 +0200

    Simplify InstrumentedThreadFactoryTest
    Use a CountDownLatch instead of wait/notify which is quite low level.
    For instance, you need to wait on the monitor in the loop, which the
    previous code didn't do.

--- metrics-core/src/test/java/com/codahale/metrics/InstrumentedThreadFactoryTest.java
+++ metrics-core/src/test/java/com/codahale/metrics/InstrumentedThreadFactoryTest.java
@@ -22,37 +22,14 @@ public class InstrumentedThreadFactoryTest {
     /**
      * Tests all parts of the InstrumentedThreadFactory except for termination since that
      * is currently difficult to do without race conditions.
-     * 
+     *
      * TODO: Try not using real threads in a unit test?
      */
     @Test
     public void reportsThreadInformation() throws Exception {
-        final CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
-        final Object lock = new Object();
+        final CountDownLatch allTasksAreCreated = new CountDownLatch(THREAD_COUNT);
+        final CountDownLatch allTasksAreCounted = new CountDownLatch(1);
         final AtomicInteger interrupted = new AtomicInteger();
-
-        /*
-         * Implements a runnable that notifies a latch after locking 'lock'.
-         * This asserts that all threads have to enter the critical block before the
-         * testing thread notifies all.
-         *
-         * We have to do this to guarantee that the thread pool has 10 LIVE threads
-         * before we check the 'created' Meter.
-         */
-        Runnable fastOne = new Runnable() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    latch.countDown();
-
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        interrupted.incrementAndGet();
-                    }
-                }
-            }
-        };
 
         Meter created = registry.meter("factory.created");
         Meter terminated = registry.meter("factory.terminated");
@@ -62,15 +39,23 @@ public class InstrumentedThreadFactoryTest {
 
         // generate demand so the executor service creates the threads through our factory.
         for (int i = 0; i < THREAD_COUNT + 1; i++) {
-            executor.submit(fastOne);
+            executor.submit(() -> {
+                allTasksAreCreated.countDown();
+
+                // This asserts that all threads have wait wail the testing thread notifies all.
+                // We have to do this to guarantee that the thread pool has 10 LIVE threads
+                // before we check the 'created' Meter.
+                try {
+                    allTasksAreCounted.await();
+                } catch (InterruptedException e) {
+                    interrupted.incrementAndGet();
+                    Thread.currentThread().interrupt();
+                }
+            });
         }
 
-        latch.await(1, TimeUnit.SECONDS);
-
-        synchronized (lock) {
-            // wake up all threads.
-            lock.notifyAll();
-        }
+        allTasksAreCreated.await(1, TimeUnit.SECONDS);
+        allTasksAreCounted.countDown();
 
         assertThat(created.getCount()).isEqualTo(10);
         assertThat(terminated.getCount()).isEqualTo(0);
