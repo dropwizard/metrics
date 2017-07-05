a6418f76aabd7a10b0fcd821925f46ab6728e733
Ras Kasa Williams <raskasa@users.noreply.github.com>
2017-07-05 08:00:05 -0400

    docs: Fix minor Javadoc spelling error in the Graphite sanitizer. (#1150)

--- metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteSanitize.java
+++ metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteSanitize.java
@@ -8,7 +8,7 @@ class GraphiteSanitize {
     private static final String DASH = "-";
 
     /**
-     * Trims the string and peplaces all whitespace characters with the provided symbol
+     * Trims the string and replaces all whitespace characters with the provided symbol
      */
     static String sanitize(String string) {
         return WHITESPACE.matcher(string.trim()).replaceAll(DASH);
