28d975861c765d3c16fc48b2bf009ab5e1ce0d6f
Artem Prigoda <arteamon@gmail.com>
2017-07-17 19:06:34 +0200

    Remove a redundant cast

--- metrics-healthchecks/src/main/java/com/codahale/metrics/health/AsyncHealthCheckDecorator.java
+++ metrics-healthchecks/src/main/java/com/codahale/metrics/health/AsyncHealthCheckDecorator.java
@@ -1,9 +1,9 @@
 package com.codahale.metrics.health;
 
+import com.codahale.metrics.health.annotation.Async;
+
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-
-import com.codahale.metrics.health.annotation.Async;
 
 /**
  * A health check decorator to manage asynchronous executions.
@@ -17,7 +17,7 @@ class AsyncHealthCheckDecorator extends HealthCheck implements Runnable {
     AsyncHealthCheckDecorator(HealthCheck healthCheck, ScheduledExecutorService executorService) {
         check(healthCheck != null, "healthCheck cannot be null");
         check(executorService != null, "executorService cannot be null");
-        Async async = (Async) healthCheck.getClass().getAnnotation(Async.class);
+        Async async = healthCheck.getClass().getAnnotation(Async.class);
         check(async != null, "healthCheck must contain Async annotation");
         check(async.period() > 0, "period cannot be less than or equal to zero");
         check(async.initialDelay() >= 0, "initialDelay cannot be less than zero");
