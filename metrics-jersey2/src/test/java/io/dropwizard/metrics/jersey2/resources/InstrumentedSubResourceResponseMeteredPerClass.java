ac8c95ceebd8e67fa30e860df6068c015237909b
Denny Abraham Cheriyan <dennyac@gmail.com>
2017-08-21 07:47:12 -0700

    Add ResponseMetered annotation

---
+++ metrics-jersey2/src/test/java/io/dropwizard/metrics/jersey2/resources/InstrumentedSubResourceResponseMeteredPerClass.java
@@ -0,0 +1,19 @@
+package io.dropwizard.metrics.jersey2.resources;
+
+import io.dropwizard.metrics.annotation.ResponseMetered;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@ResponseMetered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceResponseMeteredPerClass {
+    @GET
+    @Path("/responseMeteredPerClass")
+    public Response responseMeteredPerClass() {
+        return Response.status(Response.Status.OK).build();
+    }
+}
