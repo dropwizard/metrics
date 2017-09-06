e81b40fd3848d2402b8cdd3f93d7c9f9e6d929b2
Ben Tatham <bentatham@nanometrics.ca>
2017-09-06 16:48:19 -0400

    Add some common metric filters

--- metrics-core/src/test/java/com/codahale/metrics/MetricFilterTest.java
+++ metrics-core/src/test/java/com/codahale/metrics/MetricFilterTest.java
@@ -11,4 +11,28 @@ public class MetricFilterTest {
         assertThat(MetricFilter.ALL.matches("", mock(Metric.class)))
                 .isTrue();
     }
+
+    @Test
+    public void theStartsWithFilterMatches() throws Exception {
+        assertThat(MetricFilter.startsWith("foo").matches("foo.bar", mock(Metric.class)))
+                .isTrue();
+        assertThat(MetricFilter.startsWith("foo").matches("bar.foo", mock(Metric.class)))
+                .isFalse();
+    }
+
+    @Test
+    public void theEndsWithFilterMatches() throws Exception {
+        assertThat(MetricFilter.endsWith("foo").matches("foo.bar", mock(Metric.class)))
+                .isFalse();
+        assertThat(MetricFilter.endsWith("foo").matches("bar.foo", mock(Metric.class)))
+                .isTrue();
+    }
+
+    @Test
+    public void theContainsFilterMatches() throws Exception {
+        assertThat(MetricFilter.contains("foo").matches("bar.foo.bar", mock(Metric.class)))
+                .isTrue();
+        assertThat(MetricFilter.contains("foo").matches("bar.bar", mock(Metric.class)))
+                .isFalse();
+    }
 }
