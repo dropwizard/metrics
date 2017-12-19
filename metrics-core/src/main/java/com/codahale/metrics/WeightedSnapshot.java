6d44afd411d68d3b9b31f3b3ea850c85c31e0592
Artem Prigoda <arteamon@gmail.com>
2017-12-19 21:45:56 +0100

    Avoid NaN values in WeightedSnapshot (#1233)
    Theoretically, we could have a snapshot with the values which have
    zero weights. In this case a normalized weight will have a NaN value,
    because 0/0 = NaN. We should avoid it and in case a weight is zero,
    make the normalized weight zero too.

--- metrics-core/src/main/java/com/codahale/metrics/WeightedSnapshot.java
+++ metrics-core/src/main/java/com/codahale/metrics/WeightedSnapshot.java
@@ -52,7 +52,7 @@ public class WeightedSnapshot extends Snapshot {
 
         for (int i = 0; i < copy.length; i++) {
             this.values[i] = copy[i].value;
-            this.normWeights[i] = copy[i].weight / sumWeight;
+            this.normWeights[i] = sumWeight != 0 ? copy[i].weight / sumWeight : 0;
         }
 
         for (int i = 1; i < copy.length; i++) {
