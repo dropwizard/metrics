66e07b64d861965ef15840c3506085a39b207947
Artem Prigoda <arteamon@gmail.com>
2018-04-19 21:14:59 +0200

    chore: Ignore flaky tests for Jetty's InstrumentedHandler

--- metrics-jetty9/src/test/java/com/codahale/metrics/jetty9/InstrumentedHandlerTest.java
+++ metrics-jetty9/src/test/java/com/codahale/metrics/jetty9/InstrumentedHandlerTest.java
@@ -9,6 +9,7 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.servlet.AsyncContext;
@@ -106,6 +107,7 @@ public class InstrumentedHandlerTest {
     }
 
     @Test
+    @Ignore("flaky on virtual machines")
     public void responseTimesAreRecordedForAsyncResponses() throws Exception {
 
         final ContentResponse response = client.GET(uri("/async"));
