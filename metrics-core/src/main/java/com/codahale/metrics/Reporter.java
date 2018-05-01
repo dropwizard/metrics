43da91ed96dcfa4f27032487b617f8fef12f28eb
Tomas Celaya <tjcelaya@gmail.com>
2018-05-01 12:35:37 -0700

    Make the relationship between the Reporter and Closeable interfaces explicit. (#1307)
    * Resolves #1305 and reimplements #743. Credit to @obfischer for the initial implementation.
    
    * Fix Code Climate check and call out the java.io.Closeable interface in JmxReporter and ScheduledReporter
    
    * Incorporate feedback about explicit implements in order to maintain binary compatibility

--- metrics-core/src/main/java/com/codahale/metrics/Reporter.java
+++ metrics-core/src/main/java/com/codahale/metrics/Reporter.java
@@ -1,8 +1,10 @@
 package com.codahale.metrics;
 
+import java.io.Closeable;
+
 /*
  * A tag interface to indicate that a class is a Reporter.
  */
-public interface Reporter {
+public interface Reporter extends Closeable {
 
 }
