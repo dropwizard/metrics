c899b00114c56754d8d075ce3a10007dfb8242d9
Michael Peyton Jones <michaelpj@gmail.com>
2018-05-03 20:26:33 +0100

    DefaultObjectNameFactory: use type, be a bit more conservative about quoting (#1310)

--- metrics-jmx/src/main/java/com/codahale/metrics/jmx/DefaultObjectNameFactory.java
+++ metrics-jmx/src/main/java/com/codahale/metrics/jmx/DefaultObjectNameFactory.java
@@ -1,5 +1,7 @@
 package com.codahale.metrics.jmx;
 
+import java.util.Hashtable;
+
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
@@ -13,10 +15,28 @@ public class DefaultObjectNameFactory implements ObjectNameFactory {
     @Override
     public ObjectName createName(String type, String domain, String name) {
         try {
-            ObjectName objectName = new ObjectName(domain, "name", name);
-            if (objectName.isPattern()) {
-                objectName = new ObjectName(domain, "name", ObjectName.quote(name));
+            ObjectName objectName;
+            Hashtable<String, String> properties = new Hashtable<>();
+
+            properties.put("name", name);
+            properties.put("type", type);
+            objectName = new ObjectName(domain, properties);
+
+            /*
+             * The only way we can find out if we need to quote the properties is by
+             * checking an ObjectName that we've constructed.
+             */
+            if (objectName.isDomainPattern()) {
+                domain = ObjectName.quote(domain);
             }
+            if (objectName.isPropertyValuePattern("name")) {
+                properties.put("name", ObjectName.quote(name));
+            }
+            if (objectName.isPropertyValuePattern("type")) {
+                properties.put("type", ObjectName.quote(type));
+            }
+            objectName = new ObjectName(domain, properties);
+
             return objectName;
         } catch (MalformedObjectNameException e) {
             try {
