097adc30a94d253237c330f7e34b9617d8e7a9b2
Fred Deschenes <frederick.deschenes@frimastudio.com>
2018-06-22 15:02:52 -0400

    Update to Jdbi 3.3.0
    Jdbi's `TimingCollector` was deprecated in 3.2. I've marked
    `InstrumentedTimingCollector` as deprecated as well and added a
    replacement `InstrumentedSqlLogger`.

---
+++ metrics-jdbi3/src/main/java/com/codahale/metrics/jdbi3/InstrumentedSqlLogger.java
@@ -0,0 +1,48 @@
+package com.codahale.metrics.jdbi3;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jdbi3.strategies.SmartNameStrategy;
+import com.codahale.metrics.jdbi3.strategies.StatementNameStrategy;
+import org.jdbi.v3.core.statement.SqlLogger;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.SQLException;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A {@link SqlLogger} implementation for JDBI which uses the SQL objects' class names and
+ * method names for millisecond-precision timers.
+ */
+public class InstrumentedSqlLogger implements SqlLogger {
+    private final MetricRegistry registry;
+    private final StatementNameStrategy statementNameStrategy;
+
+    public InstrumentedSqlLogger(MetricRegistry registry) {
+        this(registry, new SmartNameStrategy());
+    }
+
+    public InstrumentedSqlLogger(MetricRegistry registry,
+                                 StatementNameStrategy statementNameStrategy) {
+        this.registry = registry;
+        this.statementNameStrategy = statementNameStrategy;
+    }
+
+    @Override
+    public void logAfterExecution(StatementContext context) {
+        log(context);
+    }
+
+    @Override
+    public void logException(StatementContext context, SQLException ex) {
+        log(context);
+    }
+
+    private void log(StatementContext context) {
+        String statementName = statementNameStrategy.getStatementName(context);
+        if (statementName != null) {
+            final long elapsed = context.getElapsedTime(ChronoUnit.NANOS);
+            registry.timer(statementName).update(elapsed, TimeUnit.NANOSECONDS);
+        }
+    }
+}
