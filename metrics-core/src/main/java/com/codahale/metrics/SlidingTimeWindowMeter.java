aa4150933ee84cc2aa9b5b3f646efd3baa5f58c0
Peter Steiner <unistein+mac-grosapi@gmail.com>
2018-11-17 20:52:22 +0100

    Copy SlidingTimeWindowMeter from snppts repo
    Backport unit tests to JUnit 4

---
+++ metrics-core/src/main/java/com/codahale/metrics/SlidingTimeWindowMeter.java
@@ -0,0 +1,202 @@
+package com.codahale.metrics;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlidingTimeWindowMeter extends Meter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SlidingTimeWindowMeter.class);
+
+    private static final long TIME_WINDOW_DURATION_MINUTES = 15;
+    private static final long TICK_INTERVAL = TimeUnit.SECONDS.toNanos(1);
+    private static final Duration TIME_WINDOW_DURATION = Duration.ofMinutes(TIME_WINDOW_DURATION_MINUTES);
+
+    // package private for the benefit of the unit test
+    static final int NUMBER_OF_BUCKETS = (int) (TIME_WINDOW_DURATION.toNanos() / TICK_INTERVAL);
+
+    private final LongAdder count = new LongAdder();
+    private final long startTime;
+
+    private final AtomicLong lastTick;
+    private final Clock clock;
+
+    private ArrayList<LongAdder> buckets;
+    private int oldestBucketIndex;
+    private int currentBucketIndex;
+    private final Instant bucketBaseTime;
+    Instant oldestBucketTime;
+
+    /**
+     * Creates a new {@link SlidingTimeWindowMeter}.
+     */
+    public SlidingTimeWindowMeter() {
+        this(Clock.defaultClock());
+    }
+
+    /**
+     * Creates a new {@link SlidingTimeWindowMeter}.
+     *
+     * @param clock the clock to use for the meter ticks
+     */
+    public SlidingTimeWindowMeter(Clock clock) {
+        this.clock = clock;
+        startTime = clock.getTick();
+        lastTick = new AtomicLong(startTime);
+
+        buckets = new ArrayList<>(NUMBER_OF_BUCKETS);
+        for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
+            buckets.add(new LongAdder());
+        }
+        bucketBaseTime = Instant.ofEpochSecond(0L, startTime);
+        oldestBucketTime = bucketBaseTime;
+        oldestBucketIndex = 0;
+        currentBucketIndex = 0;
+    }
+
+    /**
+     * Mark the occurrence of an event.
+     */
+    @Override
+    public void mark() {
+        mark(1);
+    }
+
+    /**
+     * Mark the occurrence of a given number of events.
+     *
+     * @param n the number of events
+     */
+    @Override
+    public void mark(long n) {
+        updateLastTickIfNecessary();
+        count.add(n);
+        buckets.get(currentBucketIndex).add(n);
+        LOGGER.trace("mark({}) : current bucket (idx/cnt): ({}/{})",
+                n, currentBucketIndex, buckets.get(currentBucketIndex).longValue());
+    }
+
+    private void updateLastTickIfNecessary() {
+        final long oldTick = lastTick.get();
+        final long newTick = clock.getTick();
+        // System.out.printf("oldTick %d newTick %d\n", oldTick, newTick);
+        final long age = newTick - oldTick;
+        if (age >= TICK_INTERVAL) {
+            final long newLastTick = newTick - age % TICK_INTERVAL;
+            if (lastTick.compareAndSet(oldTick, newLastTick)) {
+                Instant currentInstant = Instant.ofEpochSecond(0L, newLastTick);
+                currentBucketIndex = normalizeIndex(calculateIndexOfTick(currentInstant));
+                LOGGER.debug("new 'current bucket' {} for instant {}", currentBucketIndex, currentInstant);
+                cleanOldBuckets(currentInstant);
+            }
+        }
+    }
+
+    int calculateIndexOfTick(Instant tickTime) {
+        return (int) (Duration.between(bucketBaseTime, tickTime).toNanos() / TICK_INTERVAL);
+    }
+
+    int normalizeIndex(int index) {
+        int mod = index % NUMBER_OF_BUCKETS;
+        return mod >= 0 ? mod : mod + NUMBER_OF_BUCKETS;
+    }
+
+    private void cleanOldBuckets(Instant currentTick) {
+        int newOldestIndex;
+        Instant oldestStillNeededTime = currentTick.minus(TIME_WINDOW_DURATION).plusNanos(TICK_INTERVAL);
+        Instant youngestNotInWindow = oldestBucketTime.plus(TIME_WINDOW_DURATION);
+        LOGGER.trace("current/oldest/stillNeeded/youngestNot {}/{}/{}/{}",
+                currentTick, oldestBucketTime, oldestStillNeededTime, youngestNotInWindow);
+        if (oldestStillNeededTime.isAfter(youngestNotInWindow)) {
+            newOldestIndex = oldestBucketIndex;
+            LOGGER.debug("cleanup: throw away all values, old/new 'oldest': {}/{}", oldestBucketTime, currentTick);
+            oldestBucketTime = currentTick;
+        } else if (oldestStillNeededTime.isAfter(oldestBucketTime)) {
+            newOldestIndex = normalizeIndex(calculateIndexOfTick(oldestStillNeededTime));
+            LOGGER.debug("cleanup: old/new index {}/{}, old/new 'oldest': {}/{}",
+                    oldestBucketIndex, newOldestIndex, oldestBucketTime, currentTick);
+            oldestBucketTime = oldestStillNeededTime;
+        } else {
+            return;
+        }
+
+        if (oldestBucketIndex < newOldestIndex) {
+            for (int i = oldestBucketIndex; i < newOldestIndex; i++) {
+                buckets.get(i).reset();
+            }
+        } else {
+            for (int i = oldestBucketIndex; i < NUMBER_OF_BUCKETS; i++) {
+                buckets.get(i).reset();
+            }
+            for (int i = 0; i < newOldestIndex; i++) {
+                buckets.get(i).reset();
+            }
+        }
+        oldestBucketIndex = newOldestIndex;
+    }
+
+    private long sumBuckets(Instant toTime, int numberOfBuckets) {
+
+        // increment toIndex to include the current bucket into the sum
+        int toIndex = normalizeIndex(calculateIndexOfTick(toTime) + 1);
+        int fromIndex = normalizeIndex(toIndex - numberOfBuckets);
+        LongAdder adder = new LongAdder();
+
+        if (fromIndex < toIndex) {
+            buckets.stream()
+                    .skip(fromIndex)
+                    .limit(toIndex - fromIndex)
+                    .mapToLong(LongAdder::longValue)
+                    .forEach(adder::add);
+        } else {
+            buckets.stream().limit(toIndex).mapToLong(LongAdder::longValue).forEach(adder::add);
+            buckets.stream().skip(fromIndex).mapToLong(LongAdder::longValue).forEach(adder::add);
+        }
+        long retval = adder.longValue();
+        LOGGER.info("sum at {} over {} buckets = {}, current/from/to index {}/{}/{}, oldest index/time {}/{}",
+                toTime, numberOfBuckets, retval, currentBucketIndex, fromIndex, toIndex, oldestBucketIndex, oldestBucketTime);
+        return retval;
+    }
+
+    @Override
+    public long getCount() {
+        return count.sum();
+    }
+
+    @Override
+    public double getMeanRate() {
+        if (getCount() == 0) {
+            return 0.0;
+        } else {
+            final double elapsed = clock.getTick() - startTime;
+            return getCount() / elapsed * TimeUnit.SECONDS.toNanos(1);
+        }
+    }
+
+    @Override
+    public double getFifteenMinuteRate() {
+        updateLastTickIfNecessary();
+        Instant now = Instant.ofEpochSecond(0L, lastTick.get());
+        return sumBuckets(now, (int) (TimeUnit.MINUTES.toNanos(15) / TICK_INTERVAL));
+    }
+
+    @Override
+    public double getFiveMinuteRate() {
+        updateLastTickIfNecessary();
+        Instant now = Instant.ofEpochSecond(0L, lastTick.get());
+        return sumBuckets(now, (int) (TimeUnit.MINUTES.toNanos(5) / TICK_INTERVAL));
+    }
+
+    @Override
+    public double getOneMinuteRate() {
+        updateLastTickIfNecessary();
+        Instant now = Instant.ofEpochSecond(0L, lastTick.get());
+        return sumBuckets(now, (int) (TimeUnit.MINUTES.toNanos(1) / TICK_INTERVAL));
+    }
+}
---
+++ metrics-core/src/test/java/com/codahale/metrics/SlidingTimeWindowMeterTest.java
@@ -0,0 +1,129 @@
+package com.codahale.metrics;
+
+import static com.codahale.metrics.SlidingTimeWindowMeter.NUMBER_OF_BUCKETS;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SlidingTimeWindowMeterTest {
+
+    class MockingClock extends Clock {
+
+        long nextTick = 0L;
+
+        public void setNextTick(long nextTick) {
+            this.nextTick = nextTick;
+        }
+
+        @Override
+        public long getTick() {
+            return nextTick;
+        }
+    }
+
+    private final MockingClock mockingClock = new MockingClock();
+
+    // use a Meter as variable to make sure a SlidingTimeWindowMeter can replace a Meter
+    private Meter meter;
+
+    @Before
+    public void init() {
+        meter = new SlidingTimeWindowMeter(mockingClock);
+    }
+
+    @Test
+    public void normalizeIndex() {
+
+        SlidingTimeWindowMeter stwm = new SlidingTimeWindowMeter();
+
+        assertThat(stwm.normalizeIndex(0), is(0));
+        assertThat(stwm.normalizeIndex(900), is(0));
+        assertThat(stwm.normalizeIndex(9000), is(0));
+        assertThat(stwm.normalizeIndex(-900), is(0));
+
+        assertThat(stwm.normalizeIndex(1), is(1));
+
+        assertThat(stwm.normalizeIndex(899), is(899));
+        assertThat(stwm.normalizeIndex(-1), is(899));
+        assertThat(stwm.normalizeIndex(-901), is(899));
+    }
+
+    @Test
+    public void calculateIndexOfTick() {
+
+        SlidingTimeWindowMeter stwm = new SlidingTimeWindowMeter(new MockingClock());
+
+        assertThat(stwm.calculateIndexOfTick(Instant.ofEpochSecond(0L)), is(0));
+        assertThat(stwm.calculateIndexOfTick(Instant.ofEpochSecond(1L)), is(1));
+    }
+
+    @Test
+    public void mark_max_without_cleanup() {
+
+        int markCount = NUMBER_OF_BUCKETS;
+
+        for (int i = 0; i < markCount; i++) {
+            mockingClock.setNextTick(TimeUnit.SECONDS.toNanos(i));
+            meter.mark();
+        }
+
+        // verify that no cleanup happened yet
+        assertThat(((SlidingTimeWindowMeter) meter).oldestBucketTime, is(Instant.ofEpochSecond(0L)));
+
+        assertThat(meter.getOneMinuteRate(), is(60.0));
+        assertThat(meter.getFiveMinuteRate(), is(300.0));
+        assertThat(meter.getFifteenMinuteRate(), is(900.0));
+    }
+
+    @Test
+    public void mark_first_cleanup() {
+
+        int markCount = NUMBER_OF_BUCKETS + 1;
+
+        for (int i = 0; i < markCount; i++) {
+            mockingClock.setNextTick(TimeUnit.SECONDS.toNanos(i));
+            meter.mark();
+        }
+
+        // verify that at least one cleanup happened
+        assertThat(((SlidingTimeWindowMeter) meter).oldestBucketTime, not(is(Instant.ofEpochSecond(0L))));
+
+        assertThat(meter.getOneMinuteRate(), is(60.0));
+        assertThat(meter.getFiveMinuteRate(), is(300.0));
+        assertThat(meter.getFifteenMinuteRate(), is(900.0));
+    }
+
+    @Test
+    public void mark_10_values() {
+
+        for (int i = 0; i < 10; i++) {
+            mockingClock.setNextTick(TimeUnit.SECONDS.toNanos(i));
+            meter.mark();
+        }
+
+        assertThat(meter.getCount(), is(10L));
+        assertThat(meter.getOneMinuteRate(), is(10.0));
+        assertThat(meter.getFiveMinuteRate(), is(10.0));
+        assertThat(meter.getFifteenMinuteRate(), is(10.0));
+    }
+
+    @Test
+    public void mark_1000_values() {
+
+        for (int i = 0; i < 1000; i++) {
+            mockingClock.setNextTick(TimeUnit.SECONDS.toNanos(i));
+            meter.mark();
+        }
+
+        // only 60/300/900 of the 1000 events took place in the last 1/5/15 minute(s)
+        assertThat(meter.getOneMinuteRate(), is(60.0));
+        assertThat(meter.getFiveMinuteRate(), is(300.0));
+        assertThat(meter.getFifteenMinuteRate(), is(900.0));
+    }
+}
