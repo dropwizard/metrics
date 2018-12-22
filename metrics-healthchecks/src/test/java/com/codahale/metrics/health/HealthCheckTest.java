2be65d3bfd8af73a74ef93a401dca4a1b4d307a1
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2018-12-21 22:09:59 -0500

    Removed an unused import

--- metrics-healthchecks/src/test/java/com/codahale/metrics/health/HealthCheckTest.java
+++ metrics-healthchecks/src/test/java/com/codahale/metrics/health/HealthCheckTest.java
@@ -2,7 +2,6 @@ package com.codahale.metrics.health;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
