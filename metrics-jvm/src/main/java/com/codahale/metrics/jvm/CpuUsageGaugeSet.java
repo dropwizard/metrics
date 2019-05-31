2a8bc41d483e395400ff9636cb1c768afb917ad4
Chinmay Nerurkar <cnerurkar@appnexus.com>
2019-05-30 23:20:09 -0400

    Add Gauges to measure CPU metrics to metrics-jvm

---
+++ metrics-jvm/src/main/java/com/codahale/metrics/jvm/CpuUsageGaugeSet.java
@@ -0,0 +1,47 @@
+package com.codahale.metrics.jvm;
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricSet;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+/**
+ * A set of gauges for JVM memory CPU usage
+ */
+public class CpuUsageGaugeSet implements MetricSet {
+
+    private final OperatingSystemMXBean operatingSystemMXBean;
+
+    public CpuUsageGaugeSet()
+    {
+    	this(ManagementFactory.getOperatingSystemMXBean());
+    }
+    
+    public CpuUsageGaugeSet(OperatingSystemMXBean operatingSystemMXBean) {
+        this.operatingSystemMXBean = operatingSystemMXBean;
+    }
+
+    @Override
+    public Map<String, Metric> getMetrics() {
+        if (!(operatingSystemMXBean instanceof com.sun.management.OperatingSystemMXBean)) {
+            return Collections.emptyMap();
+        }
+        final com.sun.management.OperatingSystemMXBean osMxBean =
+            (com.sun.management.OperatingSystemMXBean) operatingSystemMXBean;
+        final Map<String, Metric> gauges = new HashMap<>();
+
+        gauges.put("process-cpu-load-percentage", (Gauge<Double>) () -> osMxBean.getProcessCpuLoad());
+        gauges.put("system-cpu-load-percentage", (Gauge<Double>) () -> osMxBean.getSystemCpuLoad());
+        gauges.put("system-load-average", (Gauge<Double>) () -> osMxBean.getSystemLoadAverage());
+        gauges.put("process-cpu-time", (Gauge<Long>) () -> osMxBean.getProcessCpuTime());
+        
+        return Collections.unmodifiableMap(gauges);
+    }
+}
---
+++ metrics-jvm/src/test/java/com/codahale/metrics/jvm/CpuUsageGaugeSetTest.java
@@ -0,0 +1,68 @@
+package com.codahale.metrics.jvm;
+
+import com.codahale.metrics.Gauge;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.management.OperatingSystemMXBean;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("rawtypes")
+public class CpuUsageGaugeSetTest {
+    private final OperatingSystemMXBean oxMxBean = mock(OperatingSystemMXBean.class);
+    private final CpuUsageGaugeSet gauges = new CpuUsageGaugeSet(oxMxBean);
+    
+    @Before
+    public void setUp() {
+        when(oxMxBean.getProcessCpuLoad()).thenReturn(10d);
+        when(oxMxBean.getSystemCpuLoad()).thenReturn(20d);
+        when(oxMxBean.getSystemLoadAverage()).thenReturn(30d);
+        when(oxMxBean.getProcessCpuTime()).thenReturn(40L);
+    }
+
+    @Test
+    public void hasASetOfGauges() {
+        assertThat(gauges.getMetrics().keySet())
+                .containsOnly(
+                		"process-cpu-load-percentage",
+                        "system-cpu-load-percentage",
+                        "system-load-average",
+                        "process-cpu-time");
+    }
+
+    @Test
+    public void hasAGaugeForProcessCpuLoadPercentage() {
+        final Gauge gauge = (Gauge) gauges.getMetrics().get("process-cpu-load-percentage");
+
+        assertThat(gauge.getValue())
+                .isEqualTo(10d);
+    }
+    
+    @Test
+    public void hasAGaugeForSystemCpuLoadPercentage() {
+    	final Gauge gauge = (Gauge) gauges.getMetrics().get("system-cpu-load-percentage");
+    	
+    	assertThat(gauge.getValue())
+    	.isEqualTo(20d);
+    }
+    
+    @Test
+    public void hasAGaugeForSystemLoadAverage() {
+    	final Gauge gauge = (Gauge) gauges.getMetrics().get("system-load-average");
+    	
+    	assertThat(gauge.getValue())
+    	.isEqualTo(30d);
+    }
+    
+    @Test
+    public void hasAGaugeForProcessCpuTime() {
+    	final Gauge gauge = (Gauge) gauges.getMetrics().get("process-cpu-time");
+    	
+    	assertThat(gauge.getValue())
+    	.isEqualTo(40L);
+    }
+}
