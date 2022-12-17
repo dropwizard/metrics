a7546271f7009b2ac881ec07574a10d94e4b607b
Jochen Schalanda <jochen@schalanda.name>
2022-12-18 00:02:49 +0100

    Add support for Jersey 3.1.x

---
+++ metrics-jersey31/src/test/java/io/dropwizard/metrics/jersey31/resources/InstrumentedFilteredResource.java
@@ -0,0 +1,61 @@
+package io.dropwizard.metrics.jersey31.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.metrics.jersey31.TestClock;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedFilteredResource {
+
+    private final TestClock testClock;
+
+    public InstrumentedFilteredResource(TestClock testClock) {
+        this.testClock = testClock;
+    }
+
+    @GET
+    @Timed
+    @Path("/timed")
+    public String timed() {
+        testClock.tick++;
+        return "yay";
+    }
+
+    @GET
+    @Timed(name = "fancyName")
+    @Path("/named")
+    public String named() {
+        testClock.tick++;
+        return "fancy";
+    }
+
+    @GET
+    @Timed(name = "absolutelyFancy", absolute = true)
+    @Path("/absolute")
+    public String absolute() {
+        testClock.tick++;
+        return "absolute";
+    }
+
+    @Path("/subresource")
+    public InstrumentedFilteredSubResource locateSubResource() {
+        return new InstrumentedFilteredSubResource();
+    }
+
+    @Produces(MediaType.TEXT_PLAIN)
+    public class InstrumentedFilteredSubResource {
+
+        @GET
+        @Timed
+        @Path("/timed")
+        public String timed() {
+            testClock.tick += 2;
+            return "yay";
+        }
+
+    }
+}
---
+++ metrics-jersey31/src/test/java/io/dropwizard/metrics/jersey31/resources/InstrumentedSubResourceMeteredPerClass.java
@@ -0,0 +1,17 @@
+package io.dropwizard.metrics.jersey31.resources;
+
+import com.codahale.metrics.annotation.Metered;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Metered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceMeteredPerClass {
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+}
