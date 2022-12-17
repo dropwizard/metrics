934a19e870e45f401bbf418962eb30846cf0943f
Jochen Schalanda <jochen@schalanda.name>
2022-12-18 00:20:12 +0100

    Add support for Jersey 3.1.x (#3042)

---
+++ metrics-jersey31/src/test/java/io/dropwizard/metrics5/jersey31/resources/InstrumentedSubResourceMeteredPerClass.java
@@ -0,0 +1,17 @@
+package io.dropwizard.metrics5.jersey31.resources;
+
+import io.dropwizard.metrics5.annotation.Metered;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Metered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceMeteredPerClass {
+    @GET
+    @Path("/meteredPerClass")
+    public String meteredPerClass() {
+        return "yay";
+    }
+}
---
+++ metrics-jersey31/src/test/java/io/dropwizard/metrics5/jersey31/resources/InstrumentedSubResourceResponseMeteredPerClass.java
@@ -0,0 +1,18 @@
+package io.dropwizard.metrics5.jersey31.resources;
+
+import io.dropwizard.metrics5.annotation.ResponseMetered;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@ResponseMetered
+@Produces(MediaType.TEXT_PLAIN)
+public class InstrumentedSubResourceResponseMeteredPerClass {
+    @GET
+    @Path("/responseMeteredPerClass")
+    public Response responseMeteredPerClass() {
+        return Response.status(Response.Status.OK).build();
+    }
+}
