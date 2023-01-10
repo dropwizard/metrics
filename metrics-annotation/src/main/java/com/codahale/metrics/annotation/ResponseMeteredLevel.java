b1aae96862616ab86e90c005c00b370a332cb197
Jochen Schalanda <jochen@schalanda.name>
2023-01-10 23:48:17 +0100

    Update Javadoc in ResponseMeteredLevel

--- metrics-annotation/src/main/java/com/codahale/metrics/annotation/ResponseMeteredLevel.java
+++ metrics-annotation/src/main/java/com/codahale/metrics/annotation/ResponseMeteredLevel.java
@@ -1,23 +1,23 @@
 package com.codahale.metrics.annotation;
 
 /**
- * ResponseMeteredLevel is a parameter for the ResponseMetered annotation. The constants of this enumerated type
- * decide what meters are included when a class or method is annotated with the ResponseMetered annotation.
- *
+ * {@link ResponseMeteredLevel} is a parameter for the {@link ResponseMetered} annotation.
+ * The constants of this enumerated type decide what meters are included when a class
+ * or method is annotated with the {@link ResponseMetered} annotation.
  */
 public enum ResponseMeteredLevel {
     /**
-     * Will include meters for 1xx/2xx/3xx/4xx/5xx responses
+     * Include meters for 1xx/2xx/3xx/4xx/5xx responses
      */
     COARSE,
 
     /**
-     * Will include meters for every response code
+     * Include meters for every response code (200, 201, 303, 304, 401, 404, 501, etc.)
      */
     DETAILED,
 
     /**
-     * Will include meters for every response code in addition to top level 1xx/2xx/3xx/4xx/5xx responses
+     * Include meters for every response code in addition to top level 1xx/2xx/3xx/4xx/5xx responses
      */
     ALL;
 }
