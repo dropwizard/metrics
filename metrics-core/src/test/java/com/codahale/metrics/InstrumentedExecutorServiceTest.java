93a639d9eeeb0ca5d40d589ad8c45a8fd2eab526
Jochen Schalanda <jochen@schalanda.name>
2024-10-03 15:15:10 +0200

    Update InstrumentedExecutorServiceTest.java

--- metrics-core/src/test/java/com/codahale/metrics/InstrumentedExecutorServiceTest.java
+++ metrics-core/src/test/java/com/codahale/metrics/InstrumentedExecutorServiceTest.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
