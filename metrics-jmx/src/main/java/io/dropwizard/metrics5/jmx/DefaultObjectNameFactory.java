4a344620df39e928a5e18ed63e6a2f1b69ad7d18
Piotr Przybylski <piotrprz@gmail.com>
2026-09-09 22:51:01 +0200

    Add tag escaping to `JMXReporter`

--- metrics-jmx/src/main/java/io/dropwizard/metrics5/jmx/DefaultObjectNameFactory.java
+++ metrics-jmx/src/main/java/io/dropwizard/metrics5/jmx/DefaultObjectNameFactory.java
@@ -1,7 +1,9 @@
 package io.dropwizard.metrics5.jmx;
 
 import io.dropwizard.metrics5.MetricName;
+
 import java.util.Hashtable;
+import java.util.regex.Pattern;
 
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
@@ -9,52 +11,49 @@ import javax.management.ObjectName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Default ObjectName factory.
+ * Values are quoted when necessary. Illegal characters in domain and tag names are always replaced with '_'.
+ */
 public class DefaultObjectNameFactory implements ObjectNameFactory {
 
-    private static final char[] QUOTABLE_CHARS = new char[] {',', '=', ':', '"'};
+    // '"' is unsafe only when used at the beginning of a value, but is kept for consistency with old escaping
+    private static final char[] QUOTABLE_CHARS = new char[]{',', '=', ':', '"', '*', '?', '\n'};
+    // keys don't support quoting
+    private static final Pattern INVALID_KEY_CHARS_PATTERN = Pattern.compile("[,=:*?\n]");
+    // domain doesn't support quoting; ':' and '\n' can't be used, while '*' or '?' would turn it into a pattern
+    private static final Pattern INVALID_DOMAIN_CHARS_PATTERN = Pattern.compile("[:*?\n]");
+
     private static final Logger LOGGER = LoggerFactory.getLogger(JmxReporter.class);
 
     @Override
     public ObjectName createName(String type, String domain, MetricName name) {
         try {
-            ObjectName objectName;
             Hashtable<String, String> properties = new Hashtable<>();
 
-            properties.put("name", name.getKey());
-            properties.put("type", type);
-            properties.putAll(name.getTags());
-            objectName = new ObjectName(domain, properties);
+            name.getTags().forEach((key, value) -> {
+                String sanitizedKey = key.isEmpty()
+                        ? "_"
+                        : sanitizeValue(key, INVALID_KEY_CHARS_PATTERN);
+                properties.put(sanitizedKey, quoteIfNeeded(value));
+            });
 
-            /*
-             * The only way we can find out if we need to quote the properties is by
-             * checking an ObjectName that we've constructed.
-             */
-            if (objectName.isDomainPattern()) {
-                domain = ObjectName.quote(domain);
-            }
-            if (objectName.isPropertyValuePattern("name") || shouldQuote(objectName.getKeyProperty("name"))) {
-                properties.put("name", ObjectName.quote(name.getKey()));
-            }
-            if (objectName.isPropertyValuePattern("type") || shouldQuote(objectName.getKeyProperty("type"))) {
-                properties.put("type", ObjectName.quote(type));
-            }
-            objectName = new ObjectName(domain, properties);
+            properties.put("name", quoteIfNeeded(name.getKey()));
+            properties.put("type", quoteIfNeeded(type));
 
-            return objectName;
+            String sanitizedDomain = sanitizeValue(domain, INVALID_DOMAIN_CHARS_PATTERN);
+            return new ObjectName(sanitizedDomain, properties);
         } catch (MalformedObjectNameException e) {
-            try {
-                return new ObjectName(domain, "name", ObjectName.quote(name.getKey()));
-            } catch (MalformedObjectNameException e1) {
-                LOGGER.warn("Unable to register {} {}", type, name, e1);
-                throw new RuntimeException(e1);
-            }
+            LOGGER.warn("Unable to register {} {}", type, name, e);
+            throw new RuntimeException(e);
         }
     }
 
     /**
      * Determines whether the value requires quoting.
      * According to the {@link ObjectName} documentation, values can be quoted or unquoted. Unquoted
-     * values may not contain any of the characters comma, equals, colon, or quote.
+     * values may not contain any of the characters: comma, equals, colon, quote or newline, and
+     * turn the object name into a pattern if they contain an asterisk or a question mark.
      *
      * @param value a value to test
      * @return true when it requires quoting, false otherwise
@@ -66,6 +65,14 @@ public class DefaultObjectNameFactory implements ObjectNameFactory {
             }
         }
         return false;
+    }
+
+    private String quoteIfNeeded(final String value) {
+        return shouldQuote(value) ? ObjectName.quote(value) : value;
+    }
+
+    private String sanitizeValue(final String value, final Pattern pattern) {
+        return pattern.matcher(value).replaceAll("_");
     }
 
 }
